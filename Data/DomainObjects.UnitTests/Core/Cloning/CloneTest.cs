7058136a33b18f1722938c6c568b5bd449979524
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-05-15 14:34:34 +0000

    Added first version of DomainObjectCloner (CreateValueClone support only)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@10233 cb0deee2-328e-b949-94ea-fb3139fab790

--- Data/DomainObjects.UnitTests/Core/Cloning/CloneTest.cs
+++ Data/DomainObjects.UnitTests/Core/Cloning/CloneTest.cs
@@ -1,88 +1,143 @@
 using System;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
+using Remotion.Data.DomainObjects.Cloning;
 using Remotion.Data.DomainObjects.UnitTests.TestDomain;
 
 namespace Remotion.Data.DomainObjects.UnitTests.Core.Cloning
 {
   [TestFixture]
-  [Ignore ("TODO: Cloning")]
   public class DomainObjectClonerTest : ClientTransactionBaseTest
   {
-    //[Test]
-    //public void CreateValueClone_CreatesNewObject ()
-    //{
-    //  DomainObjectCloner cloner = new DomainObjectCloner ();
+    private DomainObjectCloner _cloner;
+    private ClassWithAllDataTypes _classWithAllDataTypes;
+    private Order _order1;
+    private Computer _computer1;
+    private ClassWithAllDataTypes _boundSource;
 
-    //  ClassWithAllDataTypes original = ClassWithAllDataTypes.GetObject (DomainObjectIDs.ClassWithAllDataTypes1);
-    //  ClassWithAllDataTypes clone = cloner.CreateValueClone (original);
+    public override void SetUp ()
+    {
+      base.SetUp();
+      _cloner = new DomainObjectCloner ();
+      _classWithAllDataTypes = ClassWithAllDataTypes.GetObject (DomainObjectIDs.ClassWithAllDataTypes1);
+      _order1 = Order.GetObject (DomainObjectIDs.Order1);
+      _computer1 = Computer.GetObject (DomainObjectIDs.Computer1);
 
-    //  Assert.That (clone, Is.Not.SameAs (original));
-    //  Assert.That (clone.ID, Is.Not.EqualTo (original));
-    //  Assert.That (clone.State, Is.EqualTo (StateType.New));
-    //}
+      using (ClientTransaction.NewBindingTransaction ().EnterNonDiscardingScope ())
+      {
+        _boundSource = ClassWithAllDataTypes.NewObject ();
+        _boundSource.Int32Property = 123;
+      }
+    }
 
-    //[Test]
-    //public void CreateValueClone_SimpleProperties ()
-    //{
-    //  DomainObjectCloner cloner = new DomainObjectCloner ();
+    [Test]
+    public void CreateValueClone_CreatesNewObject ()
+    {
+      DomainObject clone = _cloner.CreateValueClone (_classWithAllDataTypes);
+      Assert.That (clone, Is.Not.SameAs (_classWithAllDataTypes));
+      Assert.That (clone.ID, Is.Not.EqualTo (_classWithAllDataTypes));
+      Assert.That (clone.State, Is.EqualTo (StateType.New));
+    }
 
-    //  ClassWithAllDataTypes original = ClassWithAllDataTypes.GetObject (DomainObjectIDs.ClassWithAllDataTypes1);
-    //  ClassWithAllDataTypes clone = cloner.CreateValueClone (original);
+    [Test]
+    public void CreateValueClone_CreatesObjectOfSameType ()
+    {
+      DomainObject clone = _cloner.CreateValueClone<DomainObject> (_classWithAllDataTypes);
+      Assert.That (clone.GetPublicDomainObjectType(), Is.SameAs (typeof (ClassWithAllDataTypes)));
+    }
 
-    //  Assert.That (clone.Int32Property, Is.EqualTo (original.Int32Property));
-    //}
+    [Test]
+    public void CreateValueClone_CallsNoCtor ()
+    {
+      Order clone = _cloner.CreateValueClone (_order1);
+      Assert.That (clone.CtorCalled, Is.False);
+    }
 
-    //[Test]
-    //public void CreateValueClone_OriginalValueCloned ()
-    //{
-    //  DomainObjectCloner cloner = new DomainObjectCloner ();
+    [Test]
+    public void CreateValueClone_RegistersDataContainer ()
+    {
+      ClassWithAllDataTypes clone = _cloner.CreateValueClone (_classWithAllDataTypes);
+      Assert.That (clone.InternalDataContainer.ClientTransaction, Is.SameAs (ClientTransactionMock));
+    }
 
-    //  ClassWithAllDataTypes original = ClassWithAllDataTypes.GetObject (DomainObjectIDs.ClassWithAllDataTypes1);
-    //  original.Int32Property = 2 * original.Int32Property;
-    //  ClassWithAllDataTypes clone = cloner.CreateValueClone (original);
+    [Test]
+    public void CreateValueClone_SimpleProperties ()
+    {
+      ClassWithAllDataTypes clone = _cloner.CreateValueClone (_classWithAllDataTypes);
+      Assert.That (clone.Int32Property, Is.EqualTo (_classWithAllDataTypes.Int32Property));
+    }
 
-    //  Assert.That (clone.Int32Property, Is.EqualTo (original.Int32Property));
-    //  Assert.That (clone.Properties[typeof (ClassWithAllDataTypes), "Int32Property"].GetOriginalValue<int>(),
-    //      Is.EqualTo (original.Properties[typeof (ClassWithAllDataTypes), "Int32Property"].GetOriginalValue<int> ()));
-    //}
+    [Test]
+    public void CreateValueClone_OriginalValueNotCloned ()
+    {
+      _classWithAllDataTypes.Int32Property = 2 * _classWithAllDataTypes.Int32Property;
+      ClassWithAllDataTypes clone = _cloner.CreateValueClone (_classWithAllDataTypes);
 
-    //[Test]
-    //public void Create_ValueClone_RelationProperties_NonForeignKey ()
-    //{
-    //  DomainObjectCloner cloner = new DomainObjectCloner ();
+      Assert.That (clone.Int32Property, Is.EqualTo (_classWithAllDataTypes.Int32Property));
+      Assert.That (clone.Properties[typeof (ClassWithAllDataTypes), "Int32Property"].GetOriginalValue<int> (),
+          Is.Not.EqualTo (_classWithAllDataTypes.Properties[typeof (ClassWithAllDataTypes), "Int32Property"].GetOriginalValue<int> ()));
+      Assert.That (clone.Properties[typeof (ClassWithAllDataTypes), "Int32Property"].GetOriginalValue<int> (), Is.EqualTo (0));
+    }
 
-    //  Order order = Order.GetObject (DomainObjectIDs.Order1);
-    //  Order clone = cloner.CreateValueClone (order);
+    [Test]
+    public void CreateValueClone_RelationProperties_NonForeignKey ()
+    {
+      Order clone = _cloner.CreateValueClone (_order1);
+      Assert.That (clone.OrderItems, Is.Empty);
+      Assert.That (clone.OrderTicket, Is.Null);
+    }
 
-    //  Assert.That (clone.OrderItems, Is.Empty);
-    //  Assert.That (clone.OrderTicket, Is.Null);
-    //}
+    [Test]
+    public void CreateValueClone_RelationProperties_ForeignKey ()
+    {
+      Computer clone = _cloner.CreateValueClone (_computer1);
+      Assert.That (_computer1.Employee, Is.Not.Null);
+      Assert.That (clone.Employee, Is.Null);
+      Assert.That (clone.InternalDataContainer.PropertyValues[ReflectionUtility.GetPropertyName (typeof (Computer), "Employee")].Value, Is.Null);
+    }
 
-    //[Test]
-    //public void Create_ValueClone_RelationProperties_ForeignKey ()
-    //{
-    //  DomainObjectCloner cloner = new DomainObjectCloner ();
+    [Test]
+    public void CreateValueClone_RelationProperties_ForeignKey_OriginalValue ()
+    {
+      Computer clone = _cloner.CreateValueClone (_computer1);
+      Assert.That (_computer1.Employee, Is.Not.Null);
+      Assert.That (clone.Properties[typeof (Computer), "Employee"].GetOriginalValue<Employee> (), Is.Null);
+      Assert.That (clone.InternalDataContainer.PropertyValues[ReflectionUtility.GetPropertyName (typeof (Computer), "Employee")].OriginalValue, Is.Null);
+    }
 
-    //  Computer computer = Computer.GetObject (DomainObjectIDs.Computer1);
-    //  Computer clone = cloner.CreateValueClone (computer);
+    [Test]
+    public void SourceTransaction_IsRespected ()
+    {
+      ClassWithAllDataTypes unboundClone = _cloner.CreateValueClone (_boundSource);
+      Assert.That (unboundClone.Int32Property, Is.EqualTo (123));
+    }
 
-    //  Assert.That (computer.Employee, Is.Not.Null);
-    //  Assert.That (clone.Employee, Is.Null);
-    //  Assert.That (clone.InternalDataContainer.PropertyValues[ReflectionUtility.GetPropertyName (typeof (Computer), "Employee")].Value, Is.Null);
-    //}
+    [Test]
+    [ExpectedException (typeof (InvalidOperationException), ExpectedMessage = "No ClientTransaction has been associated with the current thread or " 
+        + "this object.")]
+    public void NullSourceTransaction ()
+    {
+      using (ClientTransactionScope.EnterNullScope ())
+      {
+        _cloner.CreateValueClone (_classWithAllDataTypes);
+      }
+    }
 
-    //[Test]
-    //public void Create_ValueClone_RelationProperties_ForeignKey_OriginalValue ()
-    //{
-    //  DomainObjectCloner cloner = new DomainObjectCloner ();
+    [Test]
+    public void CloneTransaction_IsCurrent ()
+    {
+      ClassWithAllDataTypes unboundClone = _cloner.CreateValueClone (_boundSource);
+      Assert.That (unboundClone.ClientTransaction, Is.SameAs (ClientTransactionMock));
+    }
 
-    //  Computer computer = Computer.GetObject (DomainObjectIDs.Computer1);
-    //  Computer clone = cloner.CreateValueClone (computer);
-
-    //  Assert.That (computer.Employee, Is.Not.Null);
-    //  Assert.That (clone.Properties[typeof (Computer), "Employee"].GetOriginalValue<Employee>(), Is.Null);
-    //  Assert.That (clone.InternalDataContainer.PropertyValues[ReflectionUtility.GetPropertyName (typeof (Computer), "Employee")].OriginalValue, Is.Null);
-    //}
+    [Test]
+    [ExpectedException (typeof (InvalidOperationException), ExpectedMessage = "No ClientTransaction has been associated with the current thread.")]
+    public void NullCloneTransaction ()
+    {
+      using (ClientTransactionScope.EnterNullScope ())
+      {
+        _cloner.CreateValueClone (_boundSource);
+      }
+    }
   }
 }
--- Data/DomainObjects/Cloning/DomainObjectCloner.cs
+++ Data/DomainObjects/Cloning/DomainObjectCloner.cs
@@ -1,48 +1,52 @@
 using System;
+using System.Collections.Generic;
 using Remotion.Data.DomainObjects;
 using Remotion.Data.DomainObjects.Infrastructure;
 using Remotion.Data.DomainObjects.Mapping;
+using Remotion.Utilities;
 
 namespace Remotion.Data.DomainObjects.Cloning
 {
-  // TODO: Change to use ordinary property setters => event handling easier
-  ///// <summary>
-  ///// Assists in cloning <see cref="DomainObject"/> instances.
-  ///// </summary>
-  //public class DomainObjectCloner
-  //{
-  //  /// <summary>
-  //  /// Creates a new <see cref="DomainObject"/> instance of the same type and with the same property values as the given <paramref name="source"/>.
-  //  /// Relations are not cloned, foreign key properties default to null.
-  //  /// </summary>
-  //  /// <typeparam name="T">The static <see cref="DomainObject"/> type to be cloned. Note that the actual (dynamic) type of the cloned object
-  //  /// is the type defined by <paramref name="source"/>'s <see cref="ClassDefinition"/>.</typeparam>
-  //  /// <param name="source">The <see cref="DomainObject"/> to be cloned.</param>
-  //  /// <remarks>
-  //  /// The clone is created in the current transaction. No constructor is called on the clone object, and no property or relation set events are raised.
-  //  /// </remarks>
-  //  public T CreateValueClone<T> (T source)
-  //      where T: DomainObject
-  //  {
-  //    ClientTransaction sourceTransaction = source.ClientTransaction;
-  //    ClientTransaction cloneTransaction = ClientTransactionScope.CurrentTransaction;
-  //    DataContainer sourceDataContainer = source.GetDataContainerForTransaction (sourceTransaction);
+  /// <summary>
+  /// Assists in cloning <see cref="DomainObject"/> instances.
+  /// </summary>
+  public class DomainObjectCloner
+  {
+    /// <summary>
+    /// Creates a new <see cref="DomainObject"/> instance of the same type and with the same property values as the given <paramref name="source"/>.
+    /// Relations are not cloned, foreign key properties default to null.
+    /// </summary>
+    /// <typeparam name="T">The static <see cref="DomainObject"/> type to be cloned. Note that the actual (dynamic) type of the cloned object
+    /// is the type defined by <paramref name="source"/>'s <see cref="ClassDefinition"/>.</typeparam>
+    /// <param name="source">The <see cref="DomainObject"/> to be cloned.</param>
+    /// <remarks>
+    /// The clone is created in the current transaction. No constructor is called on the clone object; property or relation get and set events are 
+    /// raised as needed by the cloner.
+    /// </remarks>
+    public T CreateValueClone<T> (T source)
+        where T : DomainObject
+    {
+      ObjectID sourceID = source.ID;
+      ClientTransaction sourceTransaction = source.GetNonNullClientTransaction();
+      ClientTransaction cloneTransaction = ClientTransactionScope.CurrentTransaction;
 
-  //    ObjectID cloneID = cloneTransaction.CreateNewObjectID (source.ID.ClassDefinition);
-  //    DataContainer dataContainerClone = DataContainer.CreateNew (cloneID);
-  //    dataContainerClone.AssumeSamePropertyValues (sourceDataContainer);
-  //    ClearForeignKeys (dataContainerClone);
-  //    cloneTransaction.RegisterNewDataContainer (dataContainerClone);
-  //    return (T) RepositoryAccessor.NewObjectFromDataContainer (dataContainerClone);
-  //  }
+      DataContainer cloneDataContainer = cloneTransaction.CreateNewDataContainer (sourceID.ClassDefinition.ClassType);
+      T clone = (T) RepositoryAccessor.NewObjectFromDataContainer (cloneDataContainer);
 
-  //  private void ClearForeignKeys (DataContainer dataContainer)
-  //  {
-  //    foreach (PropertyValue propertyValue in dataContainer.PropertyValues)
-  //    {
-  //      if (propertyValue.IsRelationProperty)
-  //        propertyValue.TakeOverCommittedData (null); // TODO: stop events!
-  //    }
-  //  }
-  //}
+      IEnumerable<PropertyAccessor> cloneProperties = EnumerableUtility.Where (clone.Properties, 
+          delegate (PropertyAccessor property) { return property.Kind == PropertyKind.PropertyValue; });
+      CopyProperties (cloneProperties, cloneTransaction, source.Properties, sourceTransaction);
+      return clone;
+    }
+
+    private void CopyProperties (IEnumerable<PropertyAccessor> cloneProperties, ClientTransaction cloneTransaction, PropertyIndexer sourceProperties, ClientTransaction sourceTransaction)
+    {
+      foreach (PropertyAccessor cloneProperty in cloneProperties)
+      {
+        PropertyAccessor sourceProperty = sourceProperties[cloneProperty.PropertyIdentifier];
+        object sourceValue = sourceProperty.GetValueWithoutTypeCheckTx (sourceTransaction);
+        cloneProperty.SetValueWithoutTypeCheckTx (cloneTransaction, sourceValue);
+      }
+    }
+  }
 }
