1dd9f908d23e9c491a78361a0ca623ef67228643
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-06-19 11:49:46 +0000

    Removed SmartCloneStrategy, made it a sample in ICloneStrategy's docs
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@10309 cb0deee2-328e-b949-94ea-fb3139fab790

--- Data/DomainObjects/Cloning/ICloneStrategy.cs
+++ Data/DomainObjects/Cloning/ICloneStrategy.cs
@@ -16,6 +16,57 @@ namespace Remotion.Data.DomainObjects.Cloning
   /// <summary>
   /// Provides an interface for classes determining the details about how <see cref="DomainObjectCloner"/> clones a <see cref="DomainObject"/>.
   /// </summary>
+  /// <remarks>
+  /// <para>
+  /// Implement this interface do determine how <see cref="DomainObject"/> graphs are cloned. When <see cref="DomainObjectCloner"/> clones a single
+  /// <see cref="DomainObject"/>, it needs to determine what to do with the objects referenced by that <see cref="DomainObject"/>. This decision
+  /// is delegated to an instance of <see cref="ICloneStrategy"/>, whose <see cref="HandleReference"/> is called. From within <see cref="HandleReference"/>,
+  /// the strategy can call <see cref="CloneContext.GetCloneFor{T}"/> to obtain clones for the referenced objects.
+  /// </para>
+  /// </remarks>
+  /// <example>
+  /// The following is an example of how to implement a cloning strategy that sees 1:n relations as aggregations; therefore, when one of the 
+  /// "n" objects is cloned, the strategy inserts the clone into the original "1" object's collection rather than cloning the "1" object as well.
+  /// <code>
+  /// public class SmartCloneStrategy : ICloneStrategy
+  /// {
+  ///   public void HandleReference (
+  ///       PropertyAccessor sourceReference,
+  ///       ClientTransaction sourceTransaction,
+  ///       PropertyAccessor cloneReference,
+  ///       ClientTransaction cloneTransaction,
+  ///       CloneContext context)
+  ///   {
+  ///     if (sourceReference.Kind == PropertyKind.RelatedObject)
+  ///     {
+  ///       if (sourceReference.RelationEndPointDefinition.RelationDefinition.RelationKind == RelationKindType.OneToMany)
+  ///       {
+  ///       ///    do not clone referenced object, but insert object into original collection
+  ///         DomainObject originalRelated = (DomainObject) sourceReference.GetValueWithoutTypeCheckTx (sourceTransaction);
+  ///         cloneReference.SetValueWithoutTypeCheckTx (cloneTransaction, originalRelated);
+  ///       }
+  ///       else
+  ///       {
+  ///         DomainObject originalRelated = (DomainObject) sourceReference.GetValueWithoutTypeCheckTx (sourceTransaction);
+  ///         DomainObject cloneRelated = originalRelated != null ? context.GetCloneFor (originalRelated) : null;
+  ///         cloneReference.SetValueWithoutTypeCheckTx (cloneTransaction, cloneRelated);
+  ///       }
+  ///     }
+  ///     else
+  ///     {
+  ///       Assertion.IsTrue (sourceReference.Kind == PropertyKind.RelatedObjectCollection);
+  ///       DomainObjectCollection originalRelatedCollection = (DomainObjectCollection) sourceReference.GetValueWithoutTypeCheckTx (sourceTransaction);
+  ///       DomainObjectCollection cloneRelatedCollection = (DomainObjectCollection) cloneReference.GetValueWithoutTypeCheckTx (cloneTransaction);
+  ///       foreach (DomainObject originalRelated in originalRelatedCollection)
+  ///       {
+  ///         DomainObject cloneRelated = context.GetCloneFor (originalRelated);
+  ///         cloneRelatedCollection.Add (cloneRelated);
+  ///       }
+  ///     }
+  ///   }
+  /// }
+  /// </code>
+  /// </example>
   public interface ICloneStrategy
   {
     /// <summary>
