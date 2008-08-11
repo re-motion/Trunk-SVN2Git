1380e948e604894293e4a51759ef6fff2dd4e97c
markus.giegl <markus.giegl@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-08-11 14:10:34 +0000

    To.Text: WIP
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@10898 cb0deee2-328e-b949-94ea-fb3139fab790

--- Core/Core/Text/Diagnostic/To.cs
+++ Core/Core/Text/Diagnostic/To.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Remotion.Text.Diagnostic
 {
@@ -17,22 +19,61 @@ namespace Remotion.Text.Diagnostic
       {
         Name = name;
         Int = i0;
+        ListListString = new List<List<string>>();
       }
 
-      public string Name { get; set; }
+      //public void AddMember (int i, Object o)
+      //{
+      //  if (ListListString.Count < i)
+      //  {
+      //    ListListString
+      //  }
+      //}
+
+      public string Name { get; set; } 
       public int Int { get; set; }
-      public List<List<string>> ListListString { get; private set; }
+      public LinkedList<string> LinkedListString { get; set; }
+      public List<List<string>> ListListString { get; set; }
     }
 
 
 
     public static string Text (object o)
     {
+      // Handle fallback cascade:
+      // *) Is null
+      // *) Type handler registered
+      // *) Implements IToText
+      // *) If !IsInterface: Base type handler registered (recursive)
+      // *) Implements IEnumerable ("is container")
+      // *) If enabled: Log properties through reflection
+      // *) ToString()
+
+      if (o == null)
+      {
+        Log ("null");
+        return "null";
+      }
+
       Delegate handler = null;
-      _typeHandlerMap.TryGetValue (o.GetType(), out handler);
+      Type type = o.GetType();
+
+      Log (type.ToString());
+
+      _typeHandlerMap.TryGetValue (type, out handler);
+
       if (handler != null)
       {
         return (String) handler.DynamicInvoke (o);
+      }
+      else if (type.GetInterface("IEnumerable") != null) 
+      {
+        return o.ToString();
+        //IEnumerable collection = (IEnumerable) o;
+        //foreach (var element in collection)
+        //{
+
+        //}
       }
       else
       {
@@ -48,7 +89,57 @@ namespace Remotion.Text.Diagnostic
       _typeHandlerMap.Add (typeof (T), handler);
     }
 
- 
+    public static void ClearHandlers ()
+    {
+      _typeHandlerMap.Clear ();
+    }
+
+
+    public static void RegisterStandardHandlers ()
+    {
+      RegisterHandler<String> (x => "\"" + x + "\"");
+      RegisterHandler<char> (x => "'" + x + "'");
+    }
+
+
+    //public static string CollectionToText (IEnumerable collection, string start, string seperator, string end)
+    //{
+    //  var sb = new StringBuilder ();
+    //  sb.Append (start);
+    //  bool insertSeperator = false; // no seperator before first element
+    //  foreach (T t in collection)
+    //  {
+    //    if (insertSeperator)
+    //    {
+    //      sb.Append (seperator);
+    //    }
+    //    else
+    //    {
+    //      insertSeperator = true;
+    //    }
+
+    //    //string s = To.Text (t);
+    //    //sb.Append (s);
+    //    sb.Append (To.Text (t));
+    //    //sb.Append (" !!! ");
+    //    //sb.Append (t.ToString());
+
+    //  }
+    //  sb.Append (end);
+    //  return sb.ToString ();
+    //}
+
+
+
+    public static void Log (string s)
+    {
+      Console.WriteLine ("[To]: " + s);
+    }
+
+    public static void LogVariables (string format, params object[] parameterArray)
+    {
+      Log (String.Format (format, parameterArray));
+    }
 
 
   }
--- Core/UnitTests/Text/Diagnostic/ToTest.cs
+++ Core/UnitTests/Text/Diagnostic/ToTest.cs
@@ -2,9 +2,6 @@ using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
-using Remotion.SecurityManager.Domain.AccessControl;
-using Remotion.SecurityManager.UnitTests;
-using Remotion.SecurityManager.UnitTests.Domain.AccessControl;
 using Remotion.Text.Diagnostic;
 
 namespace Remotion.UnitTests.Text.Diagnostic
@@ -40,14 +37,27 @@ namespace Remotion.UnitTests.Text.Diagnostic
       Assert.That (To.Text (t), Is.EqualTo (t.ToString ()));
     }
 
+    private void RegisterHandlers ()
+    {
+      To.RegisterHandler<Object> (x => String.Format ("[Object: {0}]", x.ToString ()));
+      To.RegisterHandler<To.Test> (x => String.Format ("[Test: {0};{1}]", x.Name, x.Int));
+    }
 
+    private void InitTestInstanceContainer (To.Test test)
+    {
+      test.ListListString.Add (new List<string> () { "Aaa","Bbb","Ccc","Ddd"});
+      test.ListListString.Add (new List<string> () { "R1 C0", "R1 C1" });
+      test.ListListString.Add (new List<string> () { "R2 C0", "R2 C1", "R2 C2","R2 C3","R2 C4" });
+      test.ListListString.Add (new List<string> () { "R3 C0", "R3 C1", "R3 C2" });
+    }
 
     [Test]
     public void RegisteredHandlerTest ()
     {
-      To.RegisterHandler<Object> (x => String.Format ("[Object: {0}]", x.ToString()));
+      To.ClearHandlers ();
+      To.RegisterHandler<Object> (x => String.Format ("[Object: {0}]", x.ToString ()));
       Object o = new object ();
-      string toTextO = To.Text (o);
+      string toTextO = To.Text (o); 
       Log ("toTextO=" + toTextO);
       Assert.That (toTextO, Is.EqualTo (String.Format ("[Object: {0}]", o.ToString ())));
 
@@ -58,6 +68,122 @@ namespace Remotion.UnitTests.Text.Diagnostic
       Assert.That (toTextTest, Is.EqualTo ("[Test: That's not my name;179]"));
     }
 
+    [Test]
+    public void NullTest ()
+    {
+      To.ClearHandlers ();
+      Object o = null;
+      Assert.That (To.Text (o), Is.EqualTo ("null"));
+    }
+
+    [Test]
+    public void IntToStringFallbackTest ()
+    {
+      To.ClearHandlers ();
+      int i = 908;
+      Assert.That (To.Text (i), Is.EqualTo ("908"));
+    }
+
+
+    [Test]
+    public void StringHandlerTest ()
+    {
+      To.ClearHandlers ();
+      To.RegisterHandler<String> (x => "\"" + x + "\"");
+      string toTextTest = To.Text ("Some text");
+      Log ("[StringHandlerTest] toTextTest=" + toTextTest);
+      Assert.That (toTextTest, Is.EqualTo ("\"Some text\""));
+    }
+
+    [Test]
+    public void CharHandlerTest ()
+    {
+      To.ClearHandlers ();
+      To.RegisterHandler<char> (x => "'" + x + "'");
+      string toTextTest = To.Text ('x');
+      Log ("[CharHandlerTest] toTextTest=" + toTextTest);
+      Assert.That (toTextTest, Is.EqualTo ("'x'"));
+    }
+
+    [Test]
+    public void InitStandardHandlersTest ()
+    {
+      To.ClearHandlers ();
+      To.RegisterStandardHandlers ();
+      Assert.That (To.Text ("Some text"), Is.EqualTo ("\"Some text\""));
+      Assert.That (To.Text ('x'), Is.EqualTo ("'x'"));
+    }
+
+
+    [Test]
+    public void ClearHandlersTest ()
+    {
+      To.RegisterHandler<Object> (x => "[ClearHandlersTest]" + x);
+      Object o = new object ();
+      string toTextTest = To.Text (o);
+      Log ("[ClearHandlersTest] toTextTest=" + toTextTest);
+      Assert.That (To.Text (o), Is.EqualTo ("[ClearHandlersTest]" + o));
+      To.ClearHandlers();
+      Assert.That (To.Text (o), Is.EqualTo (o.ToString()));
+    }
+
+
+    [Test]
+    public void NullEnumerableTest ()
+    {
+      To.ClearHandlers ();
+      To.RegisterHandler<To.Test> (x => String.Format ("[Test: {0};{1};{2}]", To.Text (x.Name), 
+        To.Text (x.Int), To.Text (x.LinkedListString)));
+      var test = new To.Test ("That's not my name", 179);
+      test.LinkedListString = null;
+      string toTextTest = To.Text (test);
+      const string toTextTestExpected = "[Test: That's not my name;179;null]";
+      Assert.That (toTextTest, Is.EqualTo (toTextTestExpected));
+    }
+
+    //[Test]
+    //public void EmptyEnumerableTest ()
+    //{
+    //  To.ClearHandlers ();
+    //  To.RegisterHandler<To.Test> (x => String.Format ("[Test: {0};{1};{2}]", To.Text (x.Name), To.Text (x.Int), To.Text (x.ListListString)));
+    //  var test = new To.Test ("That's not my name", 179);
+    //  test.LinkedListString = new LinkedList<string> ();
+    //  string toTextTest = To.Text (test);
+    //  const string toTextTestExpected = "[Test: That's not my name;179;{}]";
+    //  Assert.That (toTextTest, Is.EqualTo (toTextTestExpected));
+    //}
+
+    //[Test]
+    //public void OneDimensionalEnumerableTest ()
+    //{
+    //  To.ClearHandlers ();
+    //  To.RegisterHandler<To.Test> (x => String.Format ("[Test: {0};{1};{2}]", To.Text (x.Name), To.Text (x.Int), To.Text (x.ListListString)));
+    //  var test = new To.Test ("That's not my name", 179);
+    //  string[] linkedListInit = { "A1", "A2", "A3", "A4", "A5" };
+    //  test.LinkedListString = new LinkedList<string> (linkedListInit);
+    //  string toTextTest = To.Text (test);
+    //  const string toTextTestExpected = "[Test: That's not my name;179;{A1,A2,A3,A4,A5}]";
+    //  Assert.That (toTextTest, Is.EqualTo (toTextTestExpected));
+    //}
+
+
+
+    //[Test]
+    //public void OneDimensionalEnumerableTest ()
+    //{
+    //  To.ClearHandlers ();
+    //  To.RegisterHandler<To.Test> (x => String.Format ("[Test: {0};{1};{2}]", To.Text (x.Name), To.Text (x.Int), To.Text(x.ListListString)));
+    //  var test = new To.Test ("That's not my name", 179);
+    //  //InitTestInstanceContainer (test);
+    //  //test.ListListString = new List<List<string>>() { new List<string>(){"A1","A2"} , new List<string>(){"B1","B2","B3" } };
+    //  test.ListString = new List<string> () { "A1", "A2", "A3","A4" ,"A5"};
+    //  //test.ListListString.Add (new List<string> () { "A1", "A2", "Ccc", "Ddd" });
+    //  string toTextTest = To.Text (test);
+    //  Log ("toTextTest=" + toTextTest);
+    //  //Assert.That (toTextTest, Is.EqualTo ("[Test: That's not my name;179;{{A1,A2},{B1,B2,B3}}]"));
+    //  Assert.That (toTextTest, Is.EqualTo ("[Test: That's not my name;179;{A1,A2,A3,A4,A5}]"));
+    //}
+
     public static void Log (string s)
     {
       Console.WriteLine (s);
@@ -65,7 +191,7 @@ namespace Remotion.UnitTests.Text.Diagnostic
 
     public static void LogVariables (string format, params object[] parameterArray)
     {
-      Console.WriteLine (String.Format(format),parameterArray);
+      Log (String.Format (format, parameterArray));
     }
 
   }
