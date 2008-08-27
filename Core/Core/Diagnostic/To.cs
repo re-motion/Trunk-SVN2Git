35305b09a10bff6bcd9c7f1868e62e48a8e8ec0c
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-08-27 07:29:39 +0000

    Fixed compiler error.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@11056 cb0deee2-328e-b949-94ea-fb3139fab790

--- Core/Core/Diagnostic/To.cs
+++ Core/Core/Diagnostic/To.cs
@@ -1,17 +1,10 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using Remotion.Logging;
 
 namespace Remotion.Diagnostic
 {
   public static class To
   {
-    private static readonly SimpleLogger log = new SimpleLogger (true);
     private static readonly ToTextProvider toTextProvider = new ToTextProvider();
-
-    //private static To() 
 
     public static string Text (object obj)
     {
@@ -21,12 +14,12 @@ namespace Remotion.Diagnostic
 
     public static void RegisterHandler<T> (Action<T, ToTextBuilder> handler)
     {
-      toTextProvider.RegisterHandler<T>(handler); 
+      toTextProvider.RegisterHandler<T> (handler);
     }
 
     public static void ClearHandlers ()
     {
-      toTextProvider.ClearHandlers ();
+      toTextProvider.ClearHandlers();
     }
 
 
@@ -36,18 +29,5 @@ namespace Remotion.Diagnostic
       toTextProvider.UseAutomaticStringEnclosing = enable;
       toTextProvider.UseAutomaticCharEnclosing = enable;
     }
-
-    private static void Log (string s)
-    {
-      //Console.WriteLine ("[To]: " + s);
-      log.It("[To]: " + s);
-    }
-
-    private static void LogVariables (string format, params object[] parameterArray)
-    {
-      Log (String.Format (format, parameterArray));
-    }
-
-
   }
 }
