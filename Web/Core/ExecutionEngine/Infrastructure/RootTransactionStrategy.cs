7eda27550038e21866f1cc3c9fc237038335bb71
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-09-30 12:27:04 +0000

    temp commit (RootTransactionStrategy implementation)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@11426 cb0deee2-328e-b949-94ea-fb3139fab790

--- Web/Core/ExecutionEngine/Infrastructure/RootTransactionStrategy.cs
+++ Web/Core/ExecutionEngine/Infrastructure/RootTransactionStrategy.cs
@@ -18,52 +18,95 @@ namespace Remotion.Web.ExecutionEngine.Infrastructure
   public class RootTransactionStrategy : TransactionStrategyBase
   {
     private readonly ITransactionScopeManager _scopeManager;
+    private readonly ITransaction _transaction;
+    private ITransactionScope _scope;
 
     public RootTransactionStrategy (bool autoCommit, IWxeFunctionExecutionListener innerListener, ITransactionScopeManager scopeManager)
         : base (autoCommit, innerListener)
     {
       ArgumentUtility.CheckNotNull ("_scopeManager", scopeManager);
+
       _scopeManager = scopeManager;
+      _transaction = scopeManager.CreateRootTransaction();
     }
 
     public override void OnExecutionPlay (WxeContext context)
     {
-      throw new System.NotImplementedException();
+      if (_scope != null)
+      {
+        throw new InvalidOperationException (
+            "OnExecutionPlay may not be invoked twice without calling OnExecutionStop, OnExecutionPause, or OnExecutionFail in-between.");
+      }
+
+      ExecuteAndWrapInnerException (delegate { _scope = _transaction.EnterScope(); }, null);
+
+      InnerListener.OnExecutionPlay (context);
     }
 
     public override void OnExecutionStop (WxeContext context)
     {
-      throw new System.NotImplementedException();
+      if (_scope == null)
+        throw new InvalidOperationException ("OnExecutionStop may not be invoked unless OnExecutionPlay was called first.");
+
+      try
+      {
+        InnerListener.OnExecutionStop (context);
+        if (AutoCommit)
+          _transaction.Commit ();
+      }
+      finally
+      {
+        ExecuteAndWrapInnerException (_scope.Leave, null);
+        ExecuteAndWrapInnerException (_transaction.Release, null);
+        _scope = null;
+      }
     }
 
     public override void OnExecutionPause (WxeContext context)
     {
-      throw new System.NotImplementedException();
+      if (_scope == null)
+        throw new InvalidOperationException ("OnExecutionPause may not be invoked unless OnExecutionPlay was called first.");
+
+      Exception innerListenerException = null;
+      try
+      {
+        InnerListener.OnExecutionPause (context);
+      }
+      catch (Exception e)
+      {
+        innerListenerException = e;
+        throw;
+      }
+      finally
+      {
+        ExecuteAndWrapInnerException (_scope.Leave, innerListenerException);
+        _scope = null;
+      }
     }
 
     public override void OnExecutionFail (WxeContext context, Exception exception)
     {
-      throw new System.NotImplementedException();
+      throw new NotImplementedException();
     }
 
     public override ITransaction Transaction
     {
-      get { throw new System.NotImplementedException(); }
+      get { return _transaction; }
     }
 
     public override void Commit ()
     {
-      throw new System.NotImplementedException();
+      throw new NotImplementedException();
     }
 
     public override void Rollback ()
     {
-      throw new System.NotImplementedException();
+      throw new NotImplementedException();
     }
 
     public override void Reset ()
     {
-      throw new System.NotImplementedException ();
+      throw new NotImplementedException();
     }
 
     public override bool IsNull
@@ -74,6 +117,26 @@ namespace Remotion.Web.ExecutionEngine.Infrastructure
     public ITransactionScopeManager ScopeManager
     {
       get { return _scopeManager; }
+    }
+
+    public ITransactionScope Scope
+    {
+      get { return _scope; }
+    }
+
+    private void ExecuteAndWrapInnerException (Action action, Exception existingInnerException)
+    {
+      try
+      {
+        action ();
+      }
+      catch (Exception e)
+      {
+        if (existingInnerException == null)
+          throw new WxeFatalExecutionException (e, null);
+        else
+          throw new WxeFatalExecutionException (existingInnerException, e);
+      }
     }
   }
 }
