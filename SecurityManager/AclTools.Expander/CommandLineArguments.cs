0f24040ebaffec38ffabdc008179ce9d9d62bf59
markus.giegl <markus.giegl@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-10-07 13:30:52 +0000

    AclExpander: Command line tool project build
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@11519 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ SecurityManager/AclTools.Expander/CommandLineArguments.cs
@@ -0,0 +1,40 @@
+/* Copyright (C) 2005 - 2008 rubicon informationstechnologie gmbh
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the re:motion license agreement in license.txt. If you did not 
+ * receive it, please visit http://www.re-motion.org/licensing.
+ * 
+ * Unless otherwise provided, this software is distributed on an "AS IS" basis, 
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+ */
+
+using System;
+using Remotion.Text.CommandLine;
+
+namespace Remotion.SecurityManager.AclTools.Expander
+{
+  public class CommandLineArguments
+  {
+    [CommandLineModeArgument (false)]
+    public OperationMode Mode;
+
+    [CommandLineStringArgument (false,
+        Description = "The name of the XML metadata file.",
+        Placeholder = "metadata")]
+    public string MetadataFile = string.Empty;
+
+    [CommandLineFlagArgument ("verbose", false,
+        Description = "Verbose output")]
+    public bool Verbose;
+
+    public bool ImportMetadata
+    {
+      get { return (Mode & OperationMode.Metadata) != 0; }
+    }
+
+    public bool ImportLocalization
+    {
+      get { return (Mode & OperationMode.Localization) != 0; }
+    }
+  }
+}
---
+++ SecurityManager/AclTools.Expander/OperationMode.cs
@@ -0,0 +1,28 @@
+/* Copyright (C) 2005 - 2008 rubicon informationstechnologie gmbh
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the re:motion license agreement in license.txt. If you did not 
+ * receive it, please visit http://www.re-motion.org/licensing.
+ * 
+ * Unless otherwise provided, this software is distributed on an "AS IS" basis, 
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. 
+ */
+
+using System;
+using Remotion.Text.CommandLine;
+
+namespace Remotion.SecurityManager.AclTools.Expander
+{
+  [Flags]
+  public enum OperationMode
+  {
+    [CommandLineMode ("metadata", Description = "Import security metadata.")]
+    Metadata = 1,
+
+    [CommandLineMode ("localization", Description = "Import metadata localization (Metadata must have been imported).")]
+    Localization = 2,
+
+    [CommandLineMode ("all", Description = "Import security metadata and localization.")]
+    All = Metadata | Localization
+  }
+}
