a6737654206726418ebb7ff48fbcc1b80535475f
stephan.zaubzer <stephan.zaubzer@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-10-14 07:38:21 +0000

    Fixed unit test (culture)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@11583 cb0deee2-328e-b949-94ea-fb3139fab790

--- Core/UnitTests/Utilities/EnumDescriptionTest.cs
+++ Core/UnitTests/Utilities/EnumDescriptionTest.cs
@@ -12,6 +12,7 @@ using System;
 using System.Globalization;
 using System.Threading;
 using NUnit.Framework;
+using Remotion.Development.UnitTesting;
 using Remotion.Utilities;
 
 namespace Remotion.UnitTests.Utilities
@@ -38,68 +39,74 @@ public enum EnumFromResource
 [TestFixture]
 public class EnumDescriptionTest
 {
-  [SetUp]
-  public void SetUp ()
-  {
-    Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
-  }
-
   [Test]
-  public void TestGetDescriptionForEnumWithDescriptions()
+  public void TestGetDescriptionForEnumWithDescriptions ()
   {
-    // try twice to test caching
-    for (int i = 0; i < 2; ++i)
+    using (new CultureScope (CultureInfo.InvariantCulture, CultureInfo.InvariantCulture))
     {
-      Assert.AreEqual ("Value One", EnumDescription.GetDescription (EnumWithDescriptions.Value1));
-      Assert.AreEqual ("Value 2", EnumDescription.GetDescription (EnumWithDescriptions.Value2));
-      Assert.AreEqual ("Value III", EnumDescription.GetDescription (EnumWithDescriptions.Value3));
+      // try twice to test caching
+      for (int i = 0; i < 2; ++i)
+      {
+        Assert.AreEqual ("Value One", EnumDescription.GetDescription (EnumWithDescriptions.Value1));
+        Assert.AreEqual ("Value 2", EnumDescription.GetDescription (EnumWithDescriptions.Value2));
+        Assert.AreEqual ("Value III", EnumDescription.GetDescription (EnumWithDescriptions.Value3));
+      }
     }
   }
 
   [Test]
   public void TestGetAvailableValuesForEnumWithDescriptions()
   {
-    // try twice to test caching
-    for (int i = 0; i < 2; ++i)
+    using (new CultureScope (CultureInfo.InvariantCulture, CultureInfo.InvariantCulture))
     {
-      EnumValue[] enumValues = EnumDescription.GetAllValues (typeof (EnumWithDescriptions));
-      Assert.AreEqual (3, enumValues.Length);
-      Assert.AreEqual (EnumWithDescriptions.Value1, enumValues[0].Value);
-      Assert.AreEqual ("Value One",                 enumValues[0].Description);
-      Assert.AreEqual (EnumWithDescriptions.Value2, enumValues[1].Value);
-      Assert.AreEqual ("Value 2",                   enumValues[1].Description);
-      Assert.AreEqual (EnumWithDescriptions.Value3, enumValues[2].Value);
-      Assert.AreEqual ("Value III",                 enumValues[2].Description);
+      // try twice to test caching
+      for (int i = 0; i < 2; ++i)
+      {
+        EnumValue[] enumValues = EnumDescription.GetAllValues (typeof (EnumWithDescriptions));
+        Assert.AreEqual (3, enumValues.Length);
+        Assert.AreEqual (EnumWithDescriptions.Value1, enumValues[0].Value);
+        Assert.AreEqual ("Value One", enumValues[0].Description);
+        Assert.AreEqual (EnumWithDescriptions.Value2, enumValues[1].Value);
+        Assert.AreEqual ("Value 2", enumValues[1].Description);
+        Assert.AreEqual (EnumWithDescriptions.Value3, enumValues[2].Value);
+        Assert.AreEqual ("Value III", enumValues[2].Description);
+      }
     }
   }
 
   [Test]
   public void TestGetDescriptionForEnumFromResource()
   {
-    Assert.AreEqual ("Wert Eins", EnumDescription.GetDescription (EnumFromResource.Value1));
-    Assert.AreEqual ("Wert 2", EnumDescription.GetDescription (EnumFromResource.Value2));
-    Assert.AreEqual ("Wert III", EnumDescription.GetDescription (EnumFromResource.Value3));
+    using (new CultureScope (CultureInfo.InvariantCulture, CultureInfo.InvariantCulture))
+    {
+      Assert.AreEqual ("Wert Eins", EnumDescription.GetDescription (EnumFromResource.Value1));
+      Assert.AreEqual ("Wert 2", EnumDescription.GetDescription (EnumFromResource.Value2));
+      Assert.AreEqual ("Wert III", EnumDescription.GetDescription (EnumFromResource.Value3));
 
-    CultureInfo culture = new CultureInfo ("en-US");
-    Assert.AreEqual ("Val1", EnumDescription.GetDescription (EnumFromResource.Value1, culture));
-    Assert.AreEqual ("Val2", EnumDescription.GetDescription (EnumFromResource.Value2, culture));
-    Assert.AreEqual ("Val3", EnumDescription.GetDescription (EnumFromResource.Value3, culture));
+      CultureInfo culture = new CultureInfo ("en-US");
+      Assert.AreEqual ("Val1", EnumDescription.GetDescription (EnumFromResource.Value1, culture));
+      Assert.AreEqual ("Val2", EnumDescription.GetDescription (EnumFromResource.Value2, culture));
+      Assert.AreEqual ("Val3", EnumDescription.GetDescription (EnumFromResource.Value3, culture));
+    }
   }
 
   [Test]
   public void TestGetAvailableValuesForEnumFromResource ()
   {
-    // try twice to test caching
-    for (int i = 0; i < 2; ++i)
+    using (new CultureScope (CultureInfo.InvariantCulture, CultureInfo.InvariantCulture))
     {
-      EnumValue[] enumValues = EnumDescription.GetAllValues (typeof (EnumFromResource));
-      Assert.AreEqual (3, enumValues.Length);
-      Assert.AreEqual (EnumFromResource.Value1, enumValues[0].Value);
-      Assert.AreEqual ("Wert Eins",             enumValues[0].Description);
-      Assert.AreEqual (EnumFromResource.Value2, enumValues[1].Value);
-      Assert.AreEqual ("Wert 2",                enumValues[1].Description);
-      Assert.AreEqual (EnumFromResource.Value3, enumValues[2].Value);
-      Assert.AreEqual ("Wert III",              enumValues[2].Description);
+      // try twice to test caching
+      for (int i = 0; i < 2; ++i)
+      {
+        EnumValue[] enumValues = EnumDescription.GetAllValues (typeof (EnumFromResource));
+        Assert.AreEqual (3, enumValues.Length);
+        Assert.AreEqual (EnumFromResource.Value1, enumValues[0].Value);
+        Assert.AreEqual ("Wert Eins", enumValues[0].Description);
+        Assert.AreEqual (EnumFromResource.Value2, enumValues[1].Value);
+        Assert.AreEqual ("Wert 2", enumValues[1].Description);
+        Assert.AreEqual (EnumFromResource.Value3, enumValues[2].Value);
+        Assert.AreEqual ("Wert III", enumValues[2].Description);
+      }
     }
   }
 }
