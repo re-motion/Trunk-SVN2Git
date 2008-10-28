fd269527a32a987767ee1db025f2124cc058b04a
reinhard.gantar <reinhard.gantar@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-10-28 13:08:22 +0000

    SearchAvailableObjects.cs hack
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@11683 cb0deee2-328e-b949-94ea-fb3139fab790

--- ObjectBinding/Web.CodeGenerator/Templates/TabbedEditor/WebClient/Global.asax.cs
+++ ObjectBinding/Web.CodeGenerator/Templates/TabbedEditor/WebClient/Global.asax.cs
@@ -3,17 +3,27 @@ using Remotion.ObjectBinding.BindableObject;
 using Remotion.Data.DomainObjects.ObjectBinding;
 using Remotion.ObjectBinding;
 
-namespace $PROJECT_ROOTNAMESPACE$
+// ATTENTION! This global.asax.cs file is an interim version
+// and supposed to be
+// used with Remotion 1.11.5.x. It contains a hack for making
+// the supplementary code in SearchAllObjectsService.cs work.
+// This code is part of the domain, so a "using <YOUR DOMAIN HERE>"
+// must be provided: 
+using $DOMAIN_ROOTNAMESPACE$;
+
+namespace PhoneBook.Web
 {
   public class Global : System.Web.HttpApplication
   {
     protected void Application_Start (object sender, EventArgs e)
     {
       BindableObjectProvider.GetProvider<BindableDomainObjectProviderAttribute>().AddService (
-          typeof (BindableDomainObjectSearchService), new BindableDomainObjectSearchService ());
-      BindableObjectProvider.GetProvider<BindableDomainObjectProviderAttribute>().AddService (
           typeof (BindableDomainObjectGetObjectService), new BindableDomainObjectGetObjectService ());
 
+      // This statement registers the code with the BindableObjectProvider. ISearchAvailableObjectService
+      // is implemented in the aforementioned SearchAllObjectsService.cs.
+      BindableObjectProvider.GetProvider<BindableDomainObjectProviderAttribute>().AddService (
+          typeof (ISearchAvailableObjectsService), new SearchAllObjectsService());
     }
 
     protected void Application_End (object sender, EventArgs e)
