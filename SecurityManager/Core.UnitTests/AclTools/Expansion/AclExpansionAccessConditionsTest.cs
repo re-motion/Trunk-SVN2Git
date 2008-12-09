668eaa864c261dc0dd2e7c56359e399697186aed
markus.giegl <markus.giegl@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-12-09 14:42:31 +0000

    AclExpander: code review comments (FS) processing (WIP). AclExpansionAccessConditionsTest done.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@12091 cb0deee2-328e-b949-94ea-fb3139fab790

--- SecurityManager/Core.UnitTests/AclTools/Expansion/AclExpansionAccessConditionsTest.cs
+++ SecurityManager/Core.UnitTests/AclTools/Expansion/AclExpansionAccessConditionsTest.cs
@@ -26,7 +26,6 @@ using NUnitText = NUnit.Framework.SyntaxHelpers.Text;
 
 namespace Remotion.SecurityManager.UnitTests.AclTools.Expansion
 {
-  // TODO AE: Remove commented code. (Do not commit.)
   [TestFixture]
   public class AclExpansionAccessConditionsTest : AclToolsTestBase
   {
@@ -44,38 +43,12 @@ namespace Remotion.SecurityManager.UnitTests.AclTools.Expansion
       Assert.That (accessConditions.GroupHierarchyCondition, Is.EqualTo (GroupHierarchyCondition.Undefined));
     }
 
-    // TODO AE: Typical pattern:
-    // TODO AE: Equals_True => create two with equal members, assert they are equal
-    // TODO AE: Equals_False_Prop1, Equals_False_Prop2, Equals_False_PropN => create two with equal members but one, assert they are not equal
-    // TODO AE: GetHashCode_Equal => create two with equal members, assert hash codes are equal
-    // TODO AE: GetHashCode_NotEqual should not be tested, too volatile
 
-    [Test]
-    public void Equals ()
-    {
-      //BooleanMemberTest ((aeac, b) => aeac.HasOwningGroupCondition = b);
-      //BooleanMemberTest ((aeac, b) => aeac.HasOwningTenantCondition = b);
-      BooleanMemberTest ((aeac, b) => aeac.IsOwningUserRequired = b); // TODO AE: Inline this member, remove method.
-    }
-
-    //[Test]
-    //public void Equals_UsingLambda ()
-    //{
-    //  //CheckIfPassedPropertyChangeChangesEquality<AclExpansionAccessConditions> (aeac => aeac.HasOwningGroupCondition = true);
-    //  CheckIfPassedPropertyChangeChangesEquality<AclExpansionAccessConditions> (aeac => aeac.HasOwningTenantCondition = true);
-    //  CheckIfPassedPropertyChangeChangesEquality<AclExpansionAccessConditions> (aeac => aeac.IsOwningUserRequired = true);
-    //  CheckIfPassedPropertyChangeChangesEquality<AclExpansionAccessConditions> (aeac => aeac.AbstractRole = TestHelper.CreateAbstractRoleDefinition ("titatutest", 11235));
-    //  CheckIfPassedPropertyChangeChangesEquality<AclExpansionAccessConditions> (aeac => aeac.OwningGroup = Group2);
-    //}
-
-
-    // Check Equals operator for each property of AclExpansionAccessConditions if changing the property from its default value 
-    // leaves to two instances being unequal. // TODO AE: leads to two or leaves two? :)
+    // Check Equals operator for each property of AclExpansionAccessConditions whether changing the property from its default value 
+    // leads to inequality. 
     [Test]
     public void Equals_UsingPropertyObject ()
     {
-      //CheckIfPassedPropertyChangeChangesEquality (Properties<AclExpansionAccessConditions>.Get (aeac => aeac.HasOwningGroupCondition), true);
-      //CheckIfPassedPropertyChangeChangesEquality (Properties<AclExpansionAccessConditions>.Get (aeac => aeac.HasOwningTenantCondition), true);
       CheckIfPassedPropertyChangeChangesEquality (Properties<AclExpansionAccessConditions>.Get (aeac => aeac.IsOwningUserRequired), true);
       CheckIfPassedPropertyChangeChangesEquality (Properties<AclExpansionAccessConditions>.Get (aeac => aeac.AbstractRole), TestHelper.CreateAbstractRoleDefinition ("titatutest", 11235));
       CheckIfPassedPropertyChangeChangesEquality (Properties<AclExpansionAccessConditions>.Get (aeac => aeac.OwningGroup), Group3);
@@ -90,7 +63,6 @@ namespace Remotion.SecurityManager.UnitTests.AclTools.Expansion
     {
       var accessConditions = new AclExpansionAccessConditions ();
       var result = To.String.e (accessConditions).CheckAndConvertToString ();
-      //To.Console.s (result);
       Assert.That (result, Is.EqualTo ("[]"));
     }
 
@@ -99,42 +71,18 @@ namespace Remotion.SecurityManager.UnitTests.AclTools.Expansion
     {
       var accessConditions = new AclExpansionAccessConditions ();
       accessConditions.AbstractRole = TestHelper.CreateAbstractRoleDefinition ("xyz", 123);
-      //accessConditions.HasOwningTenantCondition = true;
       accessConditions.IsOwningUserRequired = true;
       accessConditions.OwningGroup = Group2;
       accessConditions.GroupHierarchyCondition = GroupHierarchyCondition.ThisAndParentAndChildren;
       accessConditions.OwningTenant = Tenant;
       accessConditions.TenantHierarchyCondition = TenantHierarchyCondition.ThisAndParent;
       var result = To.String.e (accessConditions).CheckAndConvertToString ();
-      // To.Console.s (result);
       Assert.That (result, Is.EqualTo (@"[userMustOwn=True,owningGroup=[""Anotha Group""],groupHierarchyCondition=ThisAndParentAndChildren,tenantMustOwn=True,abstractRoleMustMatch=True,abstractRole=[""xyz""]]"));
     }
 
 
 
-    private void BooleanMemberTest (Action<AclExpansionAccessConditions, bool> setBoolProperty)
-    {
-      var accessConditions0 = new AclExpansionAccessConditions ();
-      var accessConditions1 = new AclExpansionAccessConditions ();
-      Assert.That (accessConditions0.Equals (accessConditions1), Is.True);
-      setBoolProperty (accessConditions1, true);
-      Assert.That (accessConditions0.Equals (accessConditions1), Is.False);
-    }
-
-
-    // Check if applying the passed Action to only one instance of AclExpansionAccessConditions flips equality.
-    // TODO AE: Remove unused method.
-    private void CheckIfPassedPropertyChangeChangesEquality<TProperty> (Action<AclExpansionAccessConditions> changeProperty)
-    {
-      var accessConditions0 = new AclExpansionAccessConditions ();
-      var accessConditions1 = new AclExpansionAccessConditions ();
-      Assert.That (accessConditions0.Equals (accessConditions1), Is.True);
-      changeProperty(accessConditions1);
-      Assert.That (accessConditions0.Equals (accessConditions1), Is.False);
-    }
-
-
-    // Check if changing the passed Property of AclExpansionAccessConditions in only one instance flips equality. // TODO AE: Seems obvious, given the method name.
+    // Check if changing the passed Property of AclExpansionAccessConditions in only one instance flips equality. 
     private void CheckIfPassedPropertyChangeChangesEquality<TProperty> (Property<AclExpansionAccessConditions, TProperty> boolProperty, TProperty notEqualValue)
     {
       var accessConditions0 = new AclExpansionAccessConditions ();
@@ -143,7 +91,7 @@ namespace Remotion.SecurityManager.UnitTests.AclTools.Expansion
       Assert.That (accessConditions0.GetHashCode (), Is.EqualTo (accessConditions1.GetHashCode ()));
       boolProperty.Set (accessConditions1, notEqualValue);
       Assert.That (accessConditions0.Equals (accessConditions1), Is.False);
-      Assert.That (accessConditions0.GetHashCode (), Is.Not.EqualTo (accessConditions1.GetHashCode ()));
+      // Note: We do not assert here that GetHashCode() returns a different result, since this cannot be guaranteed.
     }
 
 
