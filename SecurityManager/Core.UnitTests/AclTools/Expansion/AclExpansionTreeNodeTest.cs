b88b5e7620625316b88f70f8b84f4db630a68fec
markus.giegl <markus.giegl@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-12-15 17:39:29 +0000

    AclExpander: code review comments (FS) processing (WIP). AclExpansionTreeNodeTest ToTextTest.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@12151 cb0deee2-328e-b949-94ea-fb3139fab790

--- SecurityManager/Core.UnitTests/AclTools/Expansion/AclExpansionTreeNodeTest.cs
+++ SecurityManager/Core.UnitTests/AclTools/Expansion/AclExpansionTreeNodeTest.cs
@@ -20,8 +20,10 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 using Remotion.Development.UnitTesting.ObjectMother;
+using Remotion.Diagnostics.ToText;
 using Remotion.SecurityManager.AclTools.Expansion.Infrastructure;
 using Remotion.SecurityManager.Domain.OrganizationalStructure;
+ 
 
 namespace Remotion.SecurityManager.UnitTests.AclTools.Expansion
 {
@@ -46,6 +48,22 @@ namespace Remotion.SecurityManager.UnitTests.AclTools.Expansion
       Assert.That (aclExpansionTreeNode.Key, Is.EqualTo (User));
       Assert.That (aclExpansionTreeNode.NumberLeafNodes, Is.EqualTo (17));
       Assert.That (aclExpansionTreeNode.Children, Is.EqualTo (children));
+    }
+
+
+    [Test]
+    public void ToTextTest ()
+    {
+      List<Role> children = ListMother.New (Role, Role2, Role3);
+      var aclExpansionTreeNode = AclExpansionTreeNode.New (User, 17, children);
+      var result = To.String.e (aclExpansionTreeNode).ToString ();
+      var resultExpected =
+      #region
+ @"
+([""DaUs""],17,
+{[""DaUs"",""Da Group"",""Supreme Being""],[""mr.smith"",""Anotha Group"",""Working Drone""],[""ryan_james"",""Da 3rd Group"",""Combatant""]})";
+      #endregion
+      Assert.That (result, Is.EqualTo (resultExpected));
     }
   }
 }
