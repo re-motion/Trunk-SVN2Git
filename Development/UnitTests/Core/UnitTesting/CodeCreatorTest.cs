c1238cec5a01e2bae7fdac0d1771b1a73915d03b
markus.giegl <markus.giegl@cb0deee2-328e-b949-94ea-fb3139fab790>
2008-12-18 12:24:50 +0000

    CodeCreator / CodeCreatorTest header.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@12190 cb0deee2-328e-b949-94ea-fb3139fab790

--- Development/UnitTests/Core/UnitTesting/CodeCreatorTest.cs
+++ Development/UnitTests/Core/UnitTesting/CodeCreatorTest.cs
@@ -1,18 +1,22 @@
-/* Copyright (C) 2005 - 2008 rubicon informationstechnologie gmbh
- *
- * This program is free software: you can redistribute it and/or modify it under 
- * the terms of the re:motion license agreement in license.txt. If you did not 
- * receive it, please visit http://www.re-motion.org/licensing.
- * 
- * Unless otherwise provided, this software is distributed on an "AS IS" basis, 
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. 
- */
-
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2008 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
 using System;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 using Remotion.Development.UnitTesting;
-using Remotion.Diagnostics.ToText;
 
 namespace Remotion.Development.UnitTests.Core.UnitTesting
 {
@@ -23,7 +27,6 @@ namespace Remotion.Development.UnitTests.Core.UnitTesting
     public void CreateResultExpectedCodeTest ()
     {
       var result = CodeCreator.CreateResultExpectedCode ("heinz");
-      To.ConsoleLine.e (result);
 
       const string resultExpected =
       #region
