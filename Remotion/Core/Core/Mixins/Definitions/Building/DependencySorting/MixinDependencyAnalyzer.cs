5507c33626cf706b8afb4ee522cb6d74718aa770
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-03-25 10:29:01 +0000

    Performed some minor refactorings
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@12536 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Core/Core/Mixins/Definitions/Building/DependencySorting/MixinDependencyAnalyzer.cs
+++ Remotion/Core/Core/Mixins/Definitions/Building/DependencySorting/MixinDependencyAnalyzer.cs
@@ -18,7 +18,7 @@ using System.Collections.Generic;
 using Remotion.Collections;
 using Remotion.Mixins.Utilities.DependencySort;
 using Remotion.Text;
-using Remotion.Utilities;
+using System.Linq;
 
 namespace Remotion.Mixins.Definitions.Building.DependencySorting
 {
@@ -62,7 +62,7 @@ namespace Remotion.Mixins.Definitions.Building.DependencySorting
 
     private List<Tuple<string, MixinDefinition>> GetMixinsByTypeName (IEnumerable<MixinDefinition> equalRoots)
     {
-      List<Tuple<string, MixinDefinition>> mixinsByTypeName = new List<Tuple<string, MixinDefinition>>();
+      var mixinsByTypeName = new List<Tuple<string, MixinDefinition>>();
       int nonAlphabetics = 0;
       foreach (MixinDefinition mixin in equalRoots)
       {
@@ -79,28 +79,19 @@ namespace Remotion.Mixins.Definitions.Building.DependencySorting
 
     public void SortAlphabetically (List<Tuple<string, MixinDefinition>> mixinsByTypeName)
     {
-      mixinsByTypeName.Sort (
-          delegate (Tuple<string, MixinDefinition> t1, Tuple<string, MixinDefinition> t2)
-          {
-            return StringComparer.Ordinal.Compare(t1.A, t2.A);
-          });
+      mixinsByTypeName.Sort ((t1, t2) => StringComparer.Ordinal.Compare (t1.A, t2.A));
     }
 
     private Exception CreateCannotResolveException (IEnumerable<MixinDefinition> equalRoots)
     {
-      MixinDefinition first;
-      using (IEnumerator<MixinDefinition> equalRootsEnumerator = equalRoots.GetEnumerator())
-      {
-        bool hasFirst = equalRootsEnumerator.MoveNext ();
-        Assertion.IsTrue (hasFirst);
-        first = equalRootsEnumerator.Current;
-      }
-
-      string message = string.Format ("The following mixins are applied to the same base class {0} and require a clear base call ordering, but do not "
-          + "provide enough dependency information: {1}.{2}Please supply additional dependencies to the mixin definitions, use the "
-          + "AcceptsAlphabeticOrderingAttribute, or adjust the mixin configuration accordingly.", first.TargetClass.FullName,
-          SeparatedStringBuilder.Build (", ", equalRoots, delegate (MixinDefinition m) { return m.FullName; }),
-          Environment.NewLine);
+      string message =
+          string.Format (
+              "The following mixins are applied to the same base class {0} and require a clear base call ordering, but do not "
+              + "provide enough dependency information: {1}.{2}Please supply additional dependencies to the mixin definitions, use the "
+              + "AcceptsAlphabeticOrderingAttribute, or adjust the mixin configuration accordingly.",
+              equalRoots.First().TargetClass.FullName,
+              SeparatedStringBuilder.Build (", ", equalRoots, m => m.FullName),
+              Environment.NewLine);
       return new ConfigurationException (message);
     }
   }
