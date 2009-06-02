3aebd554b0e243dea617d775facaf1d9255c3b93
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-06-02 14:07:57 +0000

    Updated AssemblyInfo.cs.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@12892 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/DomainObjects.RdbmsTools.UnitTests/Properties/AssemblyInfo.cs
+++ Remotion/Data/DomainObjects.RdbmsTools.UnitTests/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using Remotion.Data.DomainObjects;
 [assembly: AssemblyConfiguration (".NET Framework: net-3.5, build type: debug")]
 [assembly: AssemblyCompany("rubicon informationstechnologie gmbh")]
 [assembly: AssemblyProduct("re-motion core framework (www.re-motion.org)")]
-[assembly: AssemblyCopyright("(c) 2005 - 2008 rubicon informationstechnologie gmbh, www.rubicon.eu")]
+[assembly: AssemblyCopyright("(c) 2005 - 2009 rubicon informationstechnologie gmbh, www.rubicon.eu")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 [assembly: CLSCompliant(true)]
@@ -45,4 +45,4 @@ using Remotion.Data.DomainObjects;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.11.19.2")]
+[assembly: AssemblyVersion("1.13.9.2")]
