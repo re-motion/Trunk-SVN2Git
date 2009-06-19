fbbe2f91ca429e0e20edde849b7acc8096ebf77f
markus.giegl <markus.giegl@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-06-19 14:59:08 +0000

    Remotion.Scripting - ScriptContext skeleton
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@13202 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Core/Scripting.UnitTests/ScriptContextTest.cs
@@ -0,0 +1,15 @@
+// Copyright (C) 2005 - 2009 rubicon informationstechnologie gmbh
+// All rights reserved.
+//
+using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace Remotion.Scripting.UnitTests
+{
+  [TestFixture]
+  public class ScriptContextTest
+  {
+    
+  }
+}
