00b838c99c47f0f1f35559d103ba25ad5059385f
markus.giegl <markus.giegl@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-06-19 09:06:31 +0000

    Fixed buildall.cmd: Calls Build-RemotionLocalDoc.cmd and emits explanatory message if no keyfile is being found.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@13183 cb0deee2-328e-b949-94ea-fb3139fab790

--- buildall.cmd
+++ buildall.cmd
@@ -1,15 +1,17 @@
-rem nant incversion
-nant clean resources
-      if errorlevel 1 pause "build error"
-nant setdebug all alltests
-      if errorlevel 1 pause "build error"
-nant setrelease all alltests
-      if errorlevel 1 pause "build error"
+@echo off
 
-if "%1"=="nodoc" goto nodoc  
-nant doc-public
-      if errorlevel 1 pause "build error"
-:nodoc
+if not exist remotion.snk goto nosnk
 
-nant cleantemp zip sourcezip getcurrent
-      if errorlevel 1 pause "build error"
+Build-RemotionLocalDoc.cmd
+goto snk
+
+:nosnk
+echo.
+echo Did not find remotion.snk keyfile. 
+echo You can download the latest stable and officially signed re-motion build from the Downloads section at www.re-motion.org.  
+echo If you want to build your own version of re-motion, you need to create your own key to sign it; 
+echo to do so, please run Generate-Snk.cmd from a Visual Studio Command Prompt in this directory.
+echo.
+pause
+
+:snk
