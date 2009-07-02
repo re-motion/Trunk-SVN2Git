0273018c79a437bfe97c21986182bcf36afe45f3
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-07-02 06:52:05 +0000

    Fixed issue in Test project.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@13424 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/ObjectBinding/Web.Test/UI/NavigationTabs.ascx.cs
+++ Remotion/ObjectBinding/Web.Test/UI/NavigationTabs.ascx.cs
@@ -56,6 +56,7 @@ namespace OBWTest.UI
               (BindableObjectProvider) BusinessObjectProvider.GetProvider<BindableObjectProviderAttribute>(),
               new PropertyInfoAdapter (propertyInfo),
               propertyInfo.PropertyType,
+              propertyInfo.PropertyType,
               null,
               false,
               false));
