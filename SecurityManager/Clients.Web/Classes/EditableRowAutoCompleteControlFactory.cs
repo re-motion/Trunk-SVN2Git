756b6cb7a21d002cfb9bb26ed1c1703cd18ae089
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-07-10 08:54:36 +0000

    Updated SecurityManager for changed ISmartPage API.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@13562 cb0deee2-328e-b949-94ea-fb3139fab790

--- SecurityManager/Clients.Web/Classes/EditableRowAutoCompleteControlFactory.cs
+++ SecurityManager/Clients.Web/Classes/EditableRowAutoCompleteControlFactory.cs
@@ -16,22 +16,32 @@
 // Additional permissions are listed in the file re-motion_exceptions.txt.
 // 
 using System;
-using System.Web;
 using Remotion.ObjectBinding;
 using Remotion.ObjectBinding.Web.UI.Controls;
 using Remotion.ObjectBinding.Web.UI.Controls.Infrastructure.BocList;
 using Remotion.SecurityManager.Clients.Web.UI;
 using Remotion.SecurityManager.Domain.OrganizationalStructure;
 using Remotion.Utilities;
+using Remotion.Web.Infrastructure;
+using Remotion.Web.UI;
 
 namespace Remotion.SecurityManager.Clients.Web.Classes
 {
   public class EditableRowAutoCompleteControlFactory : EditableRowControlFactory
   {
-    public override void RegisterHtmlHeadContents (HttpContext context)
+    public EditableRowAutoCompleteControlFactory ()
     {
-      base.RegisterHtmlHeadContents (context);
-      new BocAutoCompleteReferenceValue().RegisterHtmlHeadContents (context);
+    }
+
+    public override void RegisterHtmlHeadContents (IHttpContext httpContext, HtmlHeadAppender htmlHeadAppender)
+    {
+      ArgumentUtility.CheckNotNull ("httpContext", httpContext);
+      ArgumentUtility.CheckNotNull ("htmlHeadAppender", htmlHeadAppender);
+
+      base.RegisterHtmlHeadContents (httpContext,htmlHeadAppender);
+
+      var bocAutoCompleteReferenceValue = new BocAutoCompleteReferenceValue();
+      bocAutoCompleteReferenceValue.RegisterHtmlHeadContents (httpContext, htmlHeadAppender);
     }
 
     protected override IBusinessObjectBoundEditableWebControl CreateFromPropertyPath (IBusinessObjectPropertyPath propertyPath)
