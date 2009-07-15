8b97f040485866c18dc970c50e06bc38a3e80109
markus.giegl <markus.giegl@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-07-15 14:07:12 +0000

    Remotion.Scripting - ScriptEnvironment WIP
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@13644 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Core/Scripting.UnitTests/ScriptEnvironmentTest.cs
+++ Remotion/Core/Scripting.UnitTests/ScriptEnvironmentTest.cs
@@ -1,6 +1,18 @@
-// Copyright (C) 2005 - 2009 rubicon informationstechnologie gmbh
-// All rights reserved.
-//
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// version 3.0 as published by the Free Software Foundation.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
 using System;
 using Microsoft.Scripting.Hosting;
 using NUnit.Framework;
@@ -12,11 +24,27 @@ namespace Remotion.Scripting.UnitTests
   public class ScriptEnvironmentTest
   {
     [Test]
-    public void Ctor ()
+    public void Ctor_ScriptScope ()
     {
       ScriptScope scriptScope = ScriptingHelper.CreateScriptScope (ScriptingHost.ScriptLanguageType.Python);
       var scriptEnvironment = new ScriptEnvironment (scriptScope);
       Assert.That (scriptEnvironment.ScriptScope, Is.EqualTo (scriptScope));
+    }
+
+    [Test]
+    public void DefaultCtor ()
+    {
+      var scriptEnvironment = new ScriptEnvironment ();
+      Assert.That (scriptEnvironment.ScriptScope, Is.Not.Null);
+    }
+
+    [Test]
+    public void Import ()
+    {
+      var scriptEnvironment = new ScriptEnvironment ();
+      scriptEnvironment.Import ("Remotion", "Remotion.Diagnostics.ToText", "To", "ToTextBuilder");
+      Assert.That (scriptEnvironment.ScriptScope.GetVariable("To"), Is.Not.Null);
+      Assert.That (scriptEnvironment.ScriptScope.GetVariable ("ToTextBuilder"), Is.Not.Null);
     }
   }
 }
--- Remotion/Core/Scripting/ScriptEnvironment.cs
+++ Remotion/Core/Scripting/ScriptEnvironment.cs
@@ -13,25 +13,73 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
+using System;
+using Microsoft.Scripting;
 using Microsoft.Scripting.Hosting;
+using Remotion.Diagnostics.ToText;
+using Remotion.Utilities;
 
 namespace Remotion.Scripting
 {
   /// <summary>
-  /// Wrapper around <see cref="Microsoft.Scripting.Hosting.ScriptScope"/>.
+  /// Wrapper around a DLR <see cref="Microsoft.Scripting.Hosting.ScriptScope"/>. 
+  /// A <see cref="ScriptEnvironment"/> contains the symbols (DLR: objects) a script which runs in this environment can see.
   /// </summary>
   public class ScriptEnvironment
   {
     private readonly ScriptScope _scriptScope;
 
+    /// <summary>
+    /// Default ctor. Recommended way to create a <see cref="ScriptEnvironment"/>.
+    /// </summary>
+    public ScriptEnvironment ()
+      : this (ScriptingHost.GetScriptEngine (ScriptingHost.ScriptLanguageType.Python).CreateScope ())
+    {
+    }
+    
+    /// <summary>
+    /// Wraps the passed <see cref="ScriptScope"/> in a <see cref="ScriptEnvironment"/>. Only use this if you know what you are doing.
+    /// </summary>
+    /// <param name="scriptScope"></param>
     public ScriptEnvironment (ScriptScope scriptScope)
     {
+      ArgumentUtility.CheckNotNull ("scriptScope", scriptScope);
       _scriptScope = scriptScope;
     }
+
 
     public ScriptScope ScriptScope
     {
       get { return _scriptScope; }
+    }
+
+
+    /// <summary>
+    /// Imports the passed symbols from the given namespace in the given assembly into the <see cref="ScriptEnvironment"/>. 
+    /// </summary>
+    /// <param name="assembly">Partial name of the assembly to import from (e.g. "Remotion")</param>
+    /// <param name="nameSpace">Namespace name in assembly to import from (e.g. "Remotion.Diagnostics.ToText")</param>
+    /// <param name="symbols">array of symbol names to import (e.g. "To", "ToTextBuilder", ...)</param>
+    public void Import (string assembly, string nameSpace, params string[] symbols)
+    {
+      ArgumentUtility.CheckNotNullOrEmpty ("assembly", assembly);
+      ArgumentUtility.CheckNotNullOrEmpty ("nameSpace", nameSpace);
+      ArgumentUtility.CheckNotNullOrEmpty ("symbols", symbols);
+
+      // TODO: Implement ToString(sb,ss,se) etc extension methods for IEnumerable instead
+      var toString = To.String;
+      toString.ToTextProvider.Settings.UseAutomaticStringEnclosing = false;
+
+      string scriptText = @"
+import clr
+clr.AddReferenceByPartialName('" + assembly + "')" +
+ @"
+from " + nameSpace + " import " + toString.sbLiteral ("", ",", "").elements (symbols).se ();
+
+      const ScriptingHost.ScriptLanguageType scriptLanguageType = ScriptingHost.ScriptLanguageType.Python;
+      var engine = ScriptingHost.GetScriptEngine (scriptLanguageType);
+      var scriptSource = engine.CreateScriptSourceFromString (scriptText, SourceCodeKind.Statements);
+      scriptSource.Execute (_scriptScope);
     }
   }
 }
