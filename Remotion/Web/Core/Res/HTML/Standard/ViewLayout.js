39d842b486010e686ef3a08fc2d68f835cca9475
martin.schwarzbauer <martin.schwarzbauer@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-08-04 14:28:10 +0000

    changed FixIE to set the CSS property values from the current values instead of constant values
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@13992 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Core/Res/HTML/Standard/ViewLayout.js
+++ Remotion/Web/Core/Res/HTML/Standard/ViewLayout.js
@@ -1,64 +1,72 @@
-﻿function ViewLayout() {
+﻿function ViewLayout()
+{
 }
 
-ViewLayout.AdjustWidth = function(contentElement) {
-    var children = contentElement.children('div');
-    children.each(function(i) {
-        $(this).css('position', 'absolute');
-        $(this).css('left', '0');
-        $(this).css('right', '0');
-    });
+ViewLayout.AdjustWidth = function(contentElement)
+{
+  var children = contentElement.children('div');
+  children.each(function(i)
+  {
+    $(this).css('position', 'absolute');
+    $(this).css('left', '0');
+    $(this).css('right', '0');
+  });
 };
 
-ViewLayout.AdjustHeight = function(contentElement) {
-    var margin = contentElement.outerHeight(true) - contentElement.innerHeight();
-    contentElement.height(contentElement.parent().height() - margin);
+ViewLayout.AdjustHeight = function(contentElement)
+{
+  var margin = contentElement.outerHeight(true) - contentElement.innerHeight();
+  contentElement.height(contentElement.parent().height() - margin);
 };
 
-ViewLayout.AdjustTop = function(topSibling, elementToAdjust) {
-    $(elementToAdjust).css('top', topSibling.position().top + topSibling.outerHeight(true));
+ViewLayout.AdjustTop = function(topSibling, elementToAdjust)
+{
+  $(elementToAdjust).css('top', topSibling.position().top + topSibling.outerHeight(true));
 };
 
-ViewLayout.AdjustBottom = function(bottomSibling, elementToAdjust) {
-    $(elementToAdjust).css('bottom', bottomSibling.parent().height() - bottomSibling.position().top);
+ViewLayout.AdjustBottom = function(bottomSibling, elementToAdjust)
+{
+  $(elementToAdjust).css('bottom', bottomSibling.parent().height() - bottomSibling.position().top);
 };
 
-ViewLayout.AdjustSingleView = function(containerElement) {
-    var contentElement = containerElement.children('div:first');
+ViewLayout.AdjustSingleView = function(containerElement)
+{
+  var contentElement = containerElement.children('div:first');
 
-    ViewLayout.AdjustHeight(contentElement);
-    ViewLayout.AdjustWidth(contentElement);
+  ViewLayout.AdjustHeight(contentElement);
+  ViewLayout.AdjustWidth(contentElement);
 
-    var top = contentElement.children('div:eq(0)');
-    var view = contentElement.children('div:eq(1)');
-    var bottom = contentElement.children('div:eq(2)');
+  var top = contentElement.children('div:eq(0)');
+  var view = contentElement.children('div:eq(1)');
+  var bottom = contentElement.children('div:eq(2)');
 
-    top.css('top', '0');
-    bottom.css('bottom', '0');
-    ViewLayout.AdjustTop(top, view);
-    ViewLayout.AdjustBottom(bottom, view);
+  top.css('top', '0');
+  bottom.css('bottom', '0');
+  ViewLayout.AdjustTop(top, view);
+  ViewLayout.AdjustBottom(bottom, view);
 
-    ViewLayout.FixIE6(view, bottom);
+  ViewLayout.FixIE6(view, bottom);
 };
 
-ViewLayout.AdjustTabbedMultiView = function(containerElement) {
-    var contentElement = containerElement.children('div:first');
+ViewLayout.AdjustTabbedMultiView = function(containerElement)
+{
+  var contentElement = containerElement.children('div:first');
 
-    ViewLayout.AdjustHeight(contentElement);
-    ViewLayout.AdjustWidth(contentElement);
+  ViewLayout.AdjustHeight(contentElement);
+  ViewLayout.AdjustWidth(contentElement);
 
-    var top = contentElement.children('div:eq(0)');
-    var tabs = contentElement.children('div:eq(1)');
-    var view = contentElement.children('div:eq(2)');
-    var bottom = contentElement.children('div:eq(3)');
+  var top = contentElement.children('div:eq(0)');
+  var tabs = contentElement.children('div:eq(1)');
+  var view = contentElement.children('div:eq(2)');
+  var bottom = contentElement.children('div:eq(3)');
 
-    top.css('top', '0');
-    bottom.css('bottom', '0');
-    ViewLayout.AdjustTop(top, tabs);
-    ViewLayout.AdjustTop(tabs, view);
-    ViewLayout.AdjustBottom(bottom, view);
+  top.css('top', '0');
+  bottom.css('bottom', '0');
+  ViewLayout.AdjustTop(top, tabs);
+  ViewLayout.AdjustTop(tabs, view);
+  ViewLayout.AdjustBottom(bottom, view);
 
-    ViewLayout.FixIE6(view, bottom);
+  ViewLayout.FixIE6(view, bottom);
 };
 
 ViewLayout.FixIE6 = function(view, bottom)
@@ -81,13 +89,11 @@ ViewLayout.FixIE6 = function(view, bottom)
   });
 
   // does not change style, but solves the problem that certain elements didn't show in standard mode
-  $('.tabStripTabSeparator').css('display', 'none');
-  $('.DropDownMenuContainer').css('display', 'block');
-  $('.bocListNavigator').css('display', 'block');
-  $('span.bocReferenceValueContent').css('display', 'block');
-  
-  $('span.bocAutoCompleteReferenceValueContent').css('display', 'block');
-  
-  $('span.bocDateTimeValue').css('display', 'inline-block');
-  $('a.DatePickerButton').css('display', 'inline-block');
+  $('.tabStripTabSeparator').css('display', $('.tabStripTabSeparator').css('display'));
+  $('.DropDownMenuContainer').css('display', $('.DropDownMenuContainer').css('display'));
+  $('.bocListNavigator').css('display', $('.bocListNavigator').css('display'));
+  $('span.bocReferenceValueContent').css('display', $('span.bocReferenceValueContent').css('display'));
+  $('span.bocAutoCompleteReferenceValueContent').css('display', $('span.bocAutoCompleteReferenceValueContent').css('display'));
+  $('span.bocDateTimeValue').css('display', $('span.bocDateTimeValue').css('display'));
+  $('a.DatePickerButton').css('display', $('a.DatePickerButton').css('display'));
 }
