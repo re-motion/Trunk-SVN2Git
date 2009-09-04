570e4a3cb1b35571ecba128dcd7f962a15c1a62a
markus.giegl <markus.giegl@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-09-04 14:57:38 +0000

    RelationViewer - set up projects
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@14368 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/RelationViewer.Browser/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("RelationViewer.Browser")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Microsoft")]
+[assembly: AssemblyProduct ("RelationViewer.Browser")]
+[assembly: AssemblyCopyright ("Copyright © Microsoft 2009")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible (false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid ("d824f89f-3301-46ba-b724-a981496cb6be")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
