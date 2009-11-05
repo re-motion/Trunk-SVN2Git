285450fef6c248319974dd868af7cbcf30b9c6d5
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-11-05 10:24:37 +0000

    Created separate build-artefacts for re-vision and re-linq.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@14760 cb0deee2-328e-b949-94ea-fb3139fab790

--- Build-RemotionLocal.cmd
+++ Build-RemotionLocal.cmd
@@ -5,11 +5,6 @@ if not exist remotion.snk goto nosnk
 
 echo Building re-motion without docs using %nant%...
 echo.
-
-%nant% "-f:Remotion.build" "-D:build.temp.root=\Temp\RemotionLocal" "-t:net-3.5" "-l:Build.log" "-nologo" ^
-    "-D:build.update.assembly-info=false" ^
-    clean cleantemp ^
-    resources debug
     
 if not %ERRORLEVEL%==0 goto build_failed
 
@@ -19,6 +14,8 @@ echo.
 %nant% "-f:Remotion.build" "-D:build.temp.root=\Temp\RemotionLocal" "-t:net-3.5" "-nologo" ^
     cleantemp ^
     sourcezip zip ^
+    relinq-sourcezip relinq-zip ^
+    dms-sourcezip dms-zip ^
     securityManager-sourcezip securityManager-zip
 
 if not %ERRORLEVEL%==0 goto zip_failed
