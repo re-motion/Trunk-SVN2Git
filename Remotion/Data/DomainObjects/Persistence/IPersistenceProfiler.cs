ce1eec3ac31be4f3e78c838711561895ab6b1f8b
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2009-12-07 06:21:16 +0000

    temp commit (profiling)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@14984 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/DomainObjects/Persistence/IPersistenceProfiler.cs
+++ Remotion/Data/DomainObjects/Persistence/IPersistenceProfiler.cs
@@ -16,95 +16,9 @@
 // 
 using System;
 using System.Data;
-using System.Diagnostics;
-using HibernatingRhinos.Profiler.Appender.LinqToSql;
 
 namespace Remotion.Data.DomainObjects.Persistence
 {
-  public class PersistenceProfiler: IPersistenceProfiler
-  {
-    public static readonly IPersistenceProfiler Null = new NullPersistenceProfiler();
-    private static LinqToSqlAppender s_linqToSqlAppender;
-
-    public static LinqToSqlAppender LinqToSqlAppender
-    {
-      get
-      {
-        if (s_linqToSqlAppender == null)
-        {
-          HibernatingRhinos.Profiler.Appender.LinqToSql.LinqToSqlProfiler.Initialize ();
-          s_linqToSqlAppender = LinqToSqlProfiler.GetAppender ("re-store UberProf PoC - RestoreProfiler");
-        }
-        return s_linqToSqlAppender;
-      }
-    }
-
-
-    private readonly Guid _clientTransactionID;
-    private Stopwatch _stopwatch;
-
-    public PersistenceProfiler (Guid clientTransactionID)
-    {
-      _clientTransactionID = clientTransactionID;
-    }
-
-    public void ConnectionOpened (Guid connectionID)
-    {
-      LinqToSqlAppender.ConnectionStarted (_clientTransactionID);
-    }
-
-    public void ConnectionClosed (Guid connectionID)
-    {
-      LinqToSqlAppender.ConnectionDisposed (_clientTransactionID);
-    }
-
-    public void TransactionBegan (Guid connectionID, IsolationLevel isolationLevel)
-    {
-      LinqToSqlAppender.TransactionBegan (connectionID, isolationLevel);
-    }
-
-    public void TransactionCommitted (Guid connectionID)
-    {
-      LinqToSqlAppender.TransactionCommit (connectionID);
-    }
-
-    public void TransactionRolledback (Guid connectionID)
-    {
-      LinqToSqlAppender.TransactionRolledBack (connectionID);
-    }
-
-    public void TransactionDisposed (Guid connectionID)
-    {
-      LinqToSqlAppender.TransactionDisposed (connectionID);
-    }
-
-    public void QueryExecuting (Guid connectionID, Guid queryID, string commandText)
-    {
-      LinqToSqlAppender.StatementExecuted (_clientTransactionID, queryID, commandText);
-      _stopwatch = new Stopwatch();
-      _stopwatch.Start ();
-    }
-
-    public void QueryExecuted (Guid connectionID, Guid queryID)
-    {
-      LinqToSqlAppender.CommandDurationAndRowCount (_clientTransactionID, _stopwatch.ElapsedMilliseconds, null);
-    }
-
-    public void QueryCompleted (Guid connectionID, Guid queryID, int rowCount)
-    {
-      _stopwatch.Stop();
-      LinqToSqlAppender.StatementRowCount (connectionID, queryID, rowCount);
-      _stopwatch = null;
-    }
-
-    public void QueryError (Guid connectionID, Guid queryID, Exception e)
-    {
-      _stopwatch.Stop ();
-      _stopwatch = null;
-      LinqToSqlAppender.StatementError (connectionID, e);
-    }
-  }
-
   public class NullPersistenceProfiler : IPersistenceProfiler
   {
     public NullPersistenceProfiler ()
