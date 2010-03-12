72de810a63294ff232bdf6b85c2ded84b1319d9b
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-03-12 07:36:43 +0000

    temp commit:  Split Quirksmode into Legacy-Assemblies (RM-2372)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@16051 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/ObjectBinding/Web.Test/Global.asax.cs
+++ Remotion/ObjectBinding/Web.Test/Global.asax.cs
@@ -66,7 +66,7 @@ namespace OBWTest
     protected void Application_Start (Object sender, EventArgs e)
     {
       XmlConfigurator.Configure();
-      var resourceTheme = Remotion.Web.ResourceTheme.Legacy;
+      var resourceTheme = Remotion.Web.ResourceTheme.NovaBlue;
       PreferQuirksModeRendering = resourceTheme == Remotion.Web.ResourceTheme.Legacy;
 
       string objectPath = Server.MapPath ("~/objects");
