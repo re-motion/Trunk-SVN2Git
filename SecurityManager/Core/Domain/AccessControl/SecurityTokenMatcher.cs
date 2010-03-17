9212625902949e94a386fbe19b2385a157e62817
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-03-17 20:11:06 +0000

    Fixed typo.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@16167 cb0deee2-328e-b949-94ea-fb3139fab790

--- SecurityManager/Core/Domain/AccessControl/SecurityTokenMatcher.cs
+++ SecurityManager/Core/Domain/AccessControl/SecurityTokenMatcher.cs
@@ -162,7 +162,7 @@ namespace Remotion.SecurityManager.Domain.AccessControl
         case GroupCondition.SpecificGroup:
           return MatchPrincipalAgainstGroup (token.Principal, _ace.SpecificGroup, _ace.GroupHierarchyCondition);
 
-          // BranchOfOwningGroup matches the ACE iff: The (security token) owning group or any of its parent-groups has the 
+          // BranchOfOwningGroup matches the ACE if: The (security token) owning group or any of its parent-groups has the 
           // group type stored in the ACE - and - the principal is a member of any of the groups which come below the first
           // group in the group hierarchy including and above the owning group whose group type is equal to the ACE-group-type.
           // Algorithmically the matcher first walks upward the group hierarchy of the owning group (including the owning group itself),
