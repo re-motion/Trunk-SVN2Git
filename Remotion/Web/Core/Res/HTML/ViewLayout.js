59df1dba9c4799192c1017a4e90b07d0a8881ef6
radu.bighiu <radu.bighiu@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-04-08 12:56:34 +0000

    Workaround to force Opera to calculate proper offsets by defining border 'solid 0 transparent' instead of 'none' for bottomControls
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@16566 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Core/Res/HTML/ViewLayout.js
+++ Remotion/Web/Core/Res/HTML/ViewLayout.js
@@ -24,7 +24,7 @@ ViewLayout.AdjustActiveViewContent = function(viewContent)
 {
   var viewContentBorder = viewContent.children().eq(0);
   var viewBottomControls = viewContent.next();
-  if ($.browser.opera) viewBottomControls.css({ 'border':'solid 0 transparent' });
+  if ($.browser.opera) viewBottomControls.addClass('tabbedMultiViewBottomControlsOperaWorkaround');
 
   var viewContentBorderHeight = viewContentBorder.outerHeight(true) - viewContentBorder.height();
   var viewBottomControlsBorderHeight = viewBottomControls.outerHeight(true) - viewBottomControls.height();
