fd8da2619f23253ddefcb897e5105e279676c714
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-04-13 20:39:59 +0000

    Added performance test for Security on DomainObjects.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@16623 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Data/DomainObjects.PerformanceTests/SecurityTest.cs
@@ -0,0 +1,71 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using Remotion.Data.DomainObjects.PerformanceTests.TestDomain;
+using Remotion.ObjectBinding;
+using Remotion.Security;
+using Remotion.Security.Configuration;
+
+namespace Remotion.Data.DomainObjects.PerformanceTests
+{
+  [TestFixture]
+  public class SecurityTest
+  {
+    public const int TestRepititions = 1000;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      SecurityConfiguration.Current.SecurityProvider = new StubSecurityProvider();
+      AdapterRegistry.Instance.SetAdapter (typeof (IObjectSecurityAdapter), new ObjectSecurityAdapter());
+    }
+
+    [TearDown]
+    public void TearDown ()
+    {
+      SecurityConfiguration.Current.SecurityProvider = null;
+    }
+
+    [Test]
+    public void BusinessObject_Property_IsAccessible ()
+    {
+      Console.WriteLine ("Expected average duration of SecurityTest for BusinessObject_Property_IsAccessible on reference system: ~0.8 ms");
+
+      using (ClientTransaction.CreateRootTransaction().EnterDiscardingScope())
+      {
+        var obj = (IBusinessObject) ObjectWithSecurity.NewObject();
+        var property = obj.BusinessObjectClass.GetPropertyDefinition ("TheProperty");
+
+        Assert.That (property.IsAccessible (obj.BusinessObjectClass, obj));
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        for (int i = 0; i < TestRepititions; i++)
+          property.IsAccessible (obj.BusinessObjectClass, obj);
+        stopwatch.Stop();
+
+        double averageMilliSeconds = (double) stopwatch.ElapsedMilliseconds / TestRepititions;
+        Console.WriteLine (
+            "BusinessObject_Property_IsAccessible (executed {0}x): Average duration: {1} ms",
+            TestRepititions, 
+            averageMilliSeconds.ToString ("n"));
+      }
+    }
+  }
+}
---
+++ Remotion/Data/DomainObjects.PerformanceTests/TestDomain/ObjectWithSecurity.cs
@@ -0,0 +1,66 @@
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using Remotion.Data.DomainObjects.ObjectBinding;
+using Remotion.Data.DomainObjects.Security;
+using Remotion.Security;
+
+namespace Remotion.Data.DomainObjects.PerformanceTests.TestDomain
+{
+  [Instantiable]
+  [DBTable]
+  [BindableDomainObject]
+  public abstract class ObjectWithSecurity : DomainObject, ISecurableObject, IDomainObjectSecurityContextFactory
+  {
+    public static ObjectWithSecurity NewObject ()
+    {
+      return NewObject<ObjectWithSecurity>();
+    }
+
+    protected ObjectWithSecurity ()
+    {
+    }
+
+    public abstract string TheProperty { get; set; }
+
+    IObjectSecurityStrategy ISecurableObject.GetSecurityStrategy ()
+    {
+      return new DomainObjectSecurityStrategy (RequiredSecurityForStates.NewAndDeleted, this);
+    }
+
+    Type ISecurableObject.GetSecurableType ()
+    {
+      return GetPublicDomainObjectType();
+    }
+
+    ISecurityContext ISecurityContextFactory.CreateSecurityContext ()
+    {
+      return SecurityContext.Create (GetPublicDomainObjectType (), null, null, null, new Dictionary<string, EnumWrapper> (), new EnumWrapper[0]);
+    }
+
+    public bool IsNew
+    {
+      get { return State == StateType.New; }
+    }
+
+    public bool IsDeleted
+    {
+      get { return State == StateType.Deleted; }
+    }
+  }
+}
