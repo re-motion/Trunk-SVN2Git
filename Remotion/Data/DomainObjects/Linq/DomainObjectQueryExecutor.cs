fe0d1e25d90ea148a8fed306fbc4740867e1f2c5
andreas.obkircher <andreas.obkircher@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-05-03 13:41:29 +0000

    removed obsolete review comment (RM-2694)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@16986 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/DomainObjects/Linq/DomainObjectQueryExecutor.cs
+++ Remotion/Data/DomainObjects/Linq/DomainObjectQueryExecutor.cs
@@ -424,7 +424,6 @@ namespace Remotion.Data.DomainObjects.Linq
     {
       var commandBuilder = new SqlCommandBuilder();
       _generationStage.GenerateTextForSqlStatement (commandBuilder, sqlStatement);
-      // TODO Review 2494: When the stage has been turned into a parameter, use a mock to test that ValueRequired is passed here (not SingleValueRequired)
       return commandBuilder.GetCommand();
     }
   }
