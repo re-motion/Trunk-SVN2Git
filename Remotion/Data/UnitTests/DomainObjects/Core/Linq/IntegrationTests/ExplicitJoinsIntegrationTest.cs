912ced13f5cefa8cadc71c9c566101a443f3977e
andreas.obkircher <andreas.obkircher@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-05-06 15:07:36 +0000

    renamed ISqlPreparationContext.TryGetSqlTableForQuerySource to ISqlPreparationContext.TryGetSqlTableFromHierarchy adapted SqlPreparationQueryModelVisitorContext.GetSqlTableForQuerySource adapted SqlPreparationQueryModelVisitorContext.TryGetSqlTableFromHierarchy added integration tests for explicit joins with group into (RM-2668)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@17057 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/UnitTests/DomainObjects/Core/Linq/IntegrationTests/ExplicitJoinsIntegrationTest.cs
+++ Remotion/Data/UnitTests/DomainObjects/Core/Linq/IntegrationTests/ExplicitJoinsIntegrationTest.cs
@@ -35,11 +35,97 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
           select c , DomainObjectIDs.Order4);
     }
 
-    //TODO: 2668
+    [Test]
+    public void ExplicitJoinWithInto_Once ()
+    {
+      CheckQueryResult (
+          from o in QueryFactory.CreateLinqQuery<Order> ()
+          join i in QueryFactory.CreateLinqQuery<OrderItem> () on o equals i.Order into goi
+          from oi in goi
+          select oi,
+          DomainObjectIDs.OrderItem1, DomainObjectIDs.OrderItem2, DomainObjectIDs.OrderItem3, DomainObjectIDs.OrderItem4, DomainObjectIDs.OrderItem5);
+    }
+
+    [Test]
+    public void ExplicitJoinWithInto_Twice() 
+    {
+      CheckQueryResult (
+          from o in QueryFactory.CreateLinqQuery<Order> ()
+          join i in QueryFactory.CreateLinqQuery<OrderItem> () on o equals i.Order into goi
+          join c in QueryFactory.CreateLinqQuery<Customer> () on o.Customer equals c into goc
+          from oi in goi
+          from oc in goc
+          select oi,
+          DomainObjectIDs.OrderItem1, DomainObjectIDs.OrderItem2, DomainObjectIDs.OrderItem3, DomainObjectIDs.OrderItem4, DomainObjectIDs.OrderItem5);
+    }
+
+    [Test]
+    [Ignore ("TODO: 2668")]
+    public void ExplicitJoinWithInto_InSubstatement_Once ()
+    {
+      CheckQueryResult (
+          from o in QueryFactory.CreateLinqQuery<Order> ()
+          where o.OrderNumber ==
+            (from so in QueryFactory.CreateLinqQuery<Order> ()
+             join si in QueryFactory.CreateLinqQuery<OrderItem> () on so equals si.Order into goi
+             from oi in goi
+             select oi.Order.OrderNumber).First ()
+          select o);
+    }
+
+    [Test]
+    [Ignore ("TODO: 2668")]
+    public void ExplicitJoinWithInto_InSubstatement_Twice ()
+    {
+      CheckQueryResult (
+          from o in QueryFactory.CreateLinqQuery<Order> ()
+          where o.OrderNumber ==
+            (from so in QueryFactory.CreateLinqQuery<Order> ()
+             join si in QueryFactory.CreateLinqQuery<OrderItem> () on so equals si.Order into goi
+             join si in QueryFactory.CreateLinqQuery<Customer>() on so.Customer equals si into goc
+             from oi in goi
+             from oc in goc
+             select oi.Order.OrderNumber).First ()
+          select o);
+    }
+
+    [Test]
+    [Ignore ("TODO: 2668")]
+    public void ExplicitJoinWithInto_InTwoSubstatements ()
+    {
+      CheckQueryResult (
+          from o in QueryFactory.CreateLinqQuery<Order> ()
+          where o.OrderNumber ==
+            (from so in QueryFactory.CreateLinqQuery<Order> ()
+             join si in QueryFactory.CreateLinqQuery<OrderItem> () on so equals si.Order into goi
+             from oi in goi
+             select oi.Order.OrderNumber).First ()
+            && o.Customer.Name ==
+              (from so in QueryFactory.CreateLinqQuery<Order> ()
+               join sc in QueryFactory.CreateLinqQuery<Customer> () on so.Customer equals sc into goc
+               from oc in goc
+               select oc.Name).First ()
+          select o);
+    }
+
     //[Test]
-    //public void ExplicitJoinWithInto_Once ()
+    //[Ignore ("TODO: 2668")]
+    //public void ExplicitJoinWithInto_InSameStatementAndInSubstatement ()
     //{
-      
+    //  CheckQueryResult (
+    //    from o in QueryFactory.CreateLinqQuery<Order> ()
+    //    join i in QueryFactory.CreateLinqQuery<OrderItem> () on o equals i.Order into goi
+    //    from oi in goi
+    //    where oi.Product ==
+    //    (from o in QueryFactory.CreateLinqQuery<Order> ()
+    //     join i in QueryFactory.CreateLinqQuery<OrderItem> () equals a into gia
+    //     from ia in gia
+    //     select ia.FirstName).First ()
+    //    select kc.Name,
+    //    ""
+    //    );
     //}
+
+
   }
 }
