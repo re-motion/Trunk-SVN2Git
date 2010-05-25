b6da5a5c38cba9edfbbd29c55243a0a00e1646d8
andreas.obkircher <andreas.obkircher@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-05-25 08:23:25 +0000

    Un-ignored tests that already work (RM-2779)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@17300 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/UnitTests/DomainObjects/Core/Linq/IntegrationTests/ResultOperatorIntegrationTest.cs
+++ Remotion/Data/UnitTests/DomainObjects/Core/Linq/IntegrationTests/ResultOperatorIntegrationTest.cs
@@ -442,7 +442,6 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
     }
 
     [Test]
-    [Ignore("TODO 2779")]
     public void DefaultIsEmpty_WithJoin ()
     {
       var query = (from o in QueryFactory.CreateLinqQuery<Order>()
