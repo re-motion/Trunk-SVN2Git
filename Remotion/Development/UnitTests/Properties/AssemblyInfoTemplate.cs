54f3cdb4017dd312d7e2a2d5f8fee03542eb9565
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-06-04 17:03:21 +0000

    Fixed AssemblyInfoTemplate.cs (missing using)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@17534 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Development/UnitTests/Properties/AssemblyInfoTemplate.cs
+++ Remotion/Development/UnitTests/Properties/AssemblyInfoTemplate.cs
@@ -15,6 +15,8 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System.Reflection;
+using System.Security;
+
 //
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
