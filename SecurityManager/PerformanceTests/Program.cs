cb519ebbcef7369127b706ca20e61fd641b91230
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-06-07 12:44:08 +0000

    Added tracing to performance tests.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@17546 cb0deee2-328e-b949-94ea-fb3139fab790

--- SecurityManager/PerformanceTests/Program.cs
+++ SecurityManager/PerformanceTests/Program.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
+using Autofac;
+using AutofacContrib.CommonServiceLocator;
+using Microsoft.Practices.ServiceLocation;
+using Remotion.Data.DomainObjects.Infrastructure;
+using Remotion.Data.DomainObjects.Tracing;
+using Remotion.Data.DomainObjects.UberProfIntegration;
 using Remotion.Security;
 using Remotion.Security.Configuration;
 
@@ -11,6 +17,15 @@ namespace Remotion.SecurityManager.PerformanceTests
   {
     static void Main (string[] args)
     {
+      var builder = new ContainerBuilder ();
+
+      //builder.Register (c => new LinqToSqlListenerFactory ())
+      //    .As<IClientTransactionListenerFactory, IPersistenceListenerFactory> ()
+      //    .InstancePerDependency ();
+
+      var autofacServiceLocator = new AutofacServiceLocator (builder.Build ());
+      ServiceLocator.SetLocatorProvider (() => autofacServiceLocator);
+
       ISecurityProvider provider = SecurityConfiguration.Current.SecurityProvider;
       var context =
           new SimpleSecurityContext (
@@ -32,6 +47,7 @@ namespace Remotion.SecurityManager.PerformanceTests
       stopwatch.Stop();
       Trace.Write (dummy);
       Console.WriteLine ("Time taken: {0}ms", stopwatch.ElapsedMilliseconds/10.0);
+      Console.ReadKey();
     }
   }
 }
