2964659f9eaa355b5a84c8c6cc9906e2d6d019ca
matthias.goertler <matthias.goertler@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-06-09 15:35:06 +0000

    added tests for TracingDataReader (RM-1967)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@17579 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/UnitTests/DomainObjects/Core/Tracing/TracingDataReaderTest.cs
+++ Remotion/Data/UnitTests/DomainObjects/Core/Tracing/TracingDataReaderTest.cs
@@ -46,6 +46,131 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Tracing
     }
 
     [Test]
+    public void GetName ()
+    {
+      var i = 5;
+      _innerDataReader.Expect (mock => mock.GetName (i)).Return("test");
+      _mockRepository.ReplayAll();
+
+      var result = _dataReader.GetName (i);
+
+      Assert.That (result, Is.EqualTo ("test"));
+      _mockRepository.VerifyAll();
+    }
+
+    [Test]
+    public void GetDataTypeName ()
+    {
+      var i = 5;
+      _innerDataReader.Expect (mock => mock.GetDataTypeName (i)).Return ("test");
+      _mockRepository.ReplayAll ();
+
+      var result = _dataReader.GetDataTypeName (i);
+
+      Assert.That (result, Is.EqualTo ("test"));
+      _mockRepository.VerifyAll ();
+    }
+
+    [Test]
+    public void GetFieldType ()
+    {
+      var i = 5;
+      var expectedType = typeof(string);
+
+      _innerDataReader.Expect (mock => mock.GetFieldType (i)).Return (expectedType);
+      _mockRepository.ReplayAll ();
+
+      var result = _dataReader.GetFieldType (i);
+
+      Assert.That (result, Is.EqualTo (expectedType));
+      _mockRepository.VerifyAll ();
+    }
+
+    [Test]
+    public void GetValue ()
+    {
+      var i = 5;
+      var o = "test";
+      _innerDataReader.Expect (mock => mock.GetValue (i)).Return (o);
+      _mockRepository.ReplayAll ();
+
+      var result = _dataReader.GetValue (i);
+      
+      Assert.That (result, Is.EqualTo (o));
+      _mockRepository.VerifyAll ();
+    }
+
+    [Test]
+    public void GetValues ()
+    {
+      var i = 5;
+      object[] values = new object[] { "1",2, "3"};
+      _innerDataReader.Expect (mock => mock.GetValues (values)).Return (i);
+      _mockRepository.ReplayAll ();
+
+      var result = _dataReader.GetValues (values);
+      
+      Assert.That (result, Is.EqualTo (i));
+      _mockRepository.VerifyAll ();
+    }
+
+    [Test]
+    public void GetOrdinal ()
+    {
+      var i = 5;
+      var name = "test";
+      _innerDataReader.Expect (mock => mock.GetOrdinal (name)).Return (i);
+      _mockRepository.ReplayAll ();
+
+      var result = _dataReader.GetOrdinal (name);
+
+      Assert.That (result, Is.EqualTo (i));
+      _mockRepository.VerifyAll ();
+    }
+
+    [Test]
+    public void GetBoolean ()
+    {
+      var i = 5;
+      _innerDataReader.Expect (mock => mock.GetBoolean (i)).Return (true);
+      _mockRepository.ReplayAll ();
+
+      var result = _dataReader.GetBoolean (i);
+
+      Assert.That (result, Is.True);
+      _mockRepository.VerifyAll ();
+    }
+
+    [Test]
+    public void GetByte ()
+    {
+      var i = 5;
+      var b = new Byte();
+
+      _innerDataReader.Expect (mock => mock.GetByte (i)).Return (b);
+      _mockRepository.ReplayAll ();
+
+      var result = _dataReader.GetByte (i);
+
+      Assert.That (result, Is.EqualTo (b));
+      _mockRepository.VerifyAll ();
+    }
+
+    [Test]
+    public void GetChar ()
+    {
+      var i = 5;
+      var c = 't';
+      _innerDataReader.Expect (mock => mock.GetChar (i)).Return (c);
+      _mockRepository.ReplayAll();
+
+      var result = _dataReader.GetChar (i);
+
+      Assert.That (result, Is.EqualTo (c));
+      _mockRepository.VerifyAll ();
+    }
+
+    [Test]
     public void Dispose ()
     {
       using (_mockRepository.Ordered ())
