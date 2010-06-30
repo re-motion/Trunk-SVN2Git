fdcd2da3bd06e13141f3c57af084872eb30ace2b
matthias.goertler <matthias.goertler@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-06-30 11:00:35 +0000

    added MemberResolverProviderHelper (RM-2994)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@17891 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Security/Core/Configuration/MemberResolverProviderHelper.cs
@@ -0,0 +1,50 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Configuration;
+using Remotion.Configuration;
+using Remotion.Security.Metadata;
+
+namespace Remotion.Security.Configuration
+{
+  /// <summary>Helper class that loads implementations of <see cref="IMemberResolver"/> from the <see cref="SecurityConfiguration"/> section.</summary>
+  public class MemberResolverProviderHelper : ProviderHelperBase<IMemberResolver>
+  {
+    private const string c_permissionReflectorWellKnownName = "Reflection";
+
+    public MemberResolverProviderHelper (ExtendedConfigurationSection configurationSection)
+        : base(configurationSection)
+    {
+    }
+
+    protected override ConfigurationProperty CreateDefaultProviderNameProperty ()
+    {
+      return CreateDefaultProviderNameProperty ("defaultMemberResolverProvider", c_permissionReflectorWellKnownName);
+    }
+
+    protected override ConfigurationProperty CreateProviderSettingsProperty ()
+    {
+      return CreateProviderSettingsProperty ("memberResolverProviders");
+    }
+
+    public override void PostDeserialze ()
+    {
+      CheckForDuplicateWellKownProviderName (c_permissionReflectorWellKnownName);
+    }
+
+  }
+}
