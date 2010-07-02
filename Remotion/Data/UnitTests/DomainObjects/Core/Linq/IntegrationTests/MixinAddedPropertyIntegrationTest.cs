7a4d451b7c6e8e5620b905c84289c1a614294bb2
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-07-02 10:15:56 +0000

    Fixed task number (RM-2980)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@17932 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/UnitTests/DomainObjects/Core/Linq/IntegrationTests/MixinAddedPropertyIntegrationTest.cs
+++ Remotion/Data/UnitTests/DomainObjects/Core/Linq/IntegrationTests/MixinAddedPropertyIntegrationTest.cs
@@ -22,12 +22,12 @@ using Remotion.Data.UnitTests.DomainObjects.Core.MixedDomains.TestDomain;
 
 namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
 {
-  // TODO Review 2982: Fix the tests below by adding new rows to the database. To make this easier, you can write a test that builds the desired 
-  // TODO Review 2982: data structure (creating or loading objects via NewObject()/GetObject()), then commits that transaction using 
-  // TODO Review 2982: SetDatabaseModifyable()/ClientTransaction.Current.Commit(). Fire up SQL profiler and log the INSERT/UPDATE statements, then 
-  // TODO Review 2982: add those statements to DataDomainObjects_CreateTestData.sql. Also document the data structure in the DomainObjectIDs class. 
-  // TODO Review 2982: Note that you have to "Clean" the project before building if you make changes only to the SQL script; otherwise your tests
-  // TODO Review 2982: will still run with the old SQL script...
+  // TODO Review 2980: Fix the tests below by adding new rows to the database. To make this easier, you can write a test that builds the desired 
+  // TODO Review 2980: data structure (creating or loading objects via NewObject()/GetObject()), then commits that transaction using 
+  // TODO Review 2980: SetDatabaseModifyable()/ClientTransaction.Current.Commit(). Fire up SQL profiler and log the INSERT/UPDATE statements, then 
+  // TODO Review 2980: add those statements to DataDomainObjects_CreateTestData.sql. Also document the data structure in the DomainObjectIDs class. 
+  // TODO Review 2980: Note that you have to "Clean" the project before building if you make changes only to the SQL script; otherwise your tests
+  // TODO Review 2980: will still run with the old SQL script...
   [TestFixture]
   public class MixinAddedPropertyIntegrationTest : IntegrationTestBase
   {
@@ -62,7 +62,7 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
     }
 
     [Test]
-    [Ignore ("TODO Review 2982: This test does not work because there is no instance of TargetClassForPersistentMixin in the database that has a non-null RelationPropertyID column. To remedy, change DataDomainObjects_CreateTestData.sql to include a few instances of RelationTargetForPersistentMixin, and then change the Insert statement, eg. for 784EBDDD-EE94-456D-A5F4-F6CB1B41B6CA, to include the RelationPropertyID column pointing back to one of those RelationTargets.)")]
+    [Ignore ("TODO Review 2980: This test does not work because there is no instance of TargetClassForPersistentMixin in the database that has a non-null RelationPropertyID column. To remedy, change DataDomainObjects_CreateTestData.sql to include a few instances of RelationTargetForPersistentMixin, and then change the Insert statement, eg. for 784EBDDD-EE94-456D-A5F4-F6CB1B41B6CA, to include the RelationPropertyID column pointing back to one of those RelationTargets.)")]
     public void RelationWithForeignKeyAddedByMixin ()
     {
       var query =
@@ -73,7 +73,7 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
     }
 
     [Test]
-    [Ignore ("TODO Review 2982: This test does not work because there is no instance of RelationTargetForPersistentMixin in the database that has a non-null VirtualRelationPropertyID column. To remedy, change DataDomainObjects_CreateTestData.sql to include a few instances of RelationTargetForPersistentMixin, and have one of them include a back-reference to 4ED563B8-B337-4C8E-9A77-5FA907919377 and one to have a back-reference to 784EBDDD-EE94-456D-A5F4-F6CB1B41B6CA.)")]
+    [Ignore ("TODO Review 2980: This test does not work because there is no instance of RelationTargetForPersistentMixin in the database that has a non-null VirtualRelationPropertyID column. To remedy, change DataDomainObjects_CreateTestData.sql to include a few instances of RelationTargetForPersistentMixin, and have one of them include a back-reference to 4ED563B8-B337-4C8E-9A77-5FA907919377 and one to have a back-reference to 784EBDDD-EE94-456D-A5F4-F6CB1B41B6CA.)")]
     public void RelationWithoutForeignKeyAddedByMixin ()
     {
       var query =
@@ -84,7 +84,7 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
     }
 
     [Test]
-    [Ignore ("TODO Review 2982: See above.)")]
+    [Ignore ("TODO Review 2980: See above.)")]
     public void RelationWithForeignKeyAddedByMixin_PropertyOfRelatedObject ()
     {
       var query =
@@ -95,7 +95,7 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
     }
 
     [Test]
-    [Ignore ("TODO Review 2982: See above.)")]
+    [Ignore ("TODO Review 2980: See above.)")]
     public void RelationWithoutForeignKeyAddedByMixin_PropertyOfRelatedObject ()
     {
       var query =
@@ -106,7 +106,7 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
     }
 
     [Test]
-    [Ignore ("TODO Review 2982: This test does not work because DefaultMappingResolutionStage.ResolveCollectionSourceExpression cannot handle "
+    [Ignore ("TODO Review 2980: This test does not work because DefaultMappingResolutionStage.ResolveCollectionSourceExpression cannot handle "
         + "UnaryExpressions. Change return type of ResolveCollectionSourceExpression to return Expression. In ResolvingJoinInfoVisitor, loop while the " 
         + "resolved expression is a UnaryExpression, then cast to SqlEntityExpression using an 'as' cast. If the cast fails, throw an exception "
         + "(e.g.: Only entities can be used as the collection source in from expressions, '{0}' cannot.)")]
@@ -116,7 +116,7 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
           from o in QueryFactory.CreateLinqQuery<TargetClassForPersistentMixin>()
           from related in ((IMixinAddingPersistentProperties) o).CollectionProperty1Side
           select related;
-      // TODO Review 2982: To get a more meaningful result, also add a few data records for RelationTargetForPersistentMixin that point back to some TargetClassForPersistentMixins via RelationProperty3
+      // TODO Review 2980: To get a more meaningful result, also add a few data records for RelationTargetForPersistentMixin that point back to some TargetClassForPersistentMixins via RelationProperty3
       CheckQueryResult (query);
     }
   }
