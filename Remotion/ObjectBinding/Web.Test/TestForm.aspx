527bd504b012d1cba267a560a7b948e6e2e07312
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-07-14 07:09:35 +0000

    Committed test page.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@18070 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/ObjectBinding/Web.Test/TestForm.aspx
+++ Remotion/ObjectBinding/Web.Test/TestForm.aspx
@@ -20,30 +20,8 @@
 <html>
   <head>
     <title>Test Form</title>
-<meta content="Microsoft Visual Studio .NET 7.1" name=GENERATOR>
-<meta content=C# name=CODE_LANGUAGE>
-<meta content=JavaScript name=vs_defaultClientScript>
-<meta content=http://schemas.microsoft.com/intellisense/ie5 name=vs_targetSchema>
-<remotion:htmlheadcontents id=HtmlHeadContents runat="server"></remotion:htmlheadcontents>
   </head>
 <body>
-<form id=Form method=post runat="server">
-<remotion:LazyContainer runat="server" id="LazyContainer"><remotion:bocTextValue id="TextField" runat="server" readonly="True"></remotion:bocTextValue></remotion:LazyContainer>
-<p><asp:Button id="PostBackButton" runat="server" Text="PostBack"></asp:Button></p>
-<table id="FormGrid" cellspacing="1" cellpadding="1" width="300" border="1" runat="server">
-  <tr>
-    <td colspan="2"></td></tr>
-  <tr>
-    <td></td>
-    <td><asp:textbox id="TextBox1" runat="server"></asp:textbox><asp:requiredfieldvalidator id="RequiredFieldValidator1" runat="server" ErrorMessage="RequiredFieldValidator" ControlToValidate="TextBox1" enableclientscript="False"></asp:requiredfieldvalidator></td></tr>
-  <tr>
-    <td></td>
-    <td><remotion:boctextvalue id="field" runat=server required="True" valuetype="String">
-<textboxstyle textmode="SingleLine">
-</TextBoxStyle></remotion:boctextvalue></td></tr>
-</table>
-<remotion:formgridmanager id="fgm" runat="server"/>
-</form>
 
   </body>
 </html>
--- Remotion/ObjectBinding/Web.Test/TestForm.aspx.cs
+++ Remotion/ObjectBinding/Web.Test/TestForm.aspx.cs
@@ -15,72 +15,13 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System;
+using System.Threading;
 using System.Web.UI;
-using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
-using Remotion.ObjectBinding.Web.UI.Controls;
-using Remotion.Web.UI.Controls;
 
 namespace OBWTest
 {
-
-public class TestForm : Page
-{
-  protected Button PostBackButton;
-  protected LazyContainer LazyContainer;
-  protected BocTextValue TextField;
-  protected TextBox TextBox1;
-  protected RequiredFieldValidator RequiredFieldValidator1;
-  protected HtmlTable FormGrid;
-  protected FormGridManager fgm;
-  protected BocTextValue field;
-  protected HtmlHeadContents HtmlHeadContents;
-
-	#region Web Form Designer generated code
-
-	override protected void OnInit(EventArgs e)
-	{
-		//
-		// CODEGEN: This call is required by the ASP.NET Web Form Designer.
-		//
-		InitializeComponent();
-		base.OnInit(e);
-  }
-	
-	/// <summary>
-	/// Required method for Designer support - do not modify
-	/// the contents of this method with the code editor.
-	/// </summary>
-	private void InitializeComponent()
-	{
-    this.PostBackButton.Click += new System.EventHandler(this.PostBackButton_Click);
-
-  }
-	#endregion
-
-  protected override void OnLoad(EventArgs e)
+  public class TestForm : Page
   {
-    base.OnLoad (e);
-
-    if (! IsPostBack)
-      TextField.Text = "Foo Bar";
-  
-    bool ensure = true;
-    if (ensure)
-      LazyContainer.Ensure();
   }
-
-  public override void Validate()
-  {
-    base.Validate ();
-  }
-
-  private void PostBackButton_Click(object sender, EventArgs e)
-  {
-  
-  }
-
-
-}
-
 }
