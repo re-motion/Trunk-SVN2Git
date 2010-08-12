96a42585920c77b4438f19d7fe6ae61ce56f276a
matthias.goertler <matthias.goertler@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-08-12 15:20:15 +0000

    extended TestDomain added integration tests for object binding adapted build file (RM-3139)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@18413 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/ObjectBinding/UnitTests/Core/BindableObject/SecurityIntegrationTest.cs
@@ -0,0 +1,190 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Remotion.Mixins;
+using Remotion.ObjectBinding.UnitTests.Core.TestDomain;
+using Remotion.Reflection;
+using Remotion.Security;
+using Remotion.Security.Configuration;
+using Rhino.Mocks;
+
+namespace Remotion.ObjectBinding.UnitTests.Core.BindableObject
+{
+  [TestFixture]
+  public class SecurityIntegrationTest
+  {
+    private ISecurityProvider _securityProviderStub;
+    private IPrincipalProvider _principalProviderStub;
+    private ISecurityPrincipal _securityPrincipalStub;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _securityProviderStub = MockRepository.GenerateStub<ISecurityProvider> ();
+      _principalProviderStub = MockRepository.GenerateStub<IPrincipalProvider> ();
+      _securityPrincipalStub = MockRepository.GenerateStub<ISecurityPrincipal> ();
+
+      _principalProviderStub.Stub (stub => stub.GetPrincipal ()).Return (_securityPrincipalStub);
+      
+      SecurityConfiguration.Current.SecurityProvider = _securityProviderStub;
+      SecurityConfiguration.Current.PrincipalProvider = _principalProviderStub;
+    }
+
+    [Test]
+    public void AccessGranted_PropertyWithDefaultPermission_IsReadonly ()
+    {
+      var securityContextStub = MockRepository.GenerateStub<ISecurityContext> ();
+      var securityContextFactoryStub = MockRepository.GenerateStub<ISecurityContextFactory> ();
+
+      securityContextFactoryStub.Stub (mock => mock.CreateSecurityContext ()).Return (securityContextStub);
+
+      _securityProviderStub.Stub (mock => mock.GetAccess (securityContextStub, _securityPrincipalStub)).Return (new[] { AccessType.Get (GeneralAccessTypes.Read) });
+
+      AdapterRegistry.Instance.SetAdapter (typeof (IObjectSecurityAdapter), new ObjectSecurityAdapter ());
+
+      IBusinessObject bindableSecurableObject;
+      using (new SecurityFreeSection ())
+      {
+        bindableSecurableObject = (IBusinessObject) ObjectFactory.Create (
+            false, typeof (SecurableClassWithProperties), ParamList.Create (new ObjectSecurityStrategy (securityContextFactoryStub)));
+      }
+      var property = bindableSecurableObject.BusinessObjectClass.GetPropertyDefinition ("PropertyWithDefaultPermission");
+
+      Assert.That (property.IsReadOnly (bindableSecurableObject), Is.True);
+    }
+
+    [Test]
+    public void AccessGranted_PropertyWithDefaultPermission_IsAccessible ()
+    {
+      var securityContextStub = MockRepository.GenerateStub<ISecurityContext> ();
+      var securityContextFactoryStub = MockRepository.GenerateStub<ISecurityContextFactory> ();
+
+      securityContextFactoryStub.Stub (mock => mock.CreateSecurityContext ()).Return (securityContextStub);
+
+      _securityProviderStub.Stub (mock => mock.GetAccess (securityContextStub, _securityPrincipalStub)).Return (new[] { AccessType.Get (GeneralAccessTypes.Read) });
+
+      AdapterRegistry.Instance.SetAdapter (typeof (IObjectSecurityAdapter), new ObjectSecurityAdapter ());
+
+      IBusinessObject bindableSecurableObject;
+      using (new SecurityFreeSection ())
+      {
+        bindableSecurableObject = (IBusinessObject) ObjectFactory.Create (
+            false, typeof (SecurableClassWithProperties), ParamList.Create (new ObjectSecurityStrategy (securityContextFactoryStub)));
+      }
+      var property = bindableSecurableObject.BusinessObjectClass.GetPropertyDefinition ("PropertyWithDefaultPermission");
+
+      Assert.That (property.IsAccessible (null, bindableSecurableObject), Is.True);
+    }
+
+    [Test]
+    public void AccessGranted_ReadOnlyProperty_IsReadonly ()
+    {
+      var securityContextStub = MockRepository.GenerateStub<ISecurityContext> ();
+      var securityContextFactoryStub = MockRepository.GenerateStub<ISecurityContextFactory> ();
+
+      securityContextFactoryStub.Stub (mock => mock.CreateSecurityContext ()).Return (securityContextStub);
+
+      _securityProviderStub.Stub (mock => mock.GetAccess (securityContextStub, _securityPrincipalStub)).Return (new[] { AccessType.Get (GeneralAccessTypes.Read) });
+
+      AdapterRegistry.Instance.SetAdapter (typeof (IObjectSecurityAdapter), new ObjectSecurityAdapter ());
+
+      IBusinessObject bindableSecurableObject;
+      using (new SecurityFreeSection ())
+      {
+        bindableSecurableObject = (IBusinessObject) ObjectFactory.Create (
+            false, typeof (SecurableClassWithProperties), ParamList.Create (new ObjectSecurityStrategy (securityContextFactoryStub)));
+      }
+      var property = bindableSecurableObject.BusinessObjectClass.GetPropertyDefinition ("PropertyWithReadPermission");
+
+      Assert.That (property.IsReadOnly (bindableSecurableObject), Is.True);
+    }
+
+    [Test]
+    public void NoAccessGranted_ReadOnlyProperty_IsAccessible ()
+    {
+      var securityContextStub = MockRepository.GenerateStub<ISecurityContext> ();
+      var securityContextFactoryStub = MockRepository.GenerateStub<ISecurityContextFactory> ();
+
+      securityContextFactoryStub.Stub (mock => mock.CreateSecurityContext ()).Return (securityContextStub);
+
+      _securityProviderStub.Stub (mock => mock.GetAccess (securityContextStub, _securityPrincipalStub)).Return (new[] { AccessType.Get (GeneralAccessTypes.Read) });
+
+      AdapterRegistry.Instance.SetAdapter (typeof (IObjectSecurityAdapter), new ObjectSecurityAdapter ());
+
+      IBusinessObject bindableSecurableObject;
+      using (new SecurityFreeSection ())
+      {
+        bindableSecurableObject = (IBusinessObject) ObjectFactory.Create (
+            false, typeof (SecurableClassWithProperties), ParamList.Create (new ObjectSecurityStrategy (securityContextFactoryStub)));
+      }
+      var property = bindableSecurableObject.BusinessObjectClass.GetPropertyDefinition ("PropertyWithReadPermission");
+
+      Assert.That (property.IsAccessible (null, bindableSecurableObject), Is.False);
+    }
+
+    [Test]
+    public void AccessGranted_PropertyWithWriteAccess_IsReadonly ()
+    {
+      var securityContextStub = MockRepository.GenerateStub<ISecurityContext> ();
+      var securityContextFactoryStub = MockRepository.GenerateStub<ISecurityContextFactory> ();
+
+      securityContextFactoryStub.Stub (mock => mock.CreateSecurityContext ()).Return (securityContextStub);
+
+      _securityProviderStub.Stub (mock => mock.GetAccess (securityContextStub, _securityPrincipalStub)).Return (new[] { AccessType.Get (GeneralAccessTypes.Read) });
+
+      AdapterRegistry.Instance.SetAdapter (typeof (IObjectSecurityAdapter), new ObjectSecurityAdapter ());
+
+      IBusinessObject bindableSecurableObject;
+      using (new SecurityFreeSection ())
+      {
+        bindableSecurableObject = (IBusinessObject) ObjectFactory.Create (
+            false, typeof (SecurableClassWithProperties), ParamList.Create (new ObjectSecurityStrategy (securityContextFactoryStub)));
+      }
+      var property = bindableSecurableObject.BusinessObjectClass.GetPropertyDefinition ("PropertyWithWritePermission");
+
+      Assert.That (property.IsReadOnly (bindableSecurableObject), Is.True);
+    }
+
+    [Test]
+    public void AccessGranted_PropertyWithWriteAccess_IsAccessible ()
+    {
+      var securityContextStub = MockRepository.GenerateStub<ISecurityContext> ();
+      var securityContextFactoryStub = MockRepository.GenerateStub<ISecurityContextFactory> ();
+
+      securityContextFactoryStub.Stub (mock => mock.CreateSecurityContext ()).Return (securityContextStub);
+
+      _securityProviderStub.Stub (mock => mock.GetAccess (securityContextStub, _securityPrincipalStub)).Return (new[] { AccessType.Get (GeneralAccessTypes.Read) });
+
+      AdapterRegistry.Instance.SetAdapter (typeof (IObjectSecurityAdapter), new ObjectSecurityAdapter ());
+
+      IBusinessObject bindableSecurableObject;
+      using (new SecurityFreeSection ())
+      {
+        bindableSecurableObject = (IBusinessObject) ObjectFactory.Create (
+            false, typeof (SecurableClassWithProperties), ParamList.Create (new ObjectSecurityStrategy (securityContextFactoryStub)));
+      }
+      var property = bindableSecurableObject.BusinessObjectClass.GetPropertyDefinition ("PropertyWithWritePermission");
+
+      Assert.That (property.IsAccessible (null, bindableSecurableObject), Is.True);
+    }
+
+    //mixed
+    //derived
+  }
+}
---
+++ Remotion/ObjectBinding/UnitTests/Core/TestDomain/SecurableClassWithProperties.cs
@@ -0,0 +1,61 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using Remotion.Security;
+
+namespace Remotion.ObjectBinding.UnitTests.Core.TestDomain
+{
+  [BindableObject]
+  public class SecurableClassWithProperties : ISecurableObject
+  {
+    private readonly IObjectSecurityStrategy _securityStrategy;
+    private string _readOnylProperty = string.Empty;
+    private string _accessibleProperty = string.Empty;
+
+    public SecurableClassWithProperties (IObjectSecurityStrategy securityStrategy)
+    {
+      _securityStrategy = securityStrategy;
+    }
+
+    public string PropertyWithDefaultPermission { get; set; }
+
+    [DemandPropertyReadPermission (TestAccessTypes.First)]
+    public string PropertyWithReadPermission
+    {
+      get { return _readOnylProperty; }
+      set { _readOnylProperty = value; }
+    }
+
+    [DemandPropertyWritePermission (TestAccessTypes.Second)]
+    public string PropertyWithWritePermission
+    {
+      get { return _accessibleProperty; }
+      set { _accessibleProperty = value; }
+    }
+
+
+    public IObjectSecurityStrategy GetSecurityStrategy ()
+    {
+      return _securityStrategy;
+    }
+
+    public Type GetSecurableType ()
+    {
+      return GetType();
+    }
+  }
+}
