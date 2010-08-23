e3c3af4e181d3ea4af8f0312b666aaf6266dcb9a
sascha.gindele <sascha.gindele@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-08-23 13:48:24 +0000

    rm-3172: upgrade IronPython version to 2.6.1
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@18521 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Core/Scripting.UnitTests/ScriptEnvironmentTest.cs
+++ Remotion/Core/Scripting.UnitTests/ScriptEnvironmentTest.cs
@@ -15,6 +15,7 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System;
+using IronPython.Runtime;
 using Microsoft.Scripting.Runtime;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
