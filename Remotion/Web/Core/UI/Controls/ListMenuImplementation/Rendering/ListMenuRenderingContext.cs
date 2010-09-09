477d96cc7b827c6f6fbf8a866fff00d69f294cc7
andreas.obkircher <andreas.obkircher@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-09-09 13:22:41 +0000

    added ListMenuRenderingContext and used it in ListMenuRenderer methods where it is needed (RM-3242)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@18723 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/Core/UI/Controls/ListMenuImplementation/Rendering/ListMenuRenderingContext.cs
@@ -0,0 +1,30 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Web;
+using System.Web.UI;
+
+namespace Remotion.Web.UI.Controls.ListMenuImplementation.Rendering
+{
+  public class ListMenuRenderingContext : RenderingContext<IListMenu>
+  {
+    public ListMenuRenderingContext (HttpContextBase httpContext, HtmlTextWriter writer, IStyledControl control)
+        : base(httpContext, writer, control)
+    {
+    }
+  }
+}
