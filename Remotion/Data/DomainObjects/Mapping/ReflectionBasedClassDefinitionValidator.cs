de500047d9312de05db2683be9367248cbbb8e07
andreas.obkircher <andreas.obkircher@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-10-13 07:41:02 +0000

    removed unnecessary references
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@19025 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/DomainObjects/Mapping/ReflectionBasedClassDefinitionValidator.cs
+++ Remotion/Data/DomainObjects/Mapping/ReflectionBasedClassDefinitionValidator.cs
@@ -15,10 +15,6 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System;
-using Remotion.Collections;
-using Remotion.Mixins;
-using Remotion.Mixins.Context;
-using Remotion.Text;
 
 namespace Remotion.Data.DomainObjects.Mapping
 {
