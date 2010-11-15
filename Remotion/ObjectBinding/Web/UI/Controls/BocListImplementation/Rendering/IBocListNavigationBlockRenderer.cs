0af8b7852546ac78a7375d9b93b0c589c23752c1
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-11-15 09:42:26 +0000

    Updated documentation.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@19289 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/ObjectBinding/Web/UI/Controls/BocListImplementation/Rendering/IBocListNavigationBlockRenderer.cs
+++ Remotion/ObjectBinding/Web/UI/Controls/BocListImplementation/Rendering/IBocListNavigationBlockRenderer.cs
@@ -20,7 +20,7 @@ using Remotion.Implementation;
 namespace Remotion.ObjectBinding.Web.UI.Controls.BocListImplementation.Rendering
 {
   /// <summary>
-  /// Interface for classes responsible for rendering the navigation block of a <see cref="IBocList"/> control.
+  /// Interface for classes responsible for rendering the navigation block of <see cref="IBocList"/> controls.
   /// </summary>
   [ConcreteImplementation (typeof (BocListNavigationBlockRenderer), Lifetime = LifetimeKind.Singleton)]
   public interface IBocListNavigationBlockRenderer
