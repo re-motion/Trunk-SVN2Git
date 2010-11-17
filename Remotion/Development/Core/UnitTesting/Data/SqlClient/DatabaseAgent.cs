a3cb47fc9d0e0a665a5dfecc318851dce65f60ff
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-11-17 07:09:58 +0000

    Updated DatabaseAgent to use ordinal string comparison.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@19332 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Development/Core/UnitTesting/Data/SqlClient/DatabaseAgent.cs
+++ Remotion/Development/Core/UnitTesting/Data/SqlClient/DatabaseAgent.cs
@@ -182,7 +182,7 @@ namespace Remotion.Development.UnitTesting.Data.SqlClient
       var command = new BatchCommand (lineNumber, commandBatch.Length);
       foreach (var line in commandBatch.Split (new[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries))
       {
-        if (line.Trim ().Equals ("GO", StringComparison.CurrentCultureIgnoreCase))
+        if (line.Trim ().Equals ("GO", StringComparison.OrdinalIgnoreCase))
         {
           var batch = command;
           command = new BatchCommand (lineNumber + 1, commandBatch.Length);
