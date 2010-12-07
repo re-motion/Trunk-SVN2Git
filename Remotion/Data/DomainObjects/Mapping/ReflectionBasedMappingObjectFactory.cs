692e0024a69501b053146b041614a984184004c4
andreas.obkircher <andreas.obkircher@cb0deee2-328e-b949-94ea-fb3139fab790>
2010-12-07 16:37:54 +0000

    added IMappingObjectFactory and ReflectionBasedMappingObjectFactory (RM-3554)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@19545 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Data/DomainObjects/Mapping/ReflectionBasedMappingObjectFactory.cs
@@ -0,0 +1,54 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using Remotion.Data.DomainObjects.ConfigurationLoader.ReflectionBasedConfigurationLoader;
+using Remotion.Utilities;
+
+namespace Remotion.Data.DomainObjects.Mapping
+{
+  /// <summary>
+  /// The <see cref="ReflectionBasedMappingObjectFactory"/> is used to create new mapping objects.
+  /// </summary>
+  public class ReflectionBasedMappingObjectFactory : IMappingObjectFactory
+  {
+    private readonly IMappingNameResolver _mappingNameResolver;
+
+    public ReflectionBasedMappingObjectFactory (IMappingNameResolver mappingNameResolver)
+    {
+      ArgumentUtility.CheckNotNull ("mappingNameResolver", mappingNameResolver);
+
+      _mappingNameResolver = mappingNameResolver;
+    }
+
+    public ClassDefinition CreateClassDefinition (Type type, ReflectionBasedClassDefinition baseClass)
+    {
+      ArgumentUtility.CheckNotNull ("type", type);
+
+      var classReflector = new ClassReflector (type, _mappingNameResolver);
+      return classReflector.GetMetadata (baseClass);
+    }
+
+    public ClassDefinitionCollection CreateClassDefinitionCollection (IEnumerable<Type> types)
+    {
+      ArgumentUtility.CheckNotNull ("types", types);
+
+      var classDefinitionCollectionFactory = new ClassDefinitionCollectionFactory (_mappingNameResolver);
+      return classDefinitionCollectionFactory.CreateClassDefinitionCollection (types);
+    }
+  }
+}
