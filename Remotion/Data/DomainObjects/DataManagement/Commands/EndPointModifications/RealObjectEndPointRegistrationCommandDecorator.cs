b35b519f99ddfb2d2939d933ad30ced6bc7bdb39
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-04-01 12:54:56 +0000

    Added RealObjectEndPointRegistrationCommandDecorator (RM-3835)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@20357 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Data/DomainObjects/DataManagement/Commands/EndPointModifications/RealObjectEndPointRegistrationCommandDecorator.cs
@@ -0,0 +1,108 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using Remotion.Utilities;
+
+namespace Remotion.Data.DomainObjects.DataManagement.Commands.EndPointModifications
+{
+  /// <summary>
+  /// Decorates an <see cref="IDataManagementCommand"/> and adds functionality for registering/unregistering with the opposite of a 
+  /// <see cref="RealObjectEndPoint"/>.
+  /// </summary>
+  public class RealObjectEndPointRegistrationCommandDecorator : IDataManagementCommand
+  {
+    private readonly IDataManagementCommand _decoratedCommand;
+    private readonly IRealObjectEndPoint _realObjectEndPoint;
+    private readonly IVirtualEndPoint _oldRelatedEndPoint;
+    private readonly IVirtualEndPoint _newRelatedEndPoint;
+
+    public RealObjectEndPointRegistrationCommandDecorator (
+        IDataManagementCommand decoratedCommand,
+        IRealObjectEndPoint realObjectEndPoint,
+        IVirtualEndPoint oldRelatedEndPoint,
+        IVirtualEndPoint newRelatedEndPoint)
+    {
+      ArgumentUtility.CheckNotNull ("decoratedCommand", decoratedCommand);
+      ArgumentUtility.CheckNotNull ("realObjectEndPoint", realObjectEndPoint);
+      ArgumentUtility.CheckNotNull ("oldRelatedEndPoint", oldRelatedEndPoint);
+      ArgumentUtility.CheckNotNull ("newRelatedEndPoint", newRelatedEndPoint);
+
+      _decoratedCommand = decoratedCommand;
+      _realObjectEndPoint = realObjectEndPoint;
+      _oldRelatedEndPoint = oldRelatedEndPoint;
+      _newRelatedEndPoint = newRelatedEndPoint;
+    }
+
+    public IDataManagementCommand DecoratedCommand
+    {
+      get { return _decoratedCommand; }
+    }
+
+    public IRealObjectEndPoint RealObjectEndPoint
+    {
+      get { return _realObjectEndPoint; }
+    }
+
+    public IVirtualEndPoint OldRelatedEndPoint
+    {
+      get { return _oldRelatedEndPoint; }
+    }
+
+    public IVirtualEndPoint NewRelatedEndPoint
+    {
+      get { return _newRelatedEndPoint; }
+    }
+
+    public void NotifyClientTransactionOfBegin ()
+    {
+      _decoratedCommand.NotifyClientTransactionOfBegin();
+    }
+
+    public void Begin ()
+    {
+      _decoratedCommand.Begin();
+    }
+
+    public void Perform ()
+    {
+      _oldRelatedEndPoint.UnregisterCurrentOppositeEndPoint (_realObjectEndPoint);
+      _decoratedCommand.Perform();
+      _newRelatedEndPoint.RegisterCurrentOppositeEndPoint (_realObjectEndPoint);
+    }
+
+    public void End ()
+    {
+      _decoratedCommand.End ();
+    }
+
+    public void NotifyClientTransactionOfEnd ()
+    {
+      _decoratedCommand.NotifyClientTransactionOfEnd ();
+    }
+
+    public ExpandedCommand ExpandToAllRelatedObjects ()
+    {
+      var innerExpandedCommand = _decoratedCommand.ExpandToAllRelatedObjects();
+      var decoratedExpandedCommand = new RealObjectEndPointRegistrationCommandDecorator (
+          innerExpandedCommand, 
+          _realObjectEndPoint, 
+          _oldRelatedEndPoint, 
+          _newRelatedEndPoint);
+      return new ExpandedCommand (decoratedExpandedCommand);
+    }
+  }
+}
---
+++ Remotion/Data/UnitTests/DomainObjects/Core/DataManagement/Commands/EndPointModifications/RealObjectEndPointRegistrationCommandDecoratorTest.cs
@@ -0,0 +1,116 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Remotion.Data.DomainObjects.DataManagement;
+using Remotion.Data.DomainObjects.DataManagement.Commands;
+using Remotion.Data.DomainObjects.DataManagement.Commands.EndPointModifications;
+using Rhino.Mocks;
+
+namespace Remotion.Data.UnitTests.DomainObjects.Core.DataManagement.Commands.EndPointModifications
+{
+  [TestFixture]
+  public class RealObjectEndPointRegistrationCommandDecoratorTest
+  {
+    private MockRepository _mockRepository;
+
+    private IDataManagementCommand _decoratedCommandMock;
+    private IRealObjectEndPoint _realObjectEndPointStub;
+    private IVirtualEndPoint _oldRelatedEndPointMock;
+    private IVirtualEndPoint _newRelatedEndPointMock;
+    
+    private RealObjectEndPointRegistrationCommandDecorator _decorator;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _mockRepository = new MockRepository();
+
+      _decoratedCommandMock = _mockRepository.StrictMock<IDataManagementCommand> ();
+      _realObjectEndPointStub = _mockRepository.Stub<IRealObjectEndPoint> ();
+      _oldRelatedEndPointMock = _mockRepository.StrictMock<IVirtualEndPoint> ();
+      _newRelatedEndPointMock = _mockRepository.StrictMock<IVirtualEndPoint> ();
+
+      _decorator = new RealObjectEndPointRegistrationCommandDecorator (
+          _decoratedCommandMock, 
+          _realObjectEndPointStub, 
+          _oldRelatedEndPointMock, 
+          _newRelatedEndPointMock);
+    }
+
+    [Test]
+    public void Perform ()
+    {
+      using (_mockRepository.Ordered ())
+      {
+        _oldRelatedEndPointMock.Expect (mock => mock.UnregisterCurrentOppositeEndPoint (_realObjectEndPointStub));
+        _decoratedCommandMock.Expect (mock => mock.Perform());
+        _newRelatedEndPointMock.Expect (mock => mock.RegisterCurrentOppositeEndPoint (_realObjectEndPointStub));
+      }
+
+      _mockRepository.ReplayAll();
+
+      _decorator.Perform ();
+
+      _mockRepository.VerifyAll();
+    }
+
+    [Test]
+    public void ExpandToAllRelatedObjects ()
+    {
+      var fakeExpandedCommand = new ExpandedCommand();
+      _decoratedCommandMock
+          .Stub (stub => stub.ExpandToAllRelatedObjects())
+          .Return (fakeExpandedCommand);
+      _mockRepository.ReplayAll();
+
+      var result = _decorator.ExpandToAllRelatedObjects();
+
+      Assert.That (result, Is.Not.Null);
+      
+      var nestedCommands = result.GetNestedCommands ();
+      Assert.That (nestedCommands.Count, Is.EqualTo (1));
+      Assert.That (nestedCommands[0], Is.TypeOf (typeof (RealObjectEndPointRegistrationCommandDecorator)));
+      
+      var innerExpandedCommand = (RealObjectEndPointRegistrationCommandDecorator) nestedCommands[0];
+      Assert.That (innerExpandedCommand.DecoratedCommand, Is.SameAs (fakeExpandedCommand));
+      Assert.That (innerExpandedCommand.RealObjectEndPoint, Is.SameAs (_realObjectEndPointStub));
+      Assert.That (innerExpandedCommand.OldRelatedEndPoint, Is.SameAs (_oldRelatedEndPointMock));
+      Assert.That (innerExpandedCommand.NewRelatedEndPoint, Is.SameAs (_newRelatedEndPointMock));
+    }
+
+    [Test]
+    public void DelegatingMembers ()
+    {
+      CheckOperationIsDelegated (c => c.NotifyClientTransactionOfBegin ());
+      CheckOperationIsDelegated (c => c.NotifyClientTransactionOfEnd ());
+      CheckOperationIsDelegated (c => c.Begin ());
+      CheckOperationIsDelegated (c => c.End ());
+    }
+
+    private void CheckOperationIsDelegated (Action<IDataManagementCommand> action)
+    {
+      _decoratedCommandMock.Expect (action);
+      _mockRepository.ReplayAll();
+
+      action (_decorator);
+
+      _mockRepository.VerifyAll ();
+    }
+  }
+}
