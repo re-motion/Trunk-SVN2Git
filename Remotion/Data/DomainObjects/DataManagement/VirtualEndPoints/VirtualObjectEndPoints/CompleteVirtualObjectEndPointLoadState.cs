4290043d7bf30e5f53c884c8ca81139ad6fc0e2f
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-04-13 11:04:09 +0000

    Removed obsolete comment (RM-3894)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@20459 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/DomainObjects/DataManagement/VirtualEndPoints/VirtualObjectEndPoints/CompleteVirtualObjectEndPointLoadState.cs
+++ Remotion/Data/DomainObjects/DataManagement/VirtualEndPoints/VirtualObjectEndPoints/CompleteVirtualObjectEndPointLoadState.cs
@@ -65,12 +65,7 @@ namespace Remotion.Data.DomainObjects.DataManagement.VirtualEndPoints.VirtualObj
       ArgumentUtility.CheckNotNull ("endPoint", endPoint);
       ArgumentUtility.CheckNotNull ("stateSetter", stateSetter);
 
-      // In IncompleteVirtualObjectEndPointLoadState, we automatically call MarkDataComplete when an original opposite end-point is registered in
-      // order to optimize away one virtual end-point query. That implicit call to MarkDataComplete would, however, cause the subsequent call
-      // to MarkDataComplete in DataManager.LoadLazyVirtualObjectEndPoint to fail. Therefore, we check whether the MarkDataComplete call here
-      // is "okay" and only throw (in the base MarkDataComplete call) if it isn't.
-      // if (DataKeeper.CurrentOppositeObject != item)
-        MarkDataComplete (endPoint, new[] { item }, stateSetter);
+      MarkDataComplete (endPoint, new[] { item }, stateSetter);
     }
 
     public override void SynchronizeOppositeEndPoint (IRealObjectEndPoint oppositeEndPoint)
