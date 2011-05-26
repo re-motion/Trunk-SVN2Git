bebb7388e606280ced2241c3cd65fcb3627b42da
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-05-26 06:21:33 +0000

    temp commit (RM-3981)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@20772 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Core/Core/Reflection/TypeAdapter.cs
+++ Remotion/Core/Core/Reflection/TypeAdapter.cs
@@ -263,45 +263,16 @@ namespace Remotion.Reflection
       return AttributeUtility.IsDefined<T> (_type, inherited);
     }
 
-    /// <summary>
-    /// Gets the type from which the current <see cref="Type"/> directly inherits.
-    /// </summary>
-    /// <returns>
-    /// The <see cref="Type"/> from which the current <see cref="Type"/> directly inherits, 
-    /// or null if the current Type represents the <see cref="Object"/> class or an interface.
-    /// </returns>
     public ITypeInformation BaseType
     {
       get { return Maybe.ForValue (_type.BaseType).Select (TypeAdapter.Create).ValueOrDefault(); }
     }
 
-    /// <summary>
-    /// Determines whether the specified object is an instance of the current <see cref="Type"/>.
-    /// </summary>
-    /// <returns>
-    /// <see langword="true"/> if the current Type is in the inheritance hierarchy of the object represented by <paramref name="o"/>, 
-    /// or if the current Type is an interface that <paramref name="o"/> supports. <see langword="false"/> if neither of these conditions is the case, 
-    /// or if <paramref name="o"/> is <see langword="null"/>, or if the current Type is an open generic type 
-    /// (that is, <see cref="ContainsGenericParameters"/> returns <see langword="true"/>).
-    /// </returns>
-    /// <param name="o">The object to compare with the current Type. </param>
     public bool IsInstanceOfType (object o)
     {
       return _type.IsInstanceOfType (o);
     }
 
-    /// <summary>
-    /// Determines whether the class represented by the current <see cref="Type"/> derives from the class represented by the specified <see cref="Type"/>.
-    /// </summary>
-    /// <returns>
-    /// <see langword="true"/> if the Type represented by the <paramref name="c"/> parameter and the current Type represent classes, 
-    /// and the class represented by the current Type derives from the class represented by <paramref name="c"/>; otherwise, <see langword="false"/>. 
-    /// This method also returns <see langword="false"/> if <paramref name="c"/> and the current Type represent the same class.
-    /// In addition, the implementation of <paramref name="c"/> must match the implementation of this <see cref="ITypeInformation"/>,
-    /// otherwise this method will also return <see langword="false" />.
-    /// </returns>
-    /// <param name="c">The Type to compare with the current Type. </param>
-    /// <exception cref="ArgumentNullException">The <paramref name="c"/> parameter is <see langword="null"/>. </exception>
     public bool IsSubclassOf (ITypeInformation c)
     {
       ArgumentUtility.CheckNotNull ("c", c);
@@ -313,18 +284,6 @@ namespace Remotion.Reflection
       return _type.IsSubclassOf (otherTypeAsTypeAdapter.Type);
     }
 
-    /// <summary>
-    /// Determines whether an instance of the current <see cref="Type"/> can be assigned from an instance of the specified Type.
-    /// </summary>
-    /// <returns>
-    /// <see langword="true"/> if <paramref name="c"/> and the current Type represent the same type, 
-    /// or if the current Type is in the inheritance hierarchy of <paramref name="c"/>, 
-    /// or if the current Type is an interface that <paramref name="c"/> implements, 
-    /// or if <paramref name="c"/> is a generic type parameter and the current Type represents one of the constraints of <paramref name="c"/>. 
-    /// <see langword="false"/> if none of these conditions are <see langword="true"/>, or if <paramref name="c"/> is <see langword="null"/>,
-    /// or the implementation of <paramref name="c"/> does not match the implementation of this <see cref="ITypeInformation"/>.
-    /// </returns>
-    /// <param name="c">The Type to compare with the current Type. </param>
     public bool IsAssignableFrom (ITypeInformation c)
     {
       var otherTypeAsTypeAdapter = c as TypeAdapter;
