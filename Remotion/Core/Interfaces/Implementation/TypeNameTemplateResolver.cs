c31f22032dd0ff672dfafca39320e69d641b7af2
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-06-20 09:38:49 +0000

    Adapted assertion text
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@20904 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Core/Interfaces/Implementation/TypeNameTemplateResolver.cs
+++ Remotion/Core/Interfaces/Implementation/TypeNameTemplateResolver.cs
@@ -42,7 +42,7 @@ namespace Remotion.Implementation
     private static string GetPublicKeyTokenString ()
     {
       byte[] bytes = typeof (TypeNameTemplateResolver).Assembly.GetName ().GetPublicKeyToken ();
-      Trace.Assert (bytes != null && bytes.Length > 0, "Remotion.Interfaces definitely has a strong name.");
+      Trace.Assert (bytes != null && bytes.Length > 0, "Remotion.Interfaces is expected to have a strong name.");
       return string.Format ("{0:x2}{1:x2}{2:x2}{3:x2}{4:x2}{5:x2}{6:x2}{7:x2}",
           bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7]);
     }
