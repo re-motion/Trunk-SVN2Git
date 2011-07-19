255bbd276060a0d8b83113f000b3b0ee4fe2a667
andreas.obkircher <andreas.obkircher@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-07-19 07:45:22 +0000

    renamed DataContainerReader to LegacyDataReaderContainer renamed ObjectIDReader to LegacyObjectIDReader copied the legacy implementations and added new DataReaderContainer and ObjectIDReader classes (RM-4141)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@21112 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Data/DomainObjects/Persistence/Rdbms/DataReaders/DataContainerReader.cs
@@ -0,0 +1,107 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Remotion.Data.DomainObjects.DataManagement;
+using Remotion.Data.DomainObjects.Mapping;
+using Remotion.Utilities;
+
+namespace Remotion.Data.DomainObjects.Persistence.Rdbms.DataReaders
+{
+  /// <summary>
+  /// Reads data from an <see cref="IDataReader"/> and converts it into <see cref="DataContainer"/> instances.
+  /// </summary>
+  public class DataContainerReader : IDataContainerReader
+  {
+    private readonly IValueConverter _valueConverter;
+
+    public DataContainerReader (IValueConverter valueConverter)
+    {
+      ArgumentUtility.CheckNotNull ("valueConverter", valueConverter);
+
+      _valueConverter = valueConverter;
+    }
+
+    public virtual DataContainer Read (IDataReader dataReader)
+    {
+      ArgumentUtility.CheckNotNull ("dataReader", dataReader);
+
+      if (dataReader.Read())
+        return CreateDataContainerFromReader (dataReader);
+      else
+        return null;
+    }
+
+    public virtual IEnumerable<DataContainer> ReadSequence ( IDataReader dataReader)
+    {
+      ArgumentUtility.CheckNotNull ("dataReader", dataReader);
+
+      var loadedIDs = new HashSet<ObjectID>();
+
+      while (dataReader.Read())
+      {
+        var dataContainer = CreateDataContainerFromReader (dataReader);
+        if (dataContainer != null)
+          loadedIDs.Add (dataContainer.ID);
+        
+        yield return dataContainer;
+      }
+    }
+
+    protected virtual DataContainer CreateDataContainerFromReader (IDataReader dataReader)
+    {
+      ArgumentUtility.CheckNotNull ("dataReader", dataReader);
+
+      var id = _valueConverter.GetID (dataReader);
+      if (id != null)
+      {
+        var timestamp = _valueConverter.GetTimestamp (dataReader);
+
+        var dataContainer = DataContainer.CreateForExisting (
+            id,
+            timestamp,
+            propertyDefinition => GetDataValue (dataReader, propertyDefinition, id));
+        return dataContainer;
+      }
+      return null;
+    }
+
+    private object GetDataValue (IDataReader dataReader, PropertyDefinition propertyDefinition, ObjectID objectID)
+    {
+      ArgumentUtility.CheckNotNull ("dataReader", dataReader);
+      ArgumentUtility.CheckNotNull ("propertyDefinition", propertyDefinition);
+      ArgumentUtility.CheckNotNull ("objectID", objectID);
+
+      try
+      {
+        return _valueConverter.GetValue (objectID.ClassDefinition, propertyDefinition, dataReader);
+      }
+      catch (Exception e)
+      {
+        throw new RdbmsProviderException (
+            string.Format (
+                "Error while reading property '{0}' of object '{1}': {2}",
+                propertyDefinition.PropertyName,
+                objectID,
+                e.Message),
+            e);
+      }
+    }
+  }
+  
+}
---
+++ Remotion/Data/UnitTests/DomainObjects/Core/Persistence/Rdbms/DataReaders/DataContainerReaderTest.cs
@@ -0,0 +1,147 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Data;
+using System.Linq;
+using NUnit.Framework;
+using Remotion.Data.DomainObjects;
+using Remotion.Data.DomainObjects.Persistence.Rdbms;
+using Remotion.Data.DomainObjects.Persistence.Rdbms.DataReaders;
+using Remotion.Data.UnitTests.DomainObjects.TestDomain;
+using Rhino.Mocks;
+
+namespace Remotion.Data.UnitTests.DomainObjects.Core.Persistence.Rdbms.DataReaders
+{
+  [TestFixture]
+  public class DataContainerReaderTest : SqlProviderBaseTest
+  {
+    private IDataReader _dataReaderStub;
+    private DataContainerReader _factory;
+    private IValueConverter _valueConverterStub;
+    private ObjectID _objectID;
+    private object _timestamp;
+
+    [SetUp]
+    public override void SetUp ()
+    {
+      base.SetUp();
+
+      _dataReaderStub = MockRepository.GenerateStub<IDataReader>();
+      _valueConverterStub = MockRepository.GenerateStub<IValueConverter>();
+      _factory = new DataContainerReader (_valueConverterStub);
+
+      _objectID = new ObjectID ("OrderTicket", Guid.NewGuid());
+      _timestamp = new object();
+    }
+
+    [Test]
+    public void Read_DataReaderReadFalse ()
+    {
+      _dataReaderStub.Stub (stub => stub.Read()).Return (false);
+
+      var result = _factory.Read (_dataReaderStub);
+
+      Assert.That (result, Is.Null);
+    }
+
+    [Test]
+    public void Read_DataReaderReadTrue_ValueIDNotNull ()
+    {
+      _dataReaderStub.Stub (stub => stub.Read()).Return (true);
+
+      _valueConverterStub.Stub (stub => stub.GetID (_dataReaderStub)).Return (_objectID);
+      _valueConverterStub.Stub (stub => stub.GetTimestamp (_dataReaderStub)).Return (_timestamp);
+
+      StubValueConverterForProperty (typeof (OrderTicket), "FileName", _objectID);
+      StubValueConverterForProperty (typeof (OrderTicket), "Order", _objectID);
+
+      var dataContainer = _factory.Read (_dataReaderStub);
+
+      Assert.That (dataContainer, Is.Not.Null);
+      Assert.That (dataContainer.ID, Is.SameAs (_objectID));
+    }
+
+    [Test]
+    public void ReadSequence_DataReaderReadFalse ()
+    {
+      _dataReaderStub.Stub (stub => stub.Read()).Return (false);
+
+      var result = _factory.ReadSequence (_dataReaderStub);
+
+      Assert.That (result, Is.Empty);
+    }
+
+    [Test]
+    public void ReadSequence_DataReaderReadTrue()
+    {
+      StubValueConverterForProperty (typeof (OrderTicket), "FileName", DomainObjectIDs.OrderTicket1);
+      StubValueConverterForProperty (typeof (OrderTicket), "Order", DomainObjectIDs.OrderTicket1);
+      StubValueConverterForProperty (typeof (OrderTicket), "FileName", DomainObjectIDs.OrderTicket2);
+      StubValueConverterForProperty (typeof (OrderTicket), "Order", DomainObjectIDs.OrderTicket2);
+      StubValueConverterForProperty (typeof (OrderTicket), "FileName", DomainObjectIDs.OrderTicket3);
+      StubValueConverterForProperty (typeof (OrderTicket), "Order", DomainObjectIDs.OrderTicket3);
+
+      _valueConverterStub.Stub (stub => stub.GetID (_dataReaderStub)).Return (DomainObjectIDs.OrderTicket1).Repeat.Once();
+      _valueConverterStub.Stub (stub => stub.GetID (_dataReaderStub)).Return (DomainObjectIDs.OrderTicket2).Repeat.Once ();
+      _valueConverterStub.Stub (stub => stub.GetID (_dataReaderStub)).Return (DomainObjectIDs.OrderTicket3).Repeat.Once ();
+      _valueConverterStub.Stub (stub => stub.GetTimestamp (_dataReaderStub)).Return (_timestamp);
+
+      var count = 0;
+      _dataReaderStub.Stub (stub => stub.Read ()).Return (true).WhenCalled (
+          mi =>
+          {
+            count++;
+            if (count > 2)
+            {
+              _dataReaderStub.BackToRecord ();
+              _dataReaderStub.Stub (stub => stub.Read ()).Return (false);
+            }
+          });
+
+      var result = _factory.ReadSequence (_dataReaderStub).ToArray ();
+      Assert.That (result.Length, Is.EqualTo (3));
+      Assert.That (result[0].ID, Is.EqualTo(DomainObjectIDs.OrderTicket1));
+      Assert.That (result[1].ID, Is.EqualTo(DomainObjectIDs.OrderTicket2));
+      Assert.That (result[2].ID, Is.EqualTo(DomainObjectIDs.OrderTicket3));
+    }
+
+    [Test]
+    public void ReadSequence_DataReaderReadTrue_NullIDIsReturned_Supported ()
+    {
+      _dataReaderStub.Stub (stub => stub.Read ()).Return (true).WhenCalled (
+          mi =>
+          {
+            _dataReaderStub.BackToRecord ();
+            _dataReaderStub.Stub (stub => stub.Read ()).Return (false);
+          });
+
+      var result = _factory.ReadSequence (_dataReaderStub).ToArray ();
+
+      Assert.That (result.Length, Is.EqualTo (1));
+      Assert.That (result[0], Is.Null);
+    }
+
+    public void StubValueConverterForProperty (Type declaringType, string shortPropertyName, ObjectID objectID)
+    {
+      var propertyDefinition = GetPropertyDefinition (declaringType, shortPropertyName);
+      _valueConverterStub
+          .Stub (stub => stub.GetValue (objectID.ClassDefinition, propertyDefinition, _dataReaderStub))
+          .Return (propertyDefinition.DefaultValue);
+    }
+    
+  }
+}
