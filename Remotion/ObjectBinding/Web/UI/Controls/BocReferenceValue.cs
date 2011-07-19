0a7a9e65d946cea959d52d3f82ba3d0ed0fbc06a
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-07-19 15:13:25 +0000

    Changed BocReferenceValue to reset dirty-state during SaveValue even when the control does not have a parent object or is set ReadOnly. (RM-4174)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@21125 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/ObjectBinding/Web/UI/Controls/BocReferenceValue.cs
+++ Remotion/ObjectBinding/Web/UI/Controls/BocReferenceValue.cs
@@ -403,13 +403,21 @@ namespace Remotion.ObjectBinding.Web.UI.Controls
     /// <include file='doc\include\UI\Controls\BocReferenceValue.xml' path='BocReferenceValue/SaveValue/*' />
     public override void SaveValue (bool interim)
     {
-      if (!interim && IsDirty)
+      if (interim)
+        return;
+
+      if (Property == null)
+        return;
+
+      if (DataSource == null)
+        return;
+
+      if (IsDirty)
       {
-        if (Property != null && DataSource != null && DataSource.BusinessObject != null && ! IsReadOnly)
-        {
+        if (DataSource.BusinessObject != null && !IsReadOnly)
           DataSource.BusinessObject.SetProperty (Property, Value);
-          IsDirty = false;
-        }
+
+        IsDirty = false;
       }
     }
 
