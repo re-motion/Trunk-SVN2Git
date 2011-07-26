9787a980a6e8a84e790b0be83bdcb38c1a59cdfa
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-07-26 06:15:11 +0000

    Updated documentation. (RM-4104)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@21213 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/ObjectBinding/Core/BindableObject/IDefaultValueService.cs
+++ Remotion/ObjectBinding/Core/BindableObject/IDefaultValueService.cs
@@ -14,14 +14,66 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
+using System;
+using JetBrains.Annotations;
+
 namespace Remotion.ObjectBinding.BindableObject
 {
+  /// <summary>
+  /// <see cref="IDefaultValueService"/> defines the interface required for creating an <see cref="IBusinessObject"/> instance 
+  /// that is assigned to the specified <see cref="IBusinessObjectReferenceProperty"/> as its default value.
+  /// </summary>
+  /// <remarks>
+  /// You can register a service-instance with the <see cref="BusinessObjectProvider"/>'s <see cref="IBusinessObjectProvider.AddService"/> method 
+  /// using either the <see cref="IDefaultValueService"/> type or a derived type as the key to identify this service. If you register a service using 
+  /// a derived type, you also have to apply the <see cref="DefaultValueServiceTypeAttribute"/> to the bindable object for which the service is intended.
+  /// </remarks>
   public interface IDefaultValueService : IBusinessObjectService
   {
-    bool SupportsProperty (IBusinessObjectReferenceProperty property);
+    /// <summary>
+    /// Gets a flag that describes whether the serivce can be used to create a default value for this property.
+    /// </summary>
+    /// <param name="property">The <see cref="IBusinessObjectReferenceProperty"/> to be tested. Must not be <see langword="null" />.</param>
+    /// <returns><see langword="true" /> if the default value service is compatible with this <paramref name="property"/>.</returns>
+    bool SupportsProperty ([NotNull] IBusinessObjectReferenceProperty property);
 
-    IBusinessObject Create (IBusinessObject referencingObject, IBusinessObjectReferenceProperty property);
+    /// <summary>
+    /// Creates an <see cref="IBusinessObject"/> instance that can be assigned as the default value to the <paramref name="referencingObject"/> 
+    /// via the <paramref name="property"/>.
+    /// </summary>
+    /// <param name="referencingObject">
+    /// The <see cref="IBusinessObject"/> instance the default value will be assigned to. Can be <see langword="null" />.
+    /// </param>
+    /// <param name="property">
+    /// The <see cref="IBusinessObjectReferenceProperty"/> that will hold the reference to the default value. Must not be <see langword="null" />.
+    /// </param>
+    /// <returns>A new <see cref="IBusinessObject" /> instance that can be used as the default value for the <paramref name="property"/>.</returns>
+    IBusinessObject Create ([CanBeNull] IBusinessObject referencingObject, [NotNull] IBusinessObjectReferenceProperty property);
 
-    bool IsDefaultValue (IBusinessObject referencingObject, IBusinessObjectReferenceProperty property, IBusinessObject value, IBusinessObjectProperty[] emptyProperties);
+    /// <summary>
+    /// Evaluates whether the <paramref name="value"/> is equivalent to the default value returned by the <see cref="Create"/> method.
+    /// </summary>
+    /// <param name="referencingObject">
+    /// The <see cref="IBusinessObject"/> instance the <paramref name="value"/> is assigned to. Can be <see langword="null" />.
+    /// </param>
+    /// <param name="property">
+    /// The <see cref="IBusinessObjectReferenceProperty"/> that holds the reference to the <paramref name="value"/>. Must not be <see langword="null" />.
+    /// </param>
+    /// <param name="value">The <see cref="IBusinessObject"/> instance to be evaluated.</param>
+    /// <param name="emptyProperties">The list of properties that would be cleared during the data binding's write back operation. Must not be <see langword="null" />.</param>
+    /// <returns>
+    ///   <see langword="true"/> if the <paramref name="value"/> is equivalent to the <see cref="IBusinessObject"/> instance that would be returned
+    ///   when calling the <see cref="Create"/> method.
+    /// </returns>
+    /// <remarks>
+    ///   The evaluation is intended to determine whether the <paramref name="value"/> can be recreated by calling the <see cref="Create"/> method.
+    ///   See <see cref="IBusinessObjectReferenceProperty.IsDefaultValue">IBusinessObjectReferenceProperty.IsDefaultValue</see> for additonal 
+    ///   information on how this method is used by the data binding infrastructure.
+    /// </remarks>
+    bool IsDefaultValue (
+        [CanBeNull] IBusinessObject referencingObject,
+        [NotNull] IBusinessObjectReferenceProperty property,
+        [NotNull] IBusinessObject value,
+        [NotNull] IBusinessObjectProperty[] emptyProperties);
   }
 }
