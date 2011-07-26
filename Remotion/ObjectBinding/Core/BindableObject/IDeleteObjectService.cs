312cd5abbe73fae2e0610a05e49b8b2c7d8e96d1
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-07-26 06:15:32 +0000

    Updated documentation. (RM-4142)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@21214 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/ObjectBinding/Core/BindableObject/IDeleteObjectService.cs
+++ Remotion/ObjectBinding/Core/BindableObject/IDeleteObjectService.cs
@@ -15,12 +15,38 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System;
+using JetBrains.Annotations;
 
 namespace Remotion.ObjectBinding.BindableObject
 {
+  /// <summary>
+  /// <see cref="IDeleteObjectService"/> defines the interface required for deleting the <see cref="IBusinessObject"/> instance 
+  /// that is assigned to the specified <see cref="IBusinessObjectReferenceProperty"/>.
+  /// </summary>
+  /// <remarks>
+  /// You can register a service-instance with the <see cref="BusinessObjectProvider"/>'s <see cref="IBusinessObjectProvider.AddService"/> method 
+  /// using either the <see cref="IDeleteObjectService"/> type or a derived type as the key to identify this service. If you register a service using 
+  /// a derived type, you also have to apply the <see cref="DeleteObjectServiceTypeAttribute"/> to the bindable object for which the service is intended.
+  /// </remarks>
   public interface IDeleteObjectService : IBusinessObjectService
   {
-    bool SupportsProperty (IBusinessObjectReferenceProperty property);
-    void Delete (IBusinessObject referencingObject, IBusinessObjectReferenceProperty property, IBusinessObject value);
+    /// <summary>
+    /// Gets a flag that describes whether the serivce can be used to delete the object assigned to this property.
+    /// </summary>
+    /// <param name="property">The <see cref="IBusinessObjectReferenceProperty"/> to be tested. Must not be <see langword="null" />.</param>
+    /// <returns><see langword="true" /> if the delete object service is compatible with this <paramref name="property"/>.</returns>
+    bool SupportsProperty ([NotNull] IBusinessObjectReferenceProperty property);
+
+    /// <summary>
+    /// Deletes the <paramref name="value"/> that is assigned to the <paramref name="referencingObject"/> via the <paramref name="property"/>.
+    /// </summary>
+    /// <param name="referencingObject">
+    /// The <see cref="IBusinessObject"/> instance the <paramref name="value"/> is assigned to. Can be <see langword="null" />.
+    /// </param>
+    /// <param name="property">
+    /// The <see cref="IBusinessObjectReferenceProperty"/> that holds the reference to the <paramref name="value"/>. Must not be <see langword="null" />.
+    /// </param>
+    /// <param name="value">The <see cref="IBusinessObject"/> instance to be deleted. Must not be <see langword="null" />..</param>
+    void Delete ([CanBeNull] IBusinessObject referencingObject, [NotNull] IBusinessObjectReferenceProperty property, [NotNull] IBusinessObject value);
   }
 }
