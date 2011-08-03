b14c7fd5872f33b8873fb14427722d677093419d
andreas.obkircher <andreas.obkircher@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-08-03 09:23:28 +0000

    added SqlXmlSetComparedColumnSpecification (RM-4183)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@21278 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Data/DomainObjects/Persistence/Rdbms/DbCommandBuilders/Specifications/SqlXmlSetComparedColumnSpecification.cs
@@ -0,0 +1,89 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Xml;
+using Remotion.Data.DomainObjects.Persistence.Rdbms.Model;
+using Remotion.Utilities;
+using System.Linq;
+
+namespace Remotion.Data.DomainObjects.Persistence.Rdbms.DbCommandBuilders.Specifications
+{
+  /// <summary>
+  /// The <see cref="SqlXmlSetComparedColumnSpecification"/> builds a specification command that allows retrieving a set of records.
+  /// </summary>
+  public class SqlXmlSetComparedColumnSpecification : IComparedColumnsSpecification
+  {
+    private readonly ColumnDefinition _columnDefinition;
+    private readonly object[] _objectValues;
+
+    public SqlXmlSetComparedColumnSpecification (ColumnDefinition columnDefinition, IEnumerable<object> objectValues)
+    {
+      ArgumentUtility.CheckNotNull ("columnDefinition", columnDefinition);
+      ArgumentUtility.CheckNotNull ("objectValues", objectValues);
+
+      _columnDefinition = columnDefinition;
+      _objectValues = objectValues.ToArray();
+    }
+
+    public ColumnDefinition ColumnDefinition
+    {
+      get { return _columnDefinition; }
+    }
+
+    public object[] ObjectValues
+    {
+      get { return _objectValues; }
+    }
+
+    public void AppendComparisons (StringBuilder statement, IDbCommand command, ISqlDialect sqlDialect)
+    {
+      ArgumentUtility.CheckNotNull ("statement", statement);
+      ArgumentUtility.CheckNotNull ("command", command);
+      ArgumentUtility.CheckNotNull ("sqlDialect", sqlDialect);
+
+      var stringWriter = new StringWriter (new StringBuilder());
+      var xmlWriter = new XmlTextWriter (stringWriter);
+      xmlWriter.WriteStartElement ("L");
+      foreach (var value in ObjectValues)
+      {
+        xmlWriter.WriteStartElement ("I");
+        xmlWriter.WriteString (value == null ? null : value.ToString());
+        xmlWriter.WriteEndElement ();
+      }
+      xmlWriter.WriteEndElement();
+      
+      var parameter = _columnDefinition.StorageTypeInfo.CreateDataParameter (command, stringWriter.ToString());
+      parameter.ParameterName = sqlDialect.GetParameterName (_columnDefinition.Name);
+      parameter.DbType = DbType.Xml;
+      command.Parameters.Add (parameter);
+
+      statement.Append (sqlDialect.DelimitIdentifier (_columnDefinition.Name));
+      statement.Append (" IN (");
+      statement.Append ("SELECT T.c.value('.', '").Append (_columnDefinition.StorageTypeInfo.StorageType).Append ("')");
+      statement.Append (" FROM ");
+      statement.Append (parameter.ParameterName);
+      statement.Append (".nodes('/L/I') T(c))");
+      statement.Append (sqlDialect.StatementDelimiter);
+
+      command.CommandText = statement.ToString ();
+    }
+  }
+}
---
+++ Remotion/Data/UnitTests/DomainObjects/Core/Persistence/Rdbms/DbCommandBuilders/Specifications/SqlXmlSetComparedColumnSpecificationTest.cs
@@ -0,0 +1,83 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Data;
+using System.Text;
+using NUnit.Framework;
+using Remotion.Data.DomainObjects.Persistence.Rdbms;
+using Remotion.Data.DomainObjects.Persistence.Rdbms.DbCommandBuilders.Specifications;
+using Remotion.Data.DomainObjects.Persistence.Rdbms.Model;
+using Remotion.Data.UnitTests.DomainObjects.Factories;
+using Rhino.Mocks;
+
+namespace Remotion.Data.UnitTests.DomainObjects.Core.Persistence.Rdbms.DbCommandBuilders.Specifications
+{
+  [TestFixture]
+  public class SqlXmlSetComparedColumnSpecificationTest
+  {
+    private ColumnDefinition _columnDefinition;
+    private string _objectValue1;
+    private int _objectValue2;
+    private object _objectValue3;
+    private SqlXmlSetComparedColumnSpecification _specification;
+    private StringBuilder _statement;
+    private IDataParameterCollection _parametersCollectionMock;
+    private IDbCommand _commandStub;
+    private ISqlDialect _sqlDialectStub;
+    private IDbDataParameter _parameterStub;
+
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _columnDefinition = ColumnDefinitionObjectMother.CreateColumn ("Column");
+      _objectValue1 = "<Test1";
+      _objectValue2 = 689;
+      _objectValue3 = null;
+      
+      _specification = new SqlXmlSetComparedColumnSpecification (_columnDefinition, new[] { _objectValue1, _objectValue2, _objectValue3 });
+
+      _statement = new StringBuilder ();
+
+      _parametersCollectionMock = MockRepository.GenerateStrictMock<IDataParameterCollection> ();
+
+      _commandStub = MockRepository.GenerateStub<IDbCommand> ();
+      _commandStub.Stub (stub => stub.Parameters).Return (_parametersCollectionMock);
+      
+      _parameterStub = MockRepository.GenerateStub<IDbDataParameter>();
+      _commandStub.Stub (stub => stub.CreateParameter ()).Return (_parameterStub);
+
+      _sqlDialectStub = MockRepository.GenerateStub<ISqlDialect> ();
+      _sqlDialectStub.Stub (stub => stub.StatementDelimiter).Return ("delimiter");
+    }
+
+    [Test]
+    public void AppendComparisons ()
+    {
+      _sqlDialectStub.Stub (stub => stub.DelimitIdentifier ("Column")).Return ("[delimited Column]");
+      _sqlDialectStub.Stub (stub => stub.GetParameterName ("Column")).Return ("pColumn");
+      
+      _parametersCollectionMock.Expect (mock => mock.Add (_parameterStub)).Return (0);
+      _parametersCollectionMock.Replay ();
+
+      _specification.AppendComparisons (_statement, _commandStub, _sqlDialectStub);
+
+      Assert.That (_statement.ToString (), Is.EqualTo ("[delimited Column] IN (SELECT T.c.value('.', 'varchar') FROM pColumn.nodes('/L/I') T(c))delimiter"));
+      Assert.That (_parameterStub.Value, Is.EqualTo ("<L><I>&lt;Test1</I><I>689</I><I /></L>"));
+    }
+  }
+}
