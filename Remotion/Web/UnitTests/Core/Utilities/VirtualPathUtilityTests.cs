f5f98479dd63bc76e2e7c80e794b5efe129d5647
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-09-01 14:01:19 +0000

    Added VirtualPathUtility. (RM-4276)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@21481 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/UnitTests/Core/Utilities/VirtualPathUtilityTests.cs
@@ -0,0 +1,61 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using NUnit.Framework;
+using Remotion.Web.UI.Controls;
+using Remotion.Web.Utilities;
+using Rhino.Mocks;
+
+namespace Remotion.Web.UnitTests.Core.Utilities
+{
+  [TestFixture]
+  public class VirtualPathUtilityTests
+  {
+    private IControl _controlStub;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _controlStub = MockRepository.GenerateStub<IControl>();
+      _controlStub.AppRelativeTemplateSourceDirectory = "~/base/Path";
+    }
+
+    [Test]
+    public void GetVirtualPath_RelativePath ()
+    {
+      Assert.That (VirtualPathUtility.GetVirtualPath (_controlStub, "relative/path/file.txt"), Is.EqualTo ("~/base/Path/relative/path/file.txt"));
+    }
+
+    [Test]
+    public void GetVirtualPath_VirtualPath ()
+    {
+      Assert.That (VirtualPathUtility.GetVirtualPath (_controlStub, "~/virtual/path/file.txt"), Is.EqualTo ("~/virtual/path/file.txt"));
+    }
+
+    [Test]
+    public void GetVirtualPath_FilePath ()
+    {
+      Assert.That (VirtualPathUtility.GetVirtualPath (_controlStub, "file.txt"), Is.EqualTo ("~/base/Path/file.txt"));
+    }
+
+    [Test]
+    public void GetVirtualPath_HierchyPath ()
+    {
+      Assert.That (VirtualPathUtility.GetVirtualPath (_controlStub, "../relative/file.txt"), Is.EqualTo ("~/base/relative/file.txt"));
+    }
+  }
+}
