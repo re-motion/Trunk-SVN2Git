6ecf58858964d2b9339a7d77b5e26549f43eb9fb
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-09-22 06:51:16 +0000

    Fixed license header.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@21597 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Core/Res/HTML/jquery-1.6.4.js
+++ Remotion/Web/Core/Res/HTML/jquery-1.6.4.js
@@ -1,19 +1,18 @@
-// This file is part of the re-motion Core Framework (www.re-motion.org)
-// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
-// 
-// The re-motion Core Framework is free software; you can redistribute it 
-// and/or modify it under the terms of the GNU Lesser General Public License 
-// as published by the Free Software Foundation; either version 2.1 of the 
-// License, or (at your option) any later version.
-// 
-// re-motion is distributed in the hope that it will be useful, 
-// but WITHOUT ANY WARRANTY; without even the implied warranty of 
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with re-motion; if not, see http://www.gnu.org/licenses.
-// 
+/*!
+ * jQuery JavaScript Library v1.6.4
+ * http://jquery.com/
+ *
+ * Copyright 2011, John Resig
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
+ *
+ * Includes Sizzle.js
+ * http://sizzlejs.com/
+ * Copyright 2011, The Dojo Foundation
+ * Released under the MIT, BSD, and GPL Licenses.
+ *
+ * Date: Mon Sep 12 18:54:48 2011 -0400
+ */
 (function( window, undefined ) {
 
 // Use the correct document accordingly with window argument (sandbox)
