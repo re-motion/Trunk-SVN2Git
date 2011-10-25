e579dc0952b7caa21f99dab9905b281335522dfa
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-10-25 12:27:04 +0000

    Added UserProxy class to SecurityManager domain. (RM-4412)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@21858 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ SecurityManager/Core.UnitTests/Domain/OrganizationalStructure/UserProxyTest.cs
@@ -0,0 +1,69 @@
+﻿// This file is part of re-strict (www.re-motion.org)
+// Copyright (C) 2005-2009 rubicon informationstechnologie gmbh, www.rubicon.eu
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License version 3.0 
+// as published by the Free Software Foundation.
+// 
+// This program is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program; if not, see http://www.gnu.org/licenses.
+// 
+// Additional permissions are listed in the file re-motion_exceptions.txt.
+// 
+using System;
+using NUnit.Framework;
+using Remotion.Development.UnitTesting;
+using Remotion.ObjectBinding;
+using Remotion.SecurityManager.Domain.OrganizationalStructure;
+
+namespace Remotion.SecurityManager.UnitTests.Domain.OrganizationalStructure
+{
+  [TestFixture]
+  public class UserProxyTest : DomainTest
+  {
+    private OrganizationalStructureTestHelper _testHelper;
+
+    public override void SetUp ()
+    {
+      base.SetUp();
+      _testHelper = new OrganizationalStructureTestHelper();
+      _testHelper.Transaction.EnterNonDiscardingScope();
+    }
+
+    [Test]
+    public void Create ()
+    {
+      User user = CreateUser();
+      var proxy = UserProxy.Create (user);
+
+      Assert.That (proxy.ID, Is.EqualTo (user.ID));
+      Assert.That (proxy.UniqueIdentifier, Is.EqualTo (((IBusinessObjectWithIdentity) user).UniqueIdentifier));
+      Assert.That (proxy.DisplayName, Is.EqualTo (((IBusinessObjectWithIdentity) user).DisplayName));
+    }
+
+    [Test]
+    public void Serialization ()
+    {
+      User user = CreateUser();
+      var proxy = UserProxy.Create (user);
+
+      var deserialized = Serializer.SerializeAndDeserialize (proxy);
+
+      Assert.That (deserialized.ID, Is.EqualTo (proxy.ID));
+      Assert.That (deserialized.UniqueIdentifier, Is.EqualTo (proxy.UniqueIdentifier));
+      Assert.That (deserialized.DisplayName, Is.EqualTo (proxy.DisplayName));
+    }
+
+    private User CreateUser ()
+    {
+      var tenant = _testHelper.CreateTenant ("TheTenant", "UID");
+      var group = _testHelper.CreateGroup ("TheGroup", null, tenant);
+      return _testHelper.CreateUser ("UserName", "FN", "LN", null, group, tenant);
+    }
+  }
+}
