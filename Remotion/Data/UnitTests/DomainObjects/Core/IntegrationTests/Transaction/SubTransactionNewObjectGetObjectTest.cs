ab33f8549b8ca165a02aef34f44cae716e61d9e2
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-10-25 14:00:47 +0000

    Enabled integration test (RM-4426)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@21866 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/UnitTests/DomainObjects/Core/IntegrationTests/Transaction/SubTransactionNewObjectGetObjectTest.cs
+++ Remotion/Data/UnitTests/DomainObjects/Core/IntegrationTests/Transaction/SubTransactionNewObjectGetObjectTest.cs
@@ -513,7 +513,6 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.IntegrationTests.Transactio
     }
 
     [Test]
-    [Ignore ("TODO 4426")]
     public void TryGetObjects_DeletedInParentTransaction ()
     {
       var order1 = Order.GetObject (DomainObjectIDs.Order1);
