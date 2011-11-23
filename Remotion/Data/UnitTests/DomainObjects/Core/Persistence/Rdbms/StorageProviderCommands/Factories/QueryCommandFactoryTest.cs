a1ead5c338214b3b22972e82002fba07f7250b14
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2011-11-23 16:00:11 +0000

    Fixed test (RM-4473)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@22074 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/UnitTests/DomainObjects/Core/Persistence/Rdbms/StorageProviderCommands/Factories/QueryCommandFactoryTest.cs
+++ Remotion/Data/UnitTests/DomainObjects/Core/Persistence/Rdbms/StorageProviderCommands/Factories/QueryCommandFactoryTest.cs
@@ -138,17 +138,18 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Persistence.Rdbms.StoragePr
       queryStub.Stub (stub => stub.Statement).Return ("statement");
       queryStub.Stub (stub => stub.Parameters).Return (new QueryParameterCollection { new QueryParameter ("p1", Tuple.Create (1, "a")) });
 
-      var notSupportedException = new NotSupportedException ("Message.");
       _dataStoragePropertyDefinitionFactoryStrictMock
           .Stub (stub => stub.CreateStoragePropertyDefinition (Tuple.Create (1, "a")))
-          .Return (new UnsupportedStoragePropertyDefinition (typeof (string), "X", null));
+          .Return (new UnsupportedStoragePropertyDefinition (typeof (string), "X.", null));
       _dataStoragePropertyDefinitionFactoryStrictMock.Replay ();
 
       Assert.That (
           () => _factory.CreateForDataContainerQuery (queryStub), 
           Throws.TypeOf<InvalidOperationException>()
-              .With.Message.EqualTo ("The query parameter 'p1' cannot be converted to a database value: This operation is not supported. Reason: Message.")
-              .And.InnerException.SameAs (notSupportedException));
+              .With.Message.EqualTo (
+                  "The query parameter 'p1' cannot be converted to a database value: This operation is not supported because the storage property is "
+                  + "invalid. Reason: X.")
+              .And.InnerException.TypeOf<NotSupportedException>());
     }
 
     [Test]
