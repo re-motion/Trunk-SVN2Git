849c747140e6c7c63dd4954fc96e99b1721df02c
fabian.schmied <fabian.schmied@cb0deee2-328e-b949-94ea-fb3139fab790>
2012-01-10 14:14:29 +0000

    Improved docs
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@22215 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Data/DomainObjects/DomainImplementation/UnloadService.cs
+++ Remotion/Data/DomainObjects/DomainImplementation/UnloadService.cs
@@ -109,7 +109,8 @@ namespace Remotion.Data.DomainObjects.DomainImplementation
     /// The method unloads the <see cref="DataContainer"/>, the collection end points the object is part of (but not
     /// the collection end points the object owns), the non-virtual end points owned by the object and their respective opposite virtual object 
     /// end-points. This means that unloading an object will unload a relation if and only if the object's <see cref="DataContainer"/> is holding 
-    /// the foreign key for the relation.
+    /// the foreign key for the relation. Use <see cref="UnloadVirtualEndPoint"/> or <see cref="UnloadCollectionEndPointAndData"/> to unload 
+    /// relations whose foreign keys are not held by the object.
     /// </para>
     /// <para>
     /// The unload operation is not atomic over the transaction hierarchy. It will start at the <see cref="ClientTransaction.LeafTransaction"/> 
@@ -144,7 +145,8 @@ namespace Remotion.Data.DomainObjects.DomainImplementation
     /// The method unloads the <see cref="DataContainer"/>, the collection end points the object is part of (but not
     /// the collection end points the object owns), the non-virtual end points owned by the object and their respective opposite virtual object 
     /// end-points. This means that unloading an object will unload a relation if and only if the object's <see cref="DataContainer"/> is holding 
-    /// the foreign key for the relation.
+    /// the foreign key for the relation. Use <see cref="TryUnloadVirtualEndPoint"/> or <see cref="TryUnloadCollectionEndPointAndData"/> to unload 
+    /// relations whose foreign keys are not held by the object.
     /// </para>
     /// 	<para>
     /// The unload operation is not atomic over the transaction hierarchy. It will start at the <see cref="ClientTransaction.LeafTransaction"/> 
@@ -177,10 +179,16 @@ namespace Remotion.Data.DomainObjects.DomainImplementation
     /// <exception cref="ArgumentNullException">One of the arguments passed to this method is <see langword="null" />.</exception>
     /// <exception cref="ArgumentException">The given <paramref name="endPointID"/> does not specify a collection end point.</exception>
     /// <remarks>
+    /// <para>
     /// The unload operation is not atomic over the transaction hierarchy. It will start at the <see cref="ClientTransaction.LeafTransaction"/> 
     /// and try to unload here, then it will go over the parent transactions one by one. If the operation fails in any of the transactions, 
     /// it will stop and throw an exception. At this point of time, the operation will have unloaded items from all the transactions where it 
     /// succeeded, but not in the one where it failed or those above.
+    /// </para>
+    /// <para>
+    /// This operation is atomic within each transaction in the transaction hierarchy. Within each transaction, if either the end-point or any of its 
+    /// items' data cannot be unloaded, nothing will be unloaded.
+    /// </para>
     /// </remarks>
     public static void UnloadCollectionEndPointAndData (ClientTransaction clientTransaction, RelationEndPointID endPointID)
     {
@@ -208,10 +216,16 @@ namespace Remotion.Data.DomainObjects.DomainImplementation
     /// <exception cref="ArgumentNullException">One of the arguments passed to this method is <see langword="null"/>.</exception>
     /// <exception cref="ArgumentException">The given <paramref name="endPointID"/> does not specify a collection end point.</exception>
     /// <remarks>
+    /// <para>
     /// The unload operation is not atomic over the transaction hierarchy. It will start at the <see cref="ClientTransaction.LeafTransaction"/> 
     /// and try to unload here, then it will go over the parent transactions one by one. If the operation fails in any of the transactions, 
     /// it will stop and throw an exception. At this point of time, the operation will have unloaded items from all the transactions where it 
     /// succeeded, but not in the one where it failed or those above.
+    /// </para>
+    /// <para>
+    /// This operation is atomic within each transaction in the transaction hierarchy. Within each transaction, if either the end-point or any of its 
+    /// items' data cannot be unloaded, nothing will be unloaded.
+    /// </para>
     /// </remarks>
     public static bool TryUnloadCollectionEndPointAndData (ClientTransaction clientTransaction, RelationEndPointID endPointID)
     {
