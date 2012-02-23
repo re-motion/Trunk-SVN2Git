82d14c3547a808024b173fa051fb8d680569d2fd
julian.lettner <julian.lettner@cb0deee2-328e-b949-94ea-fb3139fab790>
2012-02-23 14:56:06 +0000

    Aligned visibility with other test domain types
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@22446 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Core/UnitTests/ServiceLocation/TestDomain/ITestConcreteImplementationAttributeTypeWithInvalidImplementation.cs
+++ Remotion/Core/UnitTests/ServiceLocation/TestDomain/ITestConcreteImplementationAttributeTypeWithInvalidImplementation.cs
@@ -20,7 +20,7 @@ using Remotion.ServiceLocation;
 namespace Remotion.UnitTests.ServiceLocation.TestDomain
 {
   [ConcreteImplementation (typeof(TestConcreteImplementationAttributeType))]
-  internal interface ITestConcreteImplementationAttributeTypeWithInvalidImplementation
+  public interface ITestConcreteImplementationAttributeTypeWithInvalidImplementation
   {
   }
 }
