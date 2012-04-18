942d00e0109ca0c4686aa61dc984c66a32413116
julian.lettner <julian.lettner@cb0deee2-328e-b949-94ea-fb3139fab790>
2012-04-18 07:25:18 +0000

    Added typed Dev<T> with Dummy field for ref and out parameters
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@22965 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Development/Core/UnitTesting/Dev.cs
+++ Remotion/Development/Core/UnitTesting/Dev.cs
@@ -18,12 +18,44 @@ using System;
 
 namespace Remotion.Development.UnitTesting
 {
+  /// <summary>
+  /// Provides a <see cref="Null"/> property that can be assigned arbitrary values.
+  /// </summary>
   public static class Dev
   {
+    /// <summary>
+    /// Use this in unit tests where you need to assign a value to
+    /// something (e.g., for syntactic reasons, or to remove unused variable warnings), but don't care about the result of the assignment.
+    /// </summary>
     public static object Null
     {
       get { return null; }
-      set {  }
+      // ReSharper disable ValueParameterNotUsed
+      set { }
+      // ReSharper restore ValueParameterNotUsed
+    }
+  }
+
+  /// <summary>
+  /// Provides a <see cref="Dummy"/> field that can be used as a ref or out parameter, and a typed <see cref="Null"/> property that can be assigned 
+  /// arbitrary values and always returns the default value for <typeparamref name="T"/>.
+  /// </summary>
+  public static class Dev<T>
+  {
+    /// <summary>
+    /// Use this in unit tests where you need a ref or out parameter but but don't care about the result of the assignment.
+    /// Never rely on the value of the <see cref="Dummy"/> field, it will be changed by other tests.
+    /// </summary>
+    public static T Dummy;
+
+    /// <summary>
+    /// Use this in unit tests where you need to assign a value to
+    /// something (e.g., for syntactic reasons, or to remove unused variable warnings), but don't care about the result of the assignment.
+    /// </summary>
+    public static T Null
+    {
+      get { return default (T); }
+      set { Dev.Null = value; }
     }
   }
 }
