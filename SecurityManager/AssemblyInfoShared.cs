c8b79022c36bccc856d162ebc74f1a92c9a53592
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2012-11-07 16:25:35 +0000

    Updated AssemblyInfoShared.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@24420 cb0deee2-328e-b949-94ea-fb3139fab790

--- SecurityManager/AssemblyInfoShared.cs
+++ SecurityManager/AssemblyInfoShared.cs
@@ -28,7 +28,7 @@ using System.Reflection;
 [assembly: AssemblyProduct ("re-strict (www.re-motion.org)")]
 [assembly: AssemblyCopyright ("(c) rubicon IT GmbH, www.rubicon.eu - Licensed under GNU AGPL 3.0")]
 [assembly: AssemblyTrademark ("")]
-[assembly: AssemblyInformationalVersion ("")]
+[assembly: AssemblyInformationalVersion ("1.13.173.0")]
 
 //
 // Version information for an assembly consists of the following four values:
