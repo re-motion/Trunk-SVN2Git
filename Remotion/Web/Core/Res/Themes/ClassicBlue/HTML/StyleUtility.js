097b66195990f4dd20df5ac99fd6de94f967f4ec
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2012-11-09 10:25:56 +0000

    Refactored StyleUtility.js
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@24442 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Core/Res/Themes/ClassicBlue/HTML/StyleUtility.js
+++ Remotion/Web/Core/Res/Themes/ClassicBlue/HTML/StyleUtility.js
@@ -18,33 +18,33 @@ function StyleUtility()
 {
 }
 
-StyleUtility.CreateBorderSpans = function(selector)
+StyleUtility.CreateBorderSpans = function (selector)
 {
-  var element = $(selector);
+  var element = $ (selector);
 
-  StyleUtility.CreateAndAppendBorderSpan(element, 'top');
-  StyleUtility.CreateAndAppendBorderSpan(element, 'left');
-  StyleUtility.CreateAndAppendBorderSpan(element, 'bottom');
-  StyleUtility.CreateAndAppendBorderSpan(element, 'right');
-  StyleUtility.CreateAndAppendBorderSpan(element, 'topLeft');
-  var topRight = StyleUtility.CreateAndAppendBorderSpan(element, 'topRight');
-  var bottomLeft = StyleUtility.CreateAndAppendBorderSpan(element, 'bottomLeft');
-  var bottomRight = StyleUtility.CreateAndAppendBorderSpan(element, 'bottomRight');
+  StyleUtility.CreateAndAppendBorderSpan (element, 'top');
+  StyleUtility.CreateAndAppendBorderSpan (element, 'left');
+  StyleUtility.CreateAndAppendBorderSpan (element, 'bottom');
+  StyleUtility.CreateAndAppendBorderSpan (element, 'right');
+  StyleUtility.CreateAndAppendBorderSpan (element, 'topLeft');
+  var topRight = StyleUtility.CreateAndAppendBorderSpan (element, 'topRight');
+  var bottomLeft = StyleUtility.CreateAndAppendBorderSpan (element, 'bottomLeft');
+  var bottomRight = StyleUtility.CreateAndAppendBorderSpan (element, 'bottomRight');
 
-  if (StyleUtility.ShowBorderSpans(element, topRight, bottomLeft, bottomRight))
-    PageUtility.Instance.RegisterResizeHandler(selector, StyleUtility.OnResize);
-}
+  if (StyleUtility.ShowBorderSpans (element, topRight, bottomLeft, bottomRight))
+    PageUtility.Instance.RegisterResizeHandler (selector, StyleUtility.OnResize);
+};
 
-StyleUtility.ShowBorderSpans = function(element, topRight, bottomLeft, bottomRight)
+StyleUtility.ShowBorderSpans = function (element, topRight, bottomLeft, bottomRight)
 {
   var scrollDiv = element;
-  while (scrollDiv.css('overflow') != 'auto' && scrollDiv.css('overflow') != 'scroll' && (scrollDiv.length > 0))
-      scrollDiv = scrollDiv.children().eq(0);
-  var scrolledDiv = scrollDiv.children().eq(0);
+  while (scrollDiv.css ('overflow') != 'auto' && scrollDiv.css ('overflow') != 'scroll' && (scrollDiv.length > 0))
+    scrollDiv = scrollDiv.children().eq (0);
+  var scrolledDiv = scrollDiv.children().eq (0);
 
-  var hasScrollbarsOnOverflow = scrollDiv.css('overflow') == 'auto' || scrollDiv.css('overflow') == 'scroll';
+  var hasScrollbarsOnOverflow = scrollDiv.css ('overflow') == 'auto' || scrollDiv.css ('overflow') == 'scroll';
 
-  if (scrolledDiv.length == 1 && scrolledDiv.prop('nodeName').toLowerCase() == 'div' && hasScrollbarsOnOverflow)
+  if (scrolledDiv.length == 1 && scrolledDiv.prop ('nodeName').toLowerCase() == 'div' && hasScrollbarsOnOverflow)
   {
     var offset = 1;
     var hasVerticalScrollBar = scrolledDiv[0].scrollHeight > (scrolledDiv.height() + offset); //height includes the scrollbar, if it exists
@@ -52,106 +52,84 @@ StyleUtility.ShowBorderSpans = function(element, topRight, bottomLeft, bottomRig
     var hasExactlyOneScrollbar = (hasVerticalScrollBar && !hasHorizontalScrollbar) || (!hasVerticalScrollBar && hasHorizontalScrollbar);
 
     if (hasVerticalScrollBar)
-      $(topRight).css('display', 'none');
+      $ (topRight).css ('display', 'none');
     else
-      $(topRight).css('display', '');
+      $ (topRight).css ('display', '');
 
     if (hasHorizontalScrollbar)
-      $(bottomLeft).css('display', 'none');
+      $ (bottomLeft).css ('display', 'none');
     else
-      $(bottomLeft).css('display', '');
+      $ (bottomLeft).css ('display', '');
 
     if (hasExactlyOneScrollbar)
-      $(bottomRight).css('display', 'none');
+      $ (bottomRight).css ('display', 'none');
     else
-      $(bottomRight).css('display', '');
+      $ (bottomRight).css ('display', '');
 
     return true;
   }
   else
-  {
     return false;
-  }
-}
+};
 
-StyleUtility.CreateAndAppendBorderSpan = function(elementBody, className)
+StyleUtility.CreateAndAppendBorderSpan = function (elementBody, className)
 {
-  var borderSpan = document.createElement('SPAN');
+  var borderSpan = document.createElement ('SPAN');
   borderSpan.className = className;
 
-  elementBody[0].appendChild(borderSpan);
+  elementBody[0].appendChild (borderSpan);
 
-  return borderSpan
-}
+  return borderSpan;
+};
 
-StyleUtility.OnResize = function(element) {
+StyleUtility.OnResize = function (element)
+{
 
-  var topRight = element.find('.topRight');
-  var bottomLeft = element.find('.bottomLeft');
-  var bottomRight = element.find('.bottomRight');
+  var topRight = element.find ('.topRight');
+  var bottomLeft = element.find ('.bottomLeft');
+  var bottomRight = element.find ('.bottomRight');
 
-  StyleUtility.ShowBorderSpans(element, topRight[0], bottomLeft[0], bottomRight[0]);
-}
+  StyleUtility.ShowBorderSpans (element, topRight[0], bottomLeft[0], bottomRight[0]);
+};
 
 StyleUtility.AddBrowserSwitch = function ()
 {
   var browser;
   if ($.browser.msie)
   {
-    var majorVersion;
-    if (TypeUtility.IsDefined(window.document.documentMode))
-      majorVersion = parseInt(window.document.documentMode);
-    else
-      majorVersion = parseInt($.browser.version);
-
+    var majorVersion = BrowserUtility.GetIEVersion();
     if (majorVersion < 9)
-      browser = 'msie' + majorVersion
+      browser = 'msie' + majorVersion;
     else
       browser = 'msie';
   }
   else if ($.browser.mozilla)
-  {
     browser = 'mozilla';
-  }
   else if ($.browser.webkit)
-  {
     browser = 'webkit';
-  }
   else if ($.browser.opera)
-  {
     browser = 'opera';
-  }
   else
-  {
     browser = 'unknown';
-  }
 
   StyleUtility.AddPlatformSwitch();
 
-  if (!$('body').hasClass(browser))
-    $('body').addClass(browser);
-}
+  if (!$ ('body').hasClass (browser))
+    $ ('body').addClass (browser);
+};
 
 StyleUtility.AddPlatformSwitch = function ()
 {
   var platform;
-  if (navigator.appVersion.indexOf("Win") != -1)
-  {
+  if (navigator.appVersion.indexOf ("Win") != -1)
     platform = "win";
-  }
-  else if (navigator.appVersion.indexOf("Mac") != -1)
-  {
+  else if (navigator.appVersion.indexOf ("Mac") != -1)
     platform = "mac";
-  }
-  else if (navigator.appVersion.indexOf("X11") != -1)
-  {
+  else if (navigator.appVersion.indexOf ("X11") != -1)
     platform = "x11";
-  }
   else
-  {
     platform = "unknown";
-  }
 
-  if (!$('body').hasClass(platform))
-    $('body').addClass(platform);
-}
+  if (!$ ('body').hasClass (platform))
+    $ ('body').addClass (platform);
+};
--- Remotion/Web/Core/Res/Themes/NovaBlue/HTML/StyleUtility.js
+++ Remotion/Web/Core/Res/Themes/NovaBlue/HTML/StyleUtility.js
@@ -20,67 +20,46 @@ function StyleUtility()
 
 StyleUtility.CreateBorderSpans = function (selector)
 {
-}
+};
 
 StyleUtility.AddBrowserSwitch = function ()
 {
   var browser;
   if ($.browser.msie)
   {
-    var majorVersion;
-    if (TypeUtility.IsDefined(window.document.documentMode))
-      majorVersion = parseInt(window.document.documentMode);
-    else
-      majorVersion = parseInt($.browser.version);
-
+    var majorVersion = BrowserUtility.GetIEVersion();
     if (majorVersion < 9)
-      browser = 'msie' + majorVersion
+      browser = 'msie' + majorVersion;
     else
       browser = 'msie';
   }
   else if ($.browser.mozilla)
-  {
     browser = 'mozilla';
-  }
   else if ($.browser.webkit)
-  {
     browser = 'webkit';
-  }
   else if ($.browser.opera)
-  {
     browser = 'opera';
-  }
   else
-  {
     browser = 'browserUnknown';
-  }
 
   StyleUtility.AddPlatformSwitch();
 
-  if (!$('body').hasClass(browser))
-    $('body').addClass(browser);
-}
+  if (!$ ('body').hasClass (browser))
+    $ ('body').addClass (browser);
+};
 
 StyleUtility.AddPlatformSwitch = function ()
 {
   var platform;
-  if (navigator.appVersion.indexOf("Win") != -1)
-  {
+  if (navigator.appVersion.indexOf ("Win") != -1)
     platform = "win";
-  }
-  else if (navigator.appVersion.indexOf("Mac") != -1)
-  {
+  else if (navigator.appVersion.indexOf ("Mac") != -1)
     platform = "mac";
-  }
-  else if (navigator.appVersion.indexOf("X11") != -1)
-  {
+  else if (navigator.appVersion.indexOf ("X11") != -1)
     platform = "x11";
-  }
   else
-  {
-    platform = "platformUnknown"; 
-  }
+    platform = "platformUnknown";
 
-  if (!$('body').hasClass(platform))
-    $('body').addClass(platform);
-}
+  if (!$ ('body').hasClass (platform))
+    $ ('body').addClass (platform);
+};
