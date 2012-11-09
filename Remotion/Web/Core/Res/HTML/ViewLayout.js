7b8218375bfc75602b242454ffe8b25c68882f75
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2012-11-09 10:30:21 +0000

    Dropped unused IE6 hooks from ViewLayout.js.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@24444 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Core/Res/HTML/ViewLayout.js
+++ Remotion/Web/Core/Res/HTML/ViewLayout.js
@@ -18,52 +18,34 @@ function ViewLayout()
 {
 }
 
-ViewLayout.AdjustLayoutIE6 = function(containerElement) 
+ViewLayout.AdjustActiveViewContent = function (viewContent)
 {
-    if (!$.browser.msie || parseInt($.browser.version) > 6)
-        return;
-}
-
-ViewLayout.AdjustMultiViewLayoutIE6 = function(containerElement) 
-{
-    if (!$.browser.msie || parseInt($.browser.version) > 6)
-        return;
-}
-
-ViewLayout.AdjustSingleViewLayoutIE6 = function(containerElement) 
-{
-    if (!$.browser.msie || parseInt($.browser.version) > 6)
-        return;
-}
-
-ViewLayout.AdjustActiveViewContent = function(viewContent)
-{
-  var viewContentBorder = viewContent.children().eq(0);
+  var viewContentBorder = viewContent.children().eq (0);
   var viewBottomControls = viewContent.next();
 
-  var viewContentBorderHeight = viewContentBorder.outerHeight(true) - viewContentBorder.height();
-  var viewBottomControlsBorderHeight = viewBottomControls.outerHeight(true) - viewBottomControls.height();
+  var viewContentBorderHeight = viewContentBorder.outerHeight (true) - viewContentBorder.height();
+  var viewBottomControlsBorderHeight = viewBottomControls.outerHeight (true) - viewBottomControls.height();
 
   var viewContentOffset = viewContent.offset();
   var viewTop = viewContentOffset == null ? 0 : viewContentOffset.top;
-  
+
   var viewBottomControlsOffset = viewBottomControls.offset();
   var bottomTop = viewBottomControlsOffset == null ? 0 : viewBottomControlsOffset.top;
-  
+
   var viewNewHeight = bottomTop - viewTop - viewContentBorderHeight - viewBottomControlsBorderHeight;
 
-  viewContentBorder.height(viewNewHeight);
-}
+  viewContentBorder.height (viewNewHeight);
+};
 
 ViewLayout.AdjustSingleView = function(containerElement) 
 {
-    var viewContent = containerElement.children().eq(0).children().eq(1);
-    ViewLayout.AdjustActiveViewContent(viewContent);
+  var viewContent = containerElement.children().eq(0).children().eq(1);
+  ViewLayout.AdjustActiveViewContent(viewContent);
 };
 
 ViewLayout.AdjustTabbedMultiView = function(containerElement) 
 {
-    var viewContent = containerElement.children().eq(0).children().eq(2);
-    ViewLayout.AdjustActiveViewContent(viewContent);
+  var viewContent = containerElement.children().eq(0).children().eq(2);
+  ViewLayout.AdjustActiveViewContent(viewContent);
 };
 
