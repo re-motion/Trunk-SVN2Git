8dc888cf5257337d85d9a00367fe736223e4aec5
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2013-07-24 05:57:56 +0000

    Dropped (incomplete) serialization support from ExtendedProviderBase. (RM-5624)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@27909 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Core/Core/Configuration/ExtendedProviderBase.cs
+++ Remotion/Core/Core/Configuration/ExtendedProviderBase.cs
@@ -19,7 +19,6 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Configuration;
 using System.Configuration.Provider;
-using System.Runtime.Serialization;
 using Remotion.Utilities;
 
 namespace Remotion.Configuration
@@ -29,33 +28,8 @@ namespace Remotion.Configuration
   /// <see cref="ExtendedProviderBase"/> changes the protocoll for initializing a configuration provider from using a default constructor
   /// followed by a call to <see cref="Initialize"/> to initialize the provider during construction.
   /// </remarks>
-  public abstract class ExtendedProviderBase: ProviderBase, ISerializable
+  public abstract class ExtendedProviderBase: ProviderBase
   {
-    private static void SerializeNameValueCollection (SerializationInfo info, string key, NameValueCollection collection)
-    {
-      info.AddValue (key + ".Count", collection.Count);
-
-      for (int i = 0; i < collection.Count; ++i)
-      {
-        info.AddValue (key + ".Keys[" + i + "]", collection.Keys[i]);
-        info.AddValue (key + "[" + i + "]", collection[i]);
-      }
-    }
-
-    private static NameValueCollection DeserializeNameValueCollection (SerializationInfo info, string key)
-    {
-      int count = info.GetInt32 (key + ".Count");
-      NameValueCollection result = new NameValueCollection (count);
-
-      for (int i = 0; i < count; ++i)
-      {
-        string itemKey = info.GetString (key + ".Keys[" + i + "]");
-        string itemValue = info.GetString (key + "[" + i + "]");
-        result.Add (itemKey, itemValue);
-      }
-      return result;
-    }
-
     private readonly NameValueCollection _config;
 
     /// <summary>Initializes a new instance of the <see cref="ExtendedProviderBase"/>.</summary>
@@ -71,26 +45,11 @@ namespace Remotion.Configuration
       _config = configClone;
     }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ExtendedProviderBase"/> class in the process of deserialization.
-    /// </summary>
-    /// <param name="info">The <see cref="SerializationInfo"/> provided by the serialization engine.</param>
-    /// <param name="context">The <see cref="StreamingContext"/> provided by the serialization engine.</param>
-    protected ExtendedProviderBase (SerializationInfo info, StreamingContext context)
-        : this (ArgumentUtility.CheckNotNull ("info", info).GetString ("Name"), DeserializeNameValueCollection (info, "Config"))
-    {
-    }
-
     [EditorBrowsable (EditorBrowsableState.Never)]
     public override sealed void Initialize (string name, NameValueCollection config)
     {
       base.Initialize (name, config);
       Assertion.IsNull (_config, "Initialize can only succeed when called for the first time, from the constructor");
-    }
-
-    protected NameValueCollection Config
-    {
-      get { return _config; }
     }
 
     protected string GetAndRemoveNonEmptyStringAttribute (NameValueCollection config, string attribute, string providerName, bool required)
@@ -108,23 +67,6 @@ namespace Remotion.Configuration
       config.Remove (attribute);
       
       return value;
-    }
-
-    void ISerializable.GetObjectData (SerializationInfo info, StreamingContext context)
-    {
-      GetObjectData (info, context);
-    }
-
-    /// <summary>
-    /// Populates a <see cref="T:System.Runtime.Serialization.SerializationInfo"></see> with the data needed to serialize the target object.
-    /// </summary>
-    /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"></see> to populate with data.</param>
-    /// <param name="context">The destination (see <see cref="T:System.Runtime.Serialization.StreamingContext"></see>) for this serialization.</param>
-    /// <remarks>Override this method (and call the base implementation) to supply additional data for the serialization process.</remarks>
-    protected virtual void GetObjectData (SerializationInfo info, StreamingContext context)
-    {
-      info.AddValue ("Name", Name);
-      SerializeNameValueCollection (info, "Config", Config);
     }
   }
 }
