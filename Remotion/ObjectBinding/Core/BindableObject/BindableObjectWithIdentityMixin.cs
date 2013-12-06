10cbc5b05bcd0b8d6da4186d0d672b382315d84e
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2013-12-06 07:40:52 +0000

    Fixed DebuggerDisplay. (RM-5925)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@28527 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/ObjectBinding/Core/BindableObject/BindableObjectWithIdentityMixin.cs
+++ Remotion/ObjectBinding/Core/BindableObject/BindableObjectWithIdentityMixin.cs
@@ -28,7 +28,7 @@ namespace Remotion.ObjectBinding.BindableObject
   [CopyCustomAttributes (typeof (DebuggerDisplay))]
   public abstract class BindableObjectWithIdentityMixin : BindableObjectMixinBase<object>, IBusinessObjectWithIdentity
   {
-    [DebuggerDisplay ("{UniqueIdentifier} ({((Remotion.Mixins.IMixinTarget)this).Configuration.Type.FullName})")]
+    [DebuggerDisplay ("{UniqueIdentifier} ({((Remotion.Mixins.IMixinTarget)this).ClassContext.Type.FullName})")]
     private class DebuggerDisplay // the attributes of this class are copied to the target class
     {
     }
