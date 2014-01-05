3a1536625e71c09c6c07d022dddecc1f002aa8a9
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-01-05 10:14:41 +0000

    Reformatted WebTypeUtility. (RM-5569)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@28959 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Core/Utilities/WebTypeUtility.cs
+++ Remotion/Web/Core/Utilities/WebTypeUtility.cs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
+
 using System;
 using System.Collections;
 using System.Reflection;
@@ -22,69 +23,63 @@ using Remotion.Utilities;
 
 namespace Remotion.Web.Utilities
 {
-
-/// <summary> Utility methods for handling types in web projects. </summary>
-public sealed class WebTypeUtility
-{
-  /// <summary>
-  ///   Loads a type, optionally using an abbreviated type name as defined in 
-  ///   <see cref="TypeUtility.ParseAbbreviatedTypeName"/>.
-  /// </summary>
-  public static Type GetType (string abbreviatedTypeName)
+  /// <summary> Utility methods for handling types in web projects. </summary>
+  public static class WebTypeUtility
   {
-    ArgumentUtility.CheckNotNullOrEmpty ("abbreviatedTypeName", abbreviatedTypeName);
-    string typeName = TypeUtility.ParseAbbreviatedTypeName (abbreviatedTypeName);
-    return BuildManager.GetType (typeName, true);
-  }
-
-  /// <summary>
-  ///   Loads a type, optionally using an abbreviated type name as defined in 
-  ///   <see cref="TypeUtility.ParseAbbreviatedTypeName"/>.
-  /// </summary>
-  public static Type GetType (string abbreviatedTypeName, bool throwOnError)
-  {
-    ArgumentUtility.CheckNotNullOrEmpty ("abbreviatedTypeName", abbreviatedTypeName);
-    string typeName = TypeUtility.ParseAbbreviatedTypeName (abbreviatedTypeName);
-    return BuildManager.GetType (typeName, throwOnError);
-    }
-
-  /// <summary>
-  ///   Loads a type, optionally using an abbreviated type name as defined in 
-  ///   <see cref="TypeUtility.ParseAbbreviatedTypeName"/>.
-  /// </summary>
-  public static Type GetType (string abbreviatedTypeName, bool throwOnError, bool ignoreCase)
-  {
-    ArgumentUtility.CheckNotNullOrEmpty ("abbreviatedTypeName", abbreviatedTypeName);
-    string typeName = TypeUtility.ParseAbbreviatedTypeName (abbreviatedTypeName);
-    return BuildManager.GetType (typeName, throwOnError, ignoreCase);
-  }
-
-  public static string GetQualifiedName (Type type)
-  {
-    ArgumentUtility.CheckNotNull ("type", type);
-    if (IsCompiledType (type))
-      return type.FullName;
-    return TypeUtility.GetPartialAssemblyQualifiedName (type);
-  }
-
-  public static bool IsCompiledType (Type type)
-  {
-    ArgumentUtility.CheckNotNull ("type", type);
-    IList codeAssemblies = BuildManager.CodeAssemblies;
-    if (codeAssemblies == null)
-      return false;
-
-    foreach (Assembly assembly in codeAssemblies)
+    /// <summary>
+    ///   Loads a type, optionally using an abbreviated type name as defined in 
+    ///   <see cref="TypeUtility.ParseAbbreviatedTypeName"/>.
+    /// </summary>
+    public static Type GetType (string abbreviatedTypeName)
     {
-      if (assembly == type.Assembly)
-        return true;
+      ArgumentUtility.CheckNotNullOrEmpty ("abbreviatedTypeName", abbreviatedTypeName);
+      string typeName = TypeUtility.ParseAbbreviatedTypeName (abbreviatedTypeName);
+      return BuildManager.GetType (typeName, true);
     }
-    return false;
-  }
 
-	private WebTypeUtility()
-	{
-  }
-}
+    /// <summary>
+    ///   Loads a type, optionally using an abbreviated type name as defined in 
+    ///   <see cref="TypeUtility.ParseAbbreviatedTypeName"/>.
+    /// </summary>
+    public static Type GetType (string abbreviatedTypeName, bool throwOnError)
+    {
+      ArgumentUtility.CheckNotNullOrEmpty ("abbreviatedTypeName", abbreviatedTypeName);
+      string typeName = TypeUtility.ParseAbbreviatedTypeName (abbreviatedTypeName);
+      return BuildManager.GetType (typeName, throwOnError);
+    }
 
+    /// <summary>
+    ///   Loads a type, optionally using an abbreviated type name as defined in 
+    ///   <see cref="TypeUtility.ParseAbbreviatedTypeName"/>.
+    /// </summary>
+    public static Type GetType (string abbreviatedTypeName, bool throwOnError, bool ignoreCase)
+    {
+      ArgumentUtility.CheckNotNullOrEmpty ("abbreviatedTypeName", abbreviatedTypeName);
+      string typeName = TypeUtility.ParseAbbreviatedTypeName (abbreviatedTypeName);
+      return BuildManager.GetType (typeName, throwOnError, ignoreCase);
+    }
+
+    public static string GetQualifiedName (Type type)
+    {
+      ArgumentUtility.CheckNotNull ("type", type);
+      if (IsCompiledType (type))
+        return type.FullName;
+      return TypeUtility.GetPartialAssemblyQualifiedName (type);
+    }
+
+    public static bool IsCompiledType (Type type)
+    {
+      ArgumentUtility.CheckNotNull ("type", type);
+      IList codeAssemblies = BuildManager.CodeAssemblies;
+      if (codeAssemblies == null)
+        return false;
+
+      foreach (Assembly assembly in codeAssemblies)
+      {
+        if (assembly == type.Assembly)
+          return true;
+      }
+      return false;
+    }
+  }
 }
