2bc079a3a63a7bb06a8be1a894ec6adb0bf0cf59
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-01-05 16:30:59 +0000

    Implemented detection for installed .NET Framework version. (RM-5889)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@28968 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Core/Extensions.UnitTests/Utilities/FrameworkVersionDetector.cs
@@ -0,0 +1,62 @@
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using Microsoft.Win32;
+using NUnit.Framework;
+using Remotion.Utilities;
+
+namespace Remotion.UnitTests.Utilities
+{
+  [TestFixture]
+  public class FrameworkVersionDetectorTest
+  {
+    [Test]
+    public void IsMinimumVersion_Net_4_0 ()
+    {
+      Assert.That (FrameworkVersionDetector.IsVersionSupported (FrameworkVersion.Net_4_0), Is.True);
+    }
+
+    [Test]
+    public void IsMinimumVersion_Net_4_5 ()
+    {
+      Assert.That (FrameworkVersionDetector.IsVersionSupported (FrameworkVersion.Net_4_5), Is.True);
+    }
+
+    [Test]
+    public void IsMinimumVersion_Net_4_5_1 ()
+    {
+      Assert.That (FrameworkVersionDetector.IsVersionSupported (FrameworkVersion.Net_4_5_1), Is.EqualTo (IsNet_4_5_1_Installed()));
+    }
+
+    private bool IsNet_4_5_1_Installed ()
+    {
+      // http://msdn.microsoft.com/en-us/library/hh925568.aspx
+      // http://blogs.msdn.com/b/astebner/archive/2013/11/11/10466402.aspx
+
+      var registryKeyPath = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full";
+      using (var key = RegistryKey.OpenBaseKey (RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey (registryKeyPath))
+      {
+        Assertion.IsNotNull (key, "Registry key '{0}' not found.", registryKeyPath);
+        var release = (int?) key.GetValue ("Release");
+        Assertion.IsNotNull (release, "Registry value 'Release' not found.");
+
+        var net_4_5_1_ReleaseVersion = 378758;
+        return release >= net_4_5_1_ReleaseVersion;
+      }
+    }
+  }
+}
---
+++ Remotion/Core/Extensions/Utilities/FrameworkVersionDetector.cs
@@ -0,0 +1,53 @@
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+
+using System;
+
+namespace Remotion.Utilities
+{
+  /// <summary>
+  /// Allows for the testing of a specific .NET Framework version. This can be used to ensure a certain .NET Framework has been installed.
+  /// Otherwise, runtime expections can occur, e.g. a <see cref="MissingMethodException"/> can ben thrown.
+  /// </summary>
+  public static class FrameworkVersionDetector
+  {
+    private static readonly Lazy<bool> s_isNet_4_5_1_Supported =
+        new Lazy<bool> (() => Type.GetType ("System.Runtime.GCLargeObjectHeapCompactionMode", throwOnError: false) != null);
+
+    private static readonly Lazy<bool> s_isNet_4_5_Supported = 
+        new Lazy<bool> (() => Type.GetType ("System.Reflection.ReflectionContext", throwOnError: false) != null);
+
+    private static readonly Lazy<bool> s_isNet_4_0_Supported = new Lazy<bool> (() => Environment.Version.Major >= 4);
+
+    /// <summary>
+    /// Returns <see langword="true" /> if the <paramref name="frameworkVersion"/> is supported by the current system/runtime.
+    /// </summary>
+    public static bool IsVersionSupported (FrameworkVersion frameworkVersion)
+    {
+      if (frameworkVersion >= FrameworkVersion.Net_4_5_1)
+        return s_isNet_4_5_1_Supported.Value;
+
+      if (frameworkVersion >= FrameworkVersion.Net_4_5)
+        return s_isNet_4_5_Supported.Value;
+
+      if (frameworkVersion == FrameworkVersion.Net_4_0)
+        return s_isNet_4_0_Supported.Value;
+
+      throw new ArgumentException (string.Format("'{0}' is not a valid FrameworkVersion.", frameworkVersion));
+    }
+  }
+}
