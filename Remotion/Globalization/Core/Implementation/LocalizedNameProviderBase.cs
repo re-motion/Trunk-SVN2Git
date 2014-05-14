efdca3a1feef70bfc56d98e3b44d57201579171d
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-05-14 15:04:13 +0000

    temp commit: MultiLingualNameBasedMemberInformationGlobalizationService (RM-6155)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@29530 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Globalization/Core/Implementation/LocalizedNameProviderBase.cs
@@ -0,0 +1,101 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using JetBrains.Annotations;
+using Remotion.Utilities;
+
+namespace Remotion.Globalization.Implementation
+{
+  public abstract class LocalizedNameProviderBase<TReflectionObject>
+      where TReflectionObject : class
+  {
+    private readonly ConcurrentDictionary<TReflectionObject, Lazy<Dictionary<CultureInfo, string>>> _localizedTypeNamesForTypeInformation =
+        new ConcurrentDictionary<TReflectionObject, Lazy<Dictionary<CultureInfo, string>>>();
+
+    protected LocalizedNameProviderBase ()
+    {
+    }
+
+    [NotNull]
+    protected abstract MultiLingualNameAttribute[] GetCustomAttributes ([NotNull] TReflectionObject reflectionObject);
+
+    [NotNull]
+    protected abstract string GetContextForExceptionMessage ([NotNull] TReflectionObject reflectionObject);
+
+    [CanBeNull]
+    public string GetLocalizedNameForCurrentUICulture ([NotNull] TReflectionObject reflectionObject)
+    {
+      ArgumentUtility.CheckNotNull ("reflectionObject", reflectionObject);
+
+      var attributes = GetMultiLingualNameAttributesFromCache (reflectionObject);
+      if (!attributes.Any())
+        return null;
+
+      var currentUICulture = CultureInfo.CurrentUICulture;
+      foreach (var cultureInfo in currentUICulture.GetCultureHierarchy())
+      {
+        string localizedName;
+        if (attributes.TryGetValue (cultureInfo, out localizedName))
+          return localizedName;
+      }
+
+      throw new MissingLocalizationException (
+          string.Format (
+              "{0} has one or more MultiLingualNameAttributes applied "
+              + "but does not define a localization for the current UI culture '{1}' or a valid fallback culture "
+              + "(i.e. there is no localization defined for the invariant culture).",
+              GetContextForExceptionMessage (reflectionObject),
+              currentUICulture));
+    }
+
+    private Dictionary<CultureInfo, string> GetMultiLingualNameAttributesFromCache (TReflectionObject reflectionObject)
+    {
+      var lazyAttributes = _localizedTypeNamesForTypeInformation.GetOrAdd (
+          reflectionObject,
+          new Lazy<Dictionary<CultureInfo, string>> (
+              () => GetMultiLingualNameAttributes (reflectionObject),
+              LazyThreadSafetyMode.ExecutionAndPublication));
+
+      return lazyAttributes.Value;
+    }
+
+    private Dictionary<CultureInfo, string> GetMultiLingualNameAttributes (TReflectionObject reflectionObject)
+    {
+      var attributes = new Dictionary<CultureInfo, string>();
+      foreach (var attribute in GetCustomAttributes (reflectionObject))
+      {
+        if (attributes.ContainsKey (attribute.Culture))
+        {
+          throw new InvalidOperationException (
+              string.Format (
+                  "{0} has more than one MultiLingualNameAttribute for the culture '{1}' applied. "
+                  + "The used cultures must be unique within the set of MultiLingualNameAttributes for a type.",
+                  GetContextForExceptionMessage (reflectionObject),
+                  attribute.Culture));
+        }
+        attributes.Add (attribute.Culture, attribute.LocalizedName);
+      }
+      return attributes;
+    }
+  }
+}
--- Remotion/Globalization/Core/Implementation/MultiLingualNameBasedMemberInformationGlobalizationService.cs
+++ Remotion/Globalization/Core/Implementation/MultiLingualNameBasedMemberInformationGlobalizationService.cs
@@ -16,12 +16,6 @@
 // 
 
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading;
-using JetBrains.Annotations;
 using Remotion.Reflection;
 using Remotion.Utilities;
 
@@ -32,10 +26,26 @@ namespace Remotion.Globalization.Implementation
   /// applied to the respective reflection object.
   /// </summary>
   /// <threadsafety static="true" instance="true"/>
-  public class MultiLingualNameBasedMemberInformationGlobalizationService : IMemberInformationGlobalizationService
+  public sealed class MultiLingualNameBasedMemberInformationGlobalizationService : IMemberInformationGlobalizationService
   {
-    private readonly ConcurrentDictionary<ITypeInformation, Lazy<Dictionary<CultureInfo, string>>> _localizedTypeNamesForTypeInformation =
-        new ConcurrentDictionary<ITypeInformation, Lazy<Dictionary<CultureInfo, string>>>(); 
+    private class LocalizedNameForTypeInformationProvider : LocalizedNameProviderBase<ITypeInformation>
+    {
+      protected override MultiLingualNameAttribute[] GetCustomAttributes (ITypeInformation reflectionObject)
+      {
+        ArgumentUtility.CheckNotNull ("reflectionObject", reflectionObject);
+
+        return reflectionObject.GetCustomAttributes<MultiLingualNameAttribute> (false);
+      }
+
+      protected override string GetContextForExceptionMessage (ITypeInformation reflectionObject)
+      {
+        ArgumentUtility.CheckNotNull ("reflectionObject", reflectionObject);
+
+        return string.Format ("The type '{0}'", reflectionObject.FullName);
+      }
+    }
+
+    private readonly LocalizedNameForTypeInformationProvider _localizedNameForTypeInformationProvider = new LocalizedNameForTypeInformationProvider();
 
     public MultiLingualNameBasedMemberInformationGlobalizationService ()
     {
@@ -48,7 +58,7 @@ namespace Remotion.Globalization.Implementation
 
       for (var currentTypeInformation = typeInformation; currentTypeInformation != null; currentTypeInformation = currentTypeInformation.BaseType)
       {
-        var localizedName = GetLocalizedNameForCurrentUICulture (currentTypeInformation);
+        var localizedName = _localizedNameForTypeInformationProvider.GetLocalizedNameForCurrentUICulture (currentTypeInformation);
         if (localizedName != null)
         {
           result = localizedName;
@@ -66,60 +76,6 @@ namespace Remotion.Globalization.Implementation
         out string result)
     {
       throw new NotImplementedException();
-    }
-
-    [CanBeNull]
-    private string GetLocalizedNameForCurrentUICulture (ITypeInformation typeInformation)
-    {
-      var attributes = GetMultiLingualNameAttributesFromCache (typeInformation);
-      if (!attributes.Any())
-        return null;
-
-      var currentUICulture = CultureInfo.CurrentUICulture;
-      foreach (var cultureInfo in currentUICulture.GetCultureHierarchy())
-      {
-        string localizedName;
-        if (attributes.TryGetValue (cultureInfo, out localizedName))
-          return localizedName;
-      }
-
-      throw new MissingLocalizationException (
-          string.Format (
-              "The type '{0}' has one or more MultiLingualNameAttributes applied "
-              + "but does not define a localization for the current UI culture '{1}' or a valid fallback culture "
-              + "(i.e. there is no localization defined for the invariant culture).",
-              typeInformation.FullName,
-              currentUICulture));
-    }
-
-    private Dictionary<CultureInfo, string> GetMultiLingualNameAttributesFromCache (ITypeInformation typeInformation)
-    {
-      var lazyAttributes = _localizedTypeNamesForTypeInformation.GetOrAdd (
-          typeInformation,
-          new Lazy<Dictionary<CultureInfo, string>> (
-              () => GetMultiLingualNameAttributes (typeInformation),
-              LazyThreadSafetyMode.ExecutionAndPublication));
-
-      return lazyAttributes.Value;
-    }
-
-    private Dictionary<CultureInfo, string> GetMultiLingualNameAttributes (ITypeInformation typeInformation)
-    {
-      var attributes = new Dictionary<CultureInfo, string>();
-      foreach (var attribute in typeInformation.GetCustomAttributes<MultiLingualNameAttribute> (false))
-      {
-        if (attributes.ContainsKey (attribute.Culture))
-        {
-          throw new InvalidOperationException (
-              string.Format (
-                  "The type '{0}' has more than one MultiLingualNameAttribute for the culture '{1}' applied. "
-                  + "The used cultures must be unique within the set of MultiLingualNameAttributes for a type.",
-                  typeInformation.FullName,
-                  attribute.Culture));
-        }
-        attributes.Add (attribute.Culture, attribute.LocalizedName);
-      }
-      return attributes;
     }
   }
 }
