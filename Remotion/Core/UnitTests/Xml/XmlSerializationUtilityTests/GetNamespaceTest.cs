682a2dbb3dad2301551ceca01eb70f1f1944e7b4
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-07-27 13:02:11 +0000

    Dropped Castle.Core dependency from Remotion.UnitTests project. (RM-6235)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@29760 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Core/UnitTests/Xml/XmlSerializationUtilityTests/GetNamespaceTest.cs
+++ Remotion/Core/UnitTests/Xml/XmlSerializationUtilityTests/GetNamespaceTest.cs
@@ -14,13 +14,10 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
+
 using System;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Xml.Serialization;
-using Castle.DynamicProxy;
-using Castle.DynamicProxy.Generators.Emitters;
 using NUnit.Framework;
+using Remotion.UnitTests.Xml.XmlSerializationUtilityTests.TestDomain;
 using Remotion.Xml;
 
 namespace Remotion.UnitTests.Xml.XmlSerializationUtilityTests
@@ -29,80 +26,48 @@ namespace Remotion.UnitTests.Xml.XmlSerializationUtilityTests
   public class GetNamespaceTest
   {
     [Test]
-    public void WithXmlTypeAttribute()
+    public void WithXmlTypeAttribute ()
     {
-      Type type = CreateType ("SampleType", CreateXmlTypeAttributeBuilder ("http://type-namespace"));
-      Assert.That (XmlSerializationUtility.GetNamespace (type), Is.EqualTo ("http://type-namespace"));
+      Assert.That (XmlSerializationUtility.GetNamespace (typeof (SampleTypeWithXmlType)), Is.EqualTo ("http://type-namespace"));
     }
 
     [Test]
-    [ExpectedException (typeof (ArgumentException), ExpectedMessage = "Cannot determine the xml namespace of type 'SampleType' because neither an "
-        + "XmlTypeAttribute nor an XmlRootAttribute is used to define a namespace for the type.\r\nParameter name: type")]
-    public void WithXmlTypeAttributeWithoutNamespace()
+    [ExpectedException (typeof (ArgumentException), ExpectedMessage =
+        "Cannot determine the xml namespace of type 'Remotion.UnitTests.Xml.XmlSerializationUtilityTests.TestDomain.SampleTypeWithXmlTypeWithoutNamespace' "
+        + "because neither an XmlTypeAttribute nor an XmlRootAttribute is used to define a namespace for the type.\r\nParameter name: type")]
+    public void WithXmlTypeAttributeWithoutNamespace ()
     {
-      Type type = CreateType ("SampleType", CreateXmlTypeAttributeBuilder (null));
-      XmlSerializationUtility.GetNamespace (type);
+      XmlSerializationUtility.GetNamespace (typeof (SampleTypeWithXmlTypeWithoutNamespace));
     }
 
     [Test]
-    public void WithXmlRootAttribute()
+    public void WithXmlRootAttribute ()
     {
-      Type type = CreateType ("SampleType", CreateXmlRootAttributeBuilder ("http://root-namespace"));
-      Assert.That (XmlSerializationUtility.GetNamespace (type), Is.EqualTo ("http://root-namespace"));
+      Assert.That (XmlSerializationUtility.GetNamespace (typeof (SampleTypeWithXmlRoot)), Is.EqualTo ("http://root-namespace"));
     }
 
     [Test]
-    [ExpectedException (typeof (ArgumentException), ExpectedMessage = "Cannot determine the xml namespace of type 'SampleType' because neither an "
-        + "XmlTypeAttribute nor an XmlRootAttribute is used to define a namespace for the type.\r\nParameter name: type")]
-    public void WithXmlRootAttributeWithoutNamespace()
+    [ExpectedException (typeof (ArgumentException), ExpectedMessage =
+        "Cannot determine the xml namespace of type 'Remotion.UnitTests.Xml.XmlSerializationUtilityTests.TestDomain.SampleTypeWithXmlRootWithoutNamespace' "
+        + "because neither an XmlTypeAttribute nor an XmlRootAttribute is used to define a namespace for the type.\r\nParameter name: type")]
+    public void WithXmlRootAttributeWithoutNamespace ()
     {
-      Type type = CreateType ("SampleType", CreateXmlRootAttributeBuilder (null));
-      XmlSerializationUtility.GetNamespace (type);
+      XmlSerializationUtility.GetNamespace (typeof (SampleTypeWithXmlRootWithoutNamespace));
     }
 
     [Test]
-    public void WithXmlRootAttributeWithTypeAlsoHavingAnXmlTypeAttribute()
+    public void WithXmlRootAttributeWithTypeAlsoHavingAnXmlTypeAttribute ()
     {
-      Type type = CreateType (
-          "SampleType",
-          CreateXmlTypeAttributeBuilder ("http://type-namespace"),
-          CreateXmlRootAttributeBuilder ("http://root-namespace"));
-      Assert.That (XmlSerializationUtility.GetNamespace (type), Is.EqualTo ("http://root-namespace"));
+      Assert.That (XmlSerializationUtility.GetNamespace (typeof (SampleTypeWithXmlRootAndXmlType)), Is.EqualTo ("http://root-namespace"));
     }
 
     [Test]
-    [ExpectedException (typeof (ArgumentException), ExpectedMessage = "Cannot determine the xml namespace of type 'SampleType' because no neither an XmlTypeAttribute"
-        + " nor an XmlRootAttribute has been provided.\r\nParameter name: type")]
-    public void WithoutXmlRootAttributeAndWithoutXmlTypeAttribute()
+    [ExpectedException (typeof (ArgumentException), ExpectedMessage =
+        "Cannot determine the xml namespace of type 'Remotion.UnitTests.Xml.XmlSerializationUtilityTests.TestDomain.SampleTypeWithoutXmlAttributes' "
+        + "because no neither an XmlTypeAttribute nor an XmlRootAttribute has been provided.\r\nParameter name: type")]
+    public void WithoutXmlRootAttributeAndWithoutXmlTypeAttribute ()
     {
-      Type type = CreateType ("SampleType");
-      XmlSerializationUtility.GetNamespace (type);
-    }
-
-    private Type CreateType (string typeName, params CustomAttributeBuilder[] attributeBuilders)
-    {
-      ModuleScope moduleScope = new ModuleScope();
-      ClassEmitter classEmitter = new ClassEmitter (moduleScope, typeName, typeof (object), new Type [0]);
-      foreach (CustomAttributeBuilder attributeBuilder in attributeBuilders)
-        classEmitter.TypeBuilder.SetCustomAttribute (attributeBuilder);
-
-      return classEmitter.BuildType ();
-    }
-
-    private CustomAttributeBuilder CreateXmlTypeAttributeBuilder (string @namespace)
-    {
-      ConstructorInfo constructorInfo = typeof (XmlTypeAttribute).GetConstructor (new Type[0]);
-      PropertyInfo namespacePropertyInfo = typeof (XmlTypeAttribute).GetProperty ("Namespace");
-
-      return new CustomAttributeBuilder (constructorInfo, new object[0], new PropertyInfo[] {namespacePropertyInfo}, new object[] {@namespace});
-    }
-
-    private CustomAttributeBuilder CreateXmlRootAttributeBuilder (string @namespace)
-    {
-      ConstructorInfo constructorInfo = typeof (XmlRootAttribute).GetConstructor (new Type[0]);
-      PropertyInfo namespacePropertyInfo = typeof (XmlRootAttribute).GetProperty ("Namespace");
-
-      return new CustomAttributeBuilder (constructorInfo, new object[0], new PropertyInfo[] {namespacePropertyInfo}, new object[] {@namespace});
+      XmlSerializationUtility.GetNamespace (typeof (SampleTypeWithoutXmlAttributes));
     }
   }
 }
