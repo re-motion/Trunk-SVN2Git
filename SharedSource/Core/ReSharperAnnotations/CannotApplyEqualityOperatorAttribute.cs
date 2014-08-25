bd95fd6f7e75fa64ed5e0c5d2b6d553549e4f527
michael.ketting <michael.ketting@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-08-25 15:22:08 +0000

    Fixed ReSharper warnings via comments.
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@29844 cb0deee2-328e-b949-94ea-fb3139fab790

--- SharedSource/Core/ReSharperAnnotations/CannotApplyEqualityOperatorAttribute.cs
+++ SharedSource/Core/ReSharperAnnotations/CannotApplyEqualityOperatorAttribute.cs
@@ -37,6 +37,15 @@
 
 using System;
 
+#pragma warning disable 1591
+// ReSharper disable UnusedMember.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable IntroduceOptionalParameters.Global
+// ReSharper disable MemberCanBeProtected.Global
+// ReSharper disable InconsistentNaming
+// ReSharper disable once CheckNamespace
+
 namespace JetBrains.Annotations
 {
   /// <summary>
