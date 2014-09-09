678f933413ad9ee3e568cc26c984d62fe38eae45
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-09-09 16:30:35 +0000

    Added initial base classes for page and control objects. Added WebTestingObjectContext class. Added a Todo.cs file for general feature-related Todo comments. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@29868 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/Development.WebTesting/ControlObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Remotion.Web.Development.WebTesting
+{
+  /// <summary>
+  /// Base class for all control objects. Much like <see cref="PageObject"/>s, control objects hide the actual HTML structure from the web test
+  /// developer and instead provide a semantic interface. In contrast to <see cref="PageObject"/>s, control objects represent a specific
+  /// ASP.NET (custom) control and not a whole page.
+  /// </summary>
+  public class ControlObject : WebTestingObject
+  {
+    /// <summary>
+    /// Initializes the control object.
+    /// </summary>
+    /// <param name="context">The control's context.</param>
+    public ControlObject (WebTestingObjectContext context)
+        : base (context)
+    {
+    }
+  }
+}
---
+++ Remotion/Web/Development.WebTesting/PageObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Remotion.Web.Development.WebTesting
+{
+  /// <summary>
+  /// Base class for all page objects.
+  /// </summary>
+  /// <remarks>
+  /// See http://martinfowler.com/bliki/PageObject.html or https://code.google.com/p/selenium/wiki/PageObjects for more information on the page object
+  /// pattern.
+  /// </remarks>
+  public abstract class PageObject : WebTestingObject
+  {
+    /// <summary>
+    /// Initializes the page object.
+    /// </summary>
+    /// <param name="context">The page's context.</param>
+    protected PageObject (WebTestingObjectContext context)
+        : base (context)
+    {
+    }
+
+    /// <summary>
+    /// Returns the page's title.<br/>
+    /// The default implementation returns the corresponding window's title, defined by the HTML &lt;title&gt; tag.
+    /// </summary>
+    public virtual string Title
+    {
+      get { return Context.Window.Title; }
+    }
+  }
+}
