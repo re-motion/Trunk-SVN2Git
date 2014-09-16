8c8dc6dfa171e481681742e6062b52fe34bb147d
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-09-16 16:10:16 +0000

    Implemented BocAutoCompleteReferenceValueControlObject & BocAutoCompleteReferenceValueSelector (+ immediately extracted BocSelectorBase class). (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@29894 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/ObjectBinding/Web.Development.WebTesting/BocAutoCompleteReferenceValueSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Remotion.Web.Development.WebTesting;
+
+namespace Remotion.ObjectBinding.Web.Development.WebTesting
+{
+  /// <summary>
+  /// Control object selector for <see cref="BocAutoCompleteReferenceValueControlObject"/>.
+  /// </summary>
+  public class BocAutoCompleteReferenceValueSelector : BocSelectorBase<BocAutoCompleteReferenceValueControlObject>
+  {
+    public override BocAutoCompleteReferenceValueControlObject FindTypedControl (
+        TestObjectContext context,
+        BocControlSelectionParameters selectionParameters)
+    {
+      var scope = FindScope (context, selectionParameters);
+      return new BocAutoCompleteReferenceValueControlObject (scope.Id, context.CloneForScope (scope));
+    }
+  }
+}
