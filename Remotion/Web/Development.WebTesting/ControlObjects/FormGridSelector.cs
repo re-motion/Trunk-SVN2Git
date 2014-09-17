594430820a0ce6d3c3638bac65368b277d8038c7
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-09-17 14:49:29 +0000

    Moved XPathUtils class into own file, added comments. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@29908 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Development.WebTesting/ControlObjects/FormGridSelector.cs
+++ Remotion/Web/Development.WebTesting/ControlObjects/FormGridSelector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Coypu;
+using Remotion.Web.Development.WebTesting.Utilities;
 
 namespace Remotion.Web.Development.WebTesting.ControlObjects
 {
@@ -27,24 +28,22 @@ namespace Remotion.Web.Development.WebTesting.ControlObjects
         // scope = context.Scope.FindCss (string.Format ("table[{0}='{1}']", DiagnosticMetadataAttributes.Title, selectionParameters.Title));
 
         // This implementation assumes that the CSS class of the title cell must be formGridTitleCell.
-        scope = context.Scope.FindXPath (string.Format (".//table[contains(tbody/tr/td{0},'{1}')]", XPathUtils.CreateContainsClassCheck("formGridTitleCell"), selectionParameters.Title));
+        scope = context.Scope.FindXPath (
+            string.Format (
+                ".//table[contains(tbody/tr/td{0},'{1}')]",
+                XPathUtils.CreateContainsClassCheck ("formGridTitleCell"),
+                selectionParameters.Title));
 
         // This alterantive implementation assumes that the formGridTitleCell is in the very first row and column.
         //scope = context.Scope.FindXPath (string.Format (".//table[contains(tbody/tr[1]/td[1],'{0}')]", selectionParameters.Title));
       }
       else
-          // If no parameters are given, assume that the default form (= the only form) on the page has been requested
+      {
+        // If no parameters are given, assume that the default form (= the only form) on the page has been requested
         scope = context.Scope.FindCss ("." + c_cssClass, Options.Single);
+      }
 
       return new FormGridControlObject (scope.Id, context.CloneForScope (scope));
-    }
-  }
-
-  public static class XPathUtils
-  {
-    public static string CreateContainsClassCheck (string cssClass)
-    {
-      return string.Format ("[contains(concat(' ', normalize-space(@class), ' '), ' {0} ')]", cssClass);
     }
   }
 }
---
+++ Remotion/Web/Development.WebTesting/Utilities/XPathUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Remotion.Web.Development.WebTesting.Utilities
+{
+  /// <summary>
+  /// Utilities class for various XPath-related tasks.
+  /// </summary>
+  public static class XPathUtils
+  {
+    /// <summary>
+    /// Creates an XPath check constraint for a specific CSS class.
+    /// </summary>
+    /// <remarks>
+    /// See http://stackoverflow.com/a/9133579/1400869 for the source of the check constraint.
+    /// </remarks>
+    /// <param name="cssClass">The CSS class to check for.</param>
+    /// <returns>The XPath check constraint.</returns>
+    public static string CreateContainsClassCheck (string cssClass)
+    {
+      return string.Format ("[contains(concat(' ', normalize-space(@class), ' '), ' {0} ')]", cssClass);
+    }
+  }
+}
