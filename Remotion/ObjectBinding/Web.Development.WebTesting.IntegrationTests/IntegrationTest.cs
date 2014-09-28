ce8497d44dc010fba446bab1132892c46d4b91e9
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-09-28 19:26:38 +0000

    Ensured integration test independence in Web.Development.WebTesting.IntegrationTests project. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@29943 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/ObjectBinding/Web.Development.WebTesting.IntegrationTests/IntegrationTest.cs
+++ Remotion/ObjectBinding/Web.Development.WebTesting.IntegrationTests/IntegrationTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Coypu;
 using NUnit.Framework;
+using OpenQA.Selenium;
 using Remotion.Web.Development.WebTesting;
 using Remotion.Web.Development.WebTesting.PageObjects;
 
@@ -39,8 +41,24 @@ namespace Remotion.ObjectBinding.Web.Development.WebTesting.IntegrationTests
 
     protected RemotionPageObject Start ()
     {
+      var url = _webTestHelper.Configuration.WebApplicationRoot;
+      _webTestHelper.MainBrowserSession.Visit (url);
+      AcceptPossibleModalDialog();
+
       var context = TestObjectContext.New (_webTestHelper.MainBrowserSession);
       return new UnspecifiedPageObject (context).Expect<RemotionPageObject>();
+    }
+
+    private void AcceptPossibleModalDialog ()
+    {
+      try
+      {
+        _webTestHelper.MainBrowserSession.AcceptModalDialog (Options.NoWait);
+      }
+      catch (NoAlertPresentException)
+      {
+        // It's okay.
+      }
     }
   }
 }
