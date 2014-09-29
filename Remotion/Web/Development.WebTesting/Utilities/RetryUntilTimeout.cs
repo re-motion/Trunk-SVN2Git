b7aebf1f041c126e10baab1683478fd24bfdbb20
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-09-29 15:04:45 +0000

    Added RetryUntilTimeout utility class for .FindAllCss selectors (required for some ActaNova control objects, e.g. ActaNovaBreadCrumb selection). (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@29946 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/Development.WebTesting/Utilities/RetryUntilTimeout.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using log4net;
+
+namespace Remotion.Web.Development.WebTesting.Utilities
+{
+  /// <summary>
+  /// Executes a given <see cref="Func{TReturnType}"/> repeatedly (using the given retry interval) until no exception is thrown during execution or
+  /// until the given timeout has been reached (in which case the final exception is rethrown).
+  /// </summary>
+  public class RetryUntilTimeout<TReturnType>
+  {
+    private static readonly ILog s_log = LogManager.GetLogger (typeof (RetryUntilTimeout<TReturnType>));
+
+    private readonly Func<TReturnType> _func;
+    private readonly TimeSpan _timeout;
+    private readonly TimeSpan _retryInterval;
+
+    public RetryUntilTimeout (Func<TReturnType> func, TimeSpan timeout, TimeSpan retryInterval)
+    {
+      _func = func;
+      _timeout = timeout;
+      _retryInterval = retryInterval;
+    }
+
+    public TReturnType Run ()
+    {
+      var stopwatch = Stopwatch.StartNew();
+
+      do
+      {
+        try
+        {
+          return _func();
+        }
+        catch (Exception ex)
+        {
+          if (stopwatch.ElapsedMilliseconds < _timeout.TotalMilliseconds)
+          {
+            s_log.Debug ("RetryUntilTimeout failed with " + ex.GetType().Name + " - trying again.");
+            Thread.Sleep (_retryInterval);
+          }
+          else
+          {
+            s_log.Warn ("RetryUntilTimeout failed with " + ex.GetType().Name + " - timeout elapsed, failing.");
+            throw;
+          }
+        }
+      } while (true);
+    }
+  }
+}
