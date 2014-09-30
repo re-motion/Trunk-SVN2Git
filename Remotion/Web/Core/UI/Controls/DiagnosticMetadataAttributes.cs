7f2be6f96e4ee4a8527df408a4c881ba41849b91
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-09-30 14:57:01 +0000

    Added DiagnosticMetadataAttributes.CommandName. Changed DiagnosticMetadataAttributes.TriggersPostBack attribute name to "data-triggers-pb". (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@29956 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Core/UI/Controls/DiagnosticMetadataAttributes.cs
+++ Remotion/Web/Core/UI/Controls/DiagnosticMetadataAttributes.cs
@@ -7,13 +7,15 @@ namespace Remotion.Web.UI.Controls
   /// </summary>
   public static class DiagnosticMetadataAttributes
   {
-    // TODO RM-6297: Move BOC-revelant bits to ObjectBinding asssembly.
     // TODO RM-6297: When would title be needed, i.e. title is usally an attribute
+    public static readonly string CommandName = "data-commandname";
     public static readonly string DisplayName = "data-displayname";
     public static readonly string Title = "data-title";
-    public static readonly string TriggersPostBack = "data-autopostback";
+    public static readonly string TriggersPostBack = "data-triggers-pb";
     public static readonly string TriggersNavigation = "data-triggers-nav";
     public static readonly string IsReadOnly = "data-is-readonly";
+
+    // TODO RM-6297: Move BOC-revelant bits to ObjectBinding asssembly.
     public static readonly string IsBound = "data-is-bound";
     public static readonly string BoundType = "data-bound-type";
     public static readonly string BoundProperty = "data-bound-property";
