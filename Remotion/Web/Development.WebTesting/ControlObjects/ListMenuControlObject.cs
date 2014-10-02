937a969046d96eeac9fcd5b6f4d97c5363ac6755
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-10-02 13:05:50 +0000

    Added draft for ListMenuControlObject. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@29983 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/Development.WebTesting/ControlObjects/ListMenuControlObject.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Remotion.Web.UI.Controls;
+
+namespace Remotion.Web.Development.WebTesting.ControlObjects
+{
+  /// <summary>
+  /// Control object for <see cref="ListMenu"/>.
+  /// </summary>
+  public class ListMenuControlObject : RemotionControlObject
+  {
+     public ListMenuControlObject ([NotNull] string id, [NotNull] TestObjectContext context)
+        : base (id, context)
+    {
+    }
+  }
+}
