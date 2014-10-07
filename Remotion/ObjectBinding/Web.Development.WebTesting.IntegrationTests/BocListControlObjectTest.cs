b147923f1cd4471b024b859007ca4023d0daf911
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-10-07 09:40:18 +0000

    Added BocListControlObject incl. BocListControlObjectTest integration tests. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30022 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/ObjectBinding/Web.Development.WebTesting.IntegrationTests/BocListControlObjectTest.cs
+++ Remotion/ObjectBinding/Web.Development.WebTesting.IntegrationTests/BocListControlObjectTest.cs
@@ -88,7 +88,262 @@ namespace Remotion.ObjectBinding.Web.Development.WebTesting.IntegrationTests
       Assert.That (bocList.Scope.Id, Is.EqualTo ("body_DataEditControl_JobList_Normal"));
     }
 
-    private RemotionPageObject Start()
+    [Test]
+    public void TestGetHeaderLabels ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      Assert.That (
+          bocList.GetHeaderLabels(),
+          Is.EquivalentTo (new[] { "I_ndex", "", " ", "Command", "Menu", "Title", "StartDate", "EndDate", "DisplayName", "TitleWithCmd" }));
+    }
+
+    [Test]
+    public void TestGetRowCount ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      Assert.That (bocList.GetRowCount(), Is.EqualTo (2));
+    }
+
+    [Test]
+    public void TestPaging ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      Assert.That (bocList.GetNumberOfPages(), Is.EqualTo (3));
+
+      bocList.GoToNextPage();
+      Assert.That (bocList.GetCurrentPage(), Is.EqualTo (2));
+
+      bocList.GoToPreviousPage();
+      Assert.That (bocList.GetCurrentPage(), Is.EqualTo (1));
+
+      bocList.GoToLastPage();
+      Assert.That (bocList.GetCurrentPage(), Is.EqualTo (3));
+      Assert.That (bocList.GetRowCount(), Is.EqualTo (1));
+
+      bocList.GoToFirstPage();
+      Assert.That (bocList.GetCurrentPage(), Is.EqualTo (1));
+
+      bocList.GoToSpecificPage (3);
+      Assert.That (bocList.GetCurrentPage(), Is.EqualTo (3));
+    }
+
+    [Test]
+    public void TestGetRow ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var row = bocList.GetRow (1);
+
+      Assert.That (row.GetCell (6).GetText(), Is.EqualTo ("Programmer"));
+    }
+
+    [Test]
+    public void TestGetRowWhere ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var row = bocList.GetRowWhere ("DisplayName", "CEO");
+
+      Assert.That (row.GetCell (6).GetText(), Is.EqualTo ("CEO"));
+    }
+
+    [Test]
+    public void TestGetCellWhere ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var cell = bocList.GetCellWhere ("DisplayName", "CEO");
+
+      Assert.That (cell.GetText(), Is.EqualTo ("CEO"));
+    }
+
+    [Test]
+    public void TestClickOnSortColumn ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      bocList.ClickOnSortColumn ("StartDate");
+      bocList.ClickOnSortColumn ("Title");
+
+      Assert.That (bocList.GetRow (2).GetCell (6).GetText(), Is.EqualTo ("Programmer"));
+    }
+
+    [Test]
+    [Ignore ("Ignored until BocListControlObject.ChangeViewTo(itemID) has been implemented.")]
+    public void TestChangeViewTo ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+
+      bocList.ChangeViewToByLabel ("View 1");
+      Assert.That (home.Scope.FindIdEndingWith ("SelectedViewLabel").Text, Is.EqualTo ("ViewCmd1"));
+
+      bocList.ChangeViewTo ("ViewCmd2");
+      Assert.That (home.Scope.FindIdEndingWith ("SelectedViewLabel").Text, Is.EqualTo ("ViewCmd2"));
+    }
+
+    [Test]
+    [Ignore ("Ignored until DropDownMenuControlObject has been implemented.")]
+    public void TestGetDropDownMenu ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var dropDownMenu = bocList.GetDropDownMenu();
+      dropDownMenu.ClickItem ("OptCmd2");
+
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderLabel").Text, Is.EqualTo ("JobList_Normal"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderRowLabel").Text, Is.EqualTo ("-1"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedLabel").Text, Is.EqualTo ("ListMenuOrOptionsClick"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedParameterLabel").Text, Is.EqualTo ("OptCmd2|Option command 2"));
+    }
+
+    [Test]
+    public void TestGetListMenu ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var listMenu = bocList.GetListMenu();
+      listMenu.ClickItem ("ListMenuCmd3");
+
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderLabel").Text, Is.EqualTo ("JobList_Normal"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderRowLabel").Text, Is.EqualTo ("-1"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedLabel").Text, Is.EqualTo ("ListMenuOrOptionsClick"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedParameterLabel").Text, Is.EqualTo ("ListMenuCmd3|LM cmd 3"));
+    }
+
+    [Test]
+    public void TestRowClickSelectCheckbox ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var row = bocList.GetRow (2);
+
+      row.ClickSelectCheckbox();
+      row.GetCell (4).PerformCommand();
+
+      Assert.That (home.Scope.FindIdEndingWith ("SelectedIndicesLabel").Text, Is.EqualTo ("1"));
+    }
+
+    [Test]
+    [Ignore ("Ignored until DropDownMenuControlObject has been implemented.")]
+    public void TestRowGetRowDropDownMenu ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var row = bocList.GetRow (2);
+      var dropDownMenu = row.GetRowDropDownMenu();
+      dropDownMenu.ClickItem ("RowMenuItemCmd2");
+
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderLabel").Text, Is.EqualTo ("JobList_Normal"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderRowLabel").Text, Is.EqualTo ("1"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedLabel").Text, Is.EqualTo ("RowContextMenuClick"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedParameterLabel").Text, Is.EqualTo ("RowMenuItemCmd2|Row menu 2"));
+    }
+
+    [Test]
+    public void TestRowEdit ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var row = bocList.GetRow (2);
+
+      Assert.That (home.Scope.FindIdEndingWith ("EditModeLabel").Text, Is.EqualTo ("False"));
+
+      row.Edit();
+      Assert.That (home.Scope.FindIdEndingWith ("EditModeLabel").Text, Is.EqualTo ("True"));
+    }
+
+    [Test]
+    public void TestEditableRowSave ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var editableRow = bocList.GetRow (2).Edit();
+
+      editableRow.Save();
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderLabel").Text, Is.EqualTo ("JobList_Normal"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderRowLabel").Text, Is.EqualTo ("1"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedLabel").Text, Is.EqualTo ("InLineEdit"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedParameterLabel").Text, Is.EqualTo ("Saved"));
+    }
+
+    [Test]
+    public void TestEditableRowCancel ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var editableRow = bocList.GetRow (2).Edit();
+
+      editableRow.Cancel();
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderLabel").Text, Is.EqualTo ("JobList_Normal"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderRowLabel").Text, Is.EqualTo ("1"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedLabel").Text, Is.EqualTo ("InLineEdit"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedParameterLabel").Text, Is.EqualTo ("Canceled"));
+    }
+
+    [Test]
+    public void TestCellGetText ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var cell = bocList.GetRow (2).GetCell (9);
+
+      Assert.That (cell.GetText(), Is.EqualTo ("CEO"));
+    }
+
+    [Test]
+    public void TestCellPerformCommand ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var cell = bocList.GetRow (2).GetCell (4);
+
+      cell.PerformCommand();
+
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderLabel").Text, Is.EqualTo ("JobList_Normal"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedSenderRowLabel").Text, Is.EqualTo ("1"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedLabel").Text, Is.EqualTo ("CellCommandClick"));
+      Assert.That (home.Scope.FindIdEndingWith ("ActionPerformedParameterLabel").Text, Is.EqualTo ("|Command")); // Todo RM-6297: ItemID?
+    }
+
+    [Test]
+    [Ignore ("Ignored until BocText has been implemented.")]
+    public void TestEditableCellGetControl ()
+    {
+      var home = Start();
+
+      var bocList = home.GetList().ByLocalID ("JobList_Normal");
+      var editableRow = bocList.GetRow (2).Edit();
+      var editableCell = editableRow.GetCell (6);
+
+      //var bocText = editableCell.GetControl (new FirstControlSelectionCommand<BocText>(new BocTextSelector()));
+      //bocText.SetText ("NewTitle");
+
+      editableRow.Save();
+      Assert.That (bocList.GetCellWhere ("Title", "NewTitle").GetText(), Is.EqualTo ("NewTitle"));
+    }
+
+    private RemotionPageObject Start ()
     {
       return Start ("BocList");
     }
--- Remotion/ObjectBinding/Web.Development.WebTesting/ControlObjects/BocListControlObject.cs
+++ Remotion/ObjectBinding/Web.Development.WebTesting/ControlObjects/BocListControlObject.cs
@@ -1,6 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Coypu;
 using JetBrains.Annotations;
+using OpenQA.Selenium;
 using Remotion.Web.Development.WebTesting;
+using Remotion.Web.Development.WebTesting.ControlObjects;
+using Remotion.Web.Development.WebTesting.ControlObjects.Selectors;
+using Remotion.Web.Development.WebTesting.ControlSelection;
+using Remotion.Web.Development.WebTesting.Utilities;
+using Remotion.Web.Development.WebTesting.WaitingStrategies;
+using Remotion.Web.UI.Controls;
 
 namespace Remotion.ObjectBinding.Web.Development.WebTesting.ControlObjects
 {
@@ -9,9 +19,294 @@ namespace Remotion.ObjectBinding.Web.Development.WebTesting.ControlObjects
   /// </summary>
   public class BocListControlObject : BocControlObject
   {
+    private readonly List<string> _headerLabels;
+
     public BocListControlObject ([NotNull] string id, [NotNull] TestObjectContext context)
         : base (id, context)
     {
+      _headerLabels =
+          new RetryUntilTimeout<List<string>> (
+              () => Scope.FindAllCss (".bocListFakeTableHead th").Select (s => s.Text).ToList(),
+              Context.Configuration.SearchTimeout,
+              Context.Configuration.RetryInterval).Run();
+    }
+
+    public IReadOnlyCollection<string> GetHeaderLabels ()
+    {
+      return _headerLabels;
+    }
+
+    public int GetRowCount ()
+    {
+      return new RetryUntilTimeout<int> (
+          () => Scope.FindAllCss (".bocListTable .bocListTableBody tr").Count(),
+          Context.Configuration.SearchTimeout,
+          Context.Configuration.RetryInterval).Run();
+    }
+
+    public int GetCurrentPage ()
+    {
+      var currentPageTextInputScope = GetCurrentPageTextInputScope();
+      return int.Parse (currentPageTextInputScope.Value);
+    }
+
+    public int GetNumberOfPages ()
+    {
+      var navigatorDivScope = Scope.FindCss (".bocListNavigator"); // Todo RM-6297: render directly on bocList root element?
+      return int.Parse (navigatorDivScope[DiagnosticMetadataAttributes.BocListNumberOfPages]);
+    }
+
+    public void GoToSpecificPage (int page)
+    {
+      var currentPageTextInputScope = GetCurrentPageTextInputScope();
+      Action<ElementScope> fillAction = s =>
+      {
+        s.SendKeys (Keys.Backspace + page); // JS prevents clearing, so FillWith is not possible
+        Then.PressEnter (s);
+      };
+      currentPageTextInputScope.PerformActionUsingWaitStrategy (fillAction, Context, WaitFor.WxePostBack);
+    }
+
+    public void GoToFirstPage ()
+    {
+      var firstPageLinkScope = FindChild ("Navigation_First");
+      firstPageLinkScope.ClickAndWait (Context, WaitFor.WxePostBack);
+    }
+
+    public void GoToPreviousPage ()
+    {
+      var previousPageLinkScope = FindChild ("Navigation_Previous");
+      previousPageLinkScope.ClickAndWait (Context, WaitFor.WxePostBack);
+    }
+
+    public void GoToNextPage ()
+    {
+      var nextPageLinkScope = FindChild ("Navigation_Next");
+      nextPageLinkScope.ClickAndWait (Context, WaitFor.WxePostBack);
+    }
+
+    public void GoToLastPage ()
+    {
+      var lastPageLinkScope = FindChild ("Navigation_Last");
+      lastPageLinkScope.ClickAndWait (Context, WaitFor.WxePostBack);
+    }
+
+    public BocListRowControlObject GetRow (int index)
+    {
+      var cssSelector = string.Format (
+          ".bocListTable .bocListTableBody .bocListDataRow[{0}='{1}']",
+          DiagnosticMetadataAttributes.BocListRowIndex,
+          index);
+      var rowScope = Scope.FindCss (cssSelector);
+      rowScope.Now(); // Todo RM-6297: Change CloneForScope to ensure .Now()?
+      return new BocListRowControlObject (ID, Context.CloneForScope (rowScope));
+    }
+
+    public BocListRowControlObject GetRowWhere (string headerLabel, string containsText)
+    {
+      var cell = GetCellWhere (headerLabel, containsText);
+      var rowScope = cell.Scope.FindXPath ("..");
+      rowScope.Now(); // Todo RM-6297: Change CloneForScope to ensure .Now()?
+      return new BocListRowControlObject (ID, Context.CloneForScope (rowScope));
+    }
+
+    public BocListCellControlObject GetCellWhere (string headerLabel, string containsText)
+    {
+      var index = GetHeaderLabelIndex (headerLabel);
+      var cssSelector = string.Format (
+          ".bocListTable .bocListTableBody .bocListDataRow .bocListDataCell[{0}='{1}'] span[{2}*='{3}']",
+          DiagnosticMetadataAttributes.BocListCellIndex,
+          index,
+          DiagnosticMetadataAttributes.BocListCellContents,
+          containsText);
+      var cellScope = Scope.FindCss (cssSelector).FindXPath("../..");
+      cellScope.Now(); // Todo RM-6297: Change CloneForScope to ensure .Now()?
+      return new BocListCellControlObject (ID, Context.CloneForScope (cellScope));
+    }
+
+    public void ClickOnSortColumn (string headerLabel)
+    {
+      var index = GetHeaderLabelIndex (headerLabel);
+
+      var cssSelector = string.Format (".bocListFakeTableHead th[{0}='{1}'] a", DiagnosticMetadataAttributes.BocListCellIndex, index);
+      var sortColumnLinkScope = Scope.FindCss (cssSelector);
+
+      sortColumnLinkScope.ClickAndWait (Context, WaitFor.WxePostBack);
+    }
+
+    public void ChangeViewTo (string itemID)
+    {
+      // Todo RM-6297: think about implementation...
+    }
+
+    public void ChangeViewToByLabel (string label)
+    {
+      var availableViewsScope = FindChild ("Boc_AvailableViewsList");
+      availableViewsScope.PerformActionUsingWaitStrategy (scope => scope.SelectOption (label), Context, WaitFor.WxePostBack);
+    }
+
+    public DropDownMenuControlObject GetDropDownMenu ()
+    {
+      var dropDownMenuScope = FindChild ("Boc_OptionsMenu");
+      return new DropDownMenuControlObject (dropDownMenuScope.Id, Context.CloneForScope (dropDownMenuScope));
+    }
+
+    public ListMenuControlObject GetListMenu ()
+    {
+      var listMenuScope = FindChild ("Boc_ListMenu");
+      return new ListMenuControlObject (listMenuScope.Id, Context.CloneForScope (listMenuScope));
+    }
+
+    private int GetHeaderLabelIndex (string headerLabel)
+    {
+      return _headerLabels.IndexOf (headerLabel) + 1;
+    }
+
+    private ElementScope GetCurrentPageTextInputScope ()
+    {
+      return Scope.FindIdEndingWith ("Boc_CurrentPage_TextBox");
+    }
+  }
+
+
+  /// <summary>
+  /// Control object representing a row within a <see cref="Remotion.ObjectBinding.Web.UI.Controls.BocList"/>.
+  /// </summary>
+  public class BocListRowControlObject : BocControlObject
+  {
+    private readonly int _rowIndex;
+
+    public BocListRowControlObject ([NotNull] string id, [NotNull] TestObjectContext context)
+        : base (id, context)
+    {
+      _rowIndex = int.Parse (Scope[DiagnosticMetadataAttributes.BocListRowIndex]);
+    }
+
+    public void ClickSelectCheckbox ()
+    {
+      var zeroBasedRowIndex = _rowIndex - 1;
+      var rowSelectorCheckboxScope = FindChild (string.Format ("RowSelector_{0}", zeroBasedRowIndex));
+      rowSelectorCheckboxScope.Click();
+    }
+
+    public BocListEditableRowControlObject Edit ()
+    {
+      var editCommandScope = Scope.FindCss (string.Format ("td[{0}='{1}'] a", DiagnosticMetadataAttributes.BocListWellKnownEditCell, "true"));
+      editCommandScope.ClickAndWait (Context, WaitFor.WxePostBack);
+
+      return new BocListEditableRowControlObject (ID, Context);
+    }
+
+    public BocListCellControlObject GetCell (int index)
+    {
+      var cssSelector = string.Format ("td[{0}='{1}']", DiagnosticMetadataAttributes.BocListCellIndex, index);
+      var cellScope = Scope.FindCss (cssSelector);
+      cellScope.Now(); // Todo RM-6297: Change CloneForScope to ensure .Now()?
+
+      return new BocListCellControlObject (ID, Context.CloneForScope (cellScope));
+    }
+
+    public DropDownMenuControlObject GetRowDropDownMenu ()
+    {
+      var cssSelector = string.Format ("td[{0}='{1}'] span.DropDownMenuContainer", DiagnosticMetadataAttributes.BocListWellKnownRowDropDownMenuCell, "true");
+      var rowDropDownMenuScope = Scope.FindCss (cssSelector);
+      return new DropDownMenuControlObject (rowDropDownMenuScope.Id, Context.CloneForScope (rowDropDownMenuScope));
+    }
+  }
+
+  /// <summary>
+  /// Control object representing a row in edit-mode within a <see cref="Remotion.ObjectBinding.Web.UI.Controls.BocList"/>.
+  /// </summary>
+  public class BocListEditableRowControlObject : BocControlObject
+  {
+    public BocListEditableRowControlObject ([NotNull] string id, [NotNull] TestObjectContext context)
+        : base (id, context)
+    {
+    }
+
+    public BocListRowControlObject Save ()
+    {
+      var editCell = GetWellKnownEditCell();
+
+      var save = editCell.GetControl (new PerIndexControlSelectionCommand<CommandControlObject> (new CommandSelector(), 1));
+      save.Click();
+
+      return new BocListRowControlObject (ID, Context);
+    }
+
+    public BocListRowControlObject Cancel ()
+    {
+      var editCell = GetWellKnownEditCell();
+
+      var cancel = editCell.GetControl (new PerIndexControlSelectionCommand<CommandControlObject> (new CommandSelector(), 2));
+      cancel.Click();
+
+      return new BocListRowControlObject (ID, Context);
+    }
+
+    public BocListEditableCellControlObject GetCell (int index)
+    {
+      var cssSelector = string.Format ("td[{0}='{1}']", DiagnosticMetadataAttributes.BocListCellIndex, index);
+      var cellScope = Scope.FindCss (cssSelector);
+      cellScope.Now(); // Todo RM-6297: Change CloneForScope to ensure .Now()?
+
+      return new BocListEditableCellControlObject (ID, Context.CloneForScope (cellScope));
+    }
+
+    private BocListEditableCellControlObject GetWellKnownEditCell ()
+    {
+      var editCellScope = Scope.FindCss (string.Format ("td[{0}='{1}']", DiagnosticMetadataAttributes.BocListWellKnownEditCell, "true"));
+      editCellScope.Now(); // Todo RM-6297: Change CloneForScope to ensure .Now()?
+
+      return new BocListEditableCellControlObject (ID, Context.CloneForScope (editCellScope));
+    }
+  }
+
+  /// <summary>
+  /// Control object representing a cell within a <see cref="Remotion.ObjectBinding.Web.UI.Controls.BocList"/>.
+  /// </summary>
+  public class BocListCellControlObject : BocControlObject
+  {
+    public BocListCellControlObject ([NotNull] string id, [NotNull] TestObjectContext context)
+        : base (id, context)
+    {
+    }
+
+    /// <summary>
+    /// Returns the text content of the cell.
+    /// </summary>
+    public string GetText ()
+    {
+      return Scope.Text.Trim();
+    }
+
+    /// <summary>
+    /// Performs the cell's command.
+    /// </summary>
+    public UnspecifiedPageObject PerformCommand (IWaitingStrategy waitingStrategy = null)
+    {
+      var actualWaitingStrategy = GetActualWaitingStrategy (waitingStrategy);
+
+      var commandScope = Scope.FindCss ("a");
+      commandScope.ClickAndWait (Context, actualWaitingStrategy);
+      return UnspecifiedPage();
+    }
+  }
+
+  /// <summary>
+  /// Control object representing a cell in edit-mode within a <see cref="Remotion.ObjectBinding.Web.UI.Controls.BocList"/>.
+  /// </summary>
+  public class BocListEditableCellControlObject : BocControlObject, IControlHost
+  {
+    public BocListEditableCellControlObject ([NotNull] string id, [NotNull] TestObjectContext context)
+        : base (id, context)
+    {
+    }
+
+    public TControlObject GetControl<TControlObject> (IControlSelectionCommand<TControlObject> controlSelectionCommand)
+        where TControlObject : ControlObject
+    {
+      return controlSelectionCommand.Select (Context);
     }
   }
 }
