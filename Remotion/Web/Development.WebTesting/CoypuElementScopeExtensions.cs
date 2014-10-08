52634c0ce2a49cb90b1836c081bcb68e65e3a9f3
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-10-08 13:02:44 +0000

    Added CoypuElementScopeExtensions.ContextClick method. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30046 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Development.WebTesting/CoypuElementScopeExtensions.cs
+++ Remotion/Web/Development.WebTesting/CoypuElementScopeExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Coypu;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 
 namespace Remotion.Web.Development.WebTesting
 {
@@ -25,6 +27,22 @@ namespace Remotion.Web.Development.WebTesting
     private static void FocusLink (this ElementScope scope)
     {
       scope.SendKeys ("");
+    }
+
+    /// <summary>
+    /// Performs a context click (right click).
+    /// </summary>
+    /// <param name="scope">The <see cref="ElementScope"/> on which the action is performed.</param>
+    /// <param name="context">The corresponding control object's context.</param>
+    public static void ContextClick (this ElementScope scope, TestObjectContext context)
+    {
+      // Hack: Coypu does not directly support the Actions interface, therefore we need to fall back to using Selenium.
+      var webDriver = (IWebDriver) context.Browser.Native;
+      var nativeScope = (IWebElement) scope.Native;
+
+      var actions = new Actions (webDriver);
+      actions.ContextClick (nativeScope);
+      actions.Perform();
     }
   }
 }
