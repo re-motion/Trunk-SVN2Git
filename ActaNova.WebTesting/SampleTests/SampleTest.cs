e2bc0f342de84298e90b1756ecd77f7d6e7664a7
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-10-08 13:23:04 +0000

    Added ObjectBinding.Web.Development.WebTesting reference to temporary ActaNova.WebTesting sample project. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30053 cb0deee2-328e-b949-94ea-fb3139fab790

--- ActaNova.WebTesting/SampleTests/SampleTest.cs
+++ ActaNova.WebTesting/SampleTests/SampleTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using ActaNova.WebTesting.PageObjects;
 using NUnit.Framework;
+using Remotion.ObjectBinding.Web.Development.WebTesting.ControlObjects;
+using Remotion.ObjectBinding.Web.Development.WebTesting.ControlObjects.Selectors;
+using Remotion.ObjectBinding.Web.Development.WebTesting.ControlSelection;
 
 namespace ActaNova.WebTesting.SampleTests
 {
@@ -13,6 +16,13 @@ namespace ActaNova.WebTesting.SampleTests
       var home = Start();
 
       var newCitizenConcernPage = home.MainMenu.Select ("New", "Citizen concern").Expect<ActaNovaMainPageObject>();
+
+      newCitizenConcernPage.DetailsArea.GetControl (
+          new PerDomainPropertyControlSelectionCommand<BocAutoCompleteReferenceValueControlObject> (
+              new BocAutoCompleteReferenceValueSelector(),
+              "ApplicationContext")).FillWith ("BA - Bürgeranliegen");
+
+
     }
   }
 }
