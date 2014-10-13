3a2f0c5506ee2f3f7e73fccf65fe7c1729384638
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-10-13 08:20:22 +0000

    Added WebTreeViewSelector (not everything implemented yet, no integration tests yet). (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30077 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/Development.WebTesting/ControlObjects/Selectors/WebTreeViewSelector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Remotion.Web.Development.WebTesting.ControlObjects.Selectors
+{
+  /// <summary>
+  /// Control object selector for <see cref="WebTreeViewControlObject"/>.
+  /// </summary>
+  public class WebTreeViewSelector : RemotionControlSelectorBase<WebTreeViewControlObject>
+  {
+    public WebTreeViewSelector ()
+        : base ("div", "<not possible>") // Todo RM-6297: do something about <not possible>...we need a second base class...
+    {
+    }
+  }
+}
