9e5148c960bc8abe98507ae642f7ab8d0c4d5fdb
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-10-13 14:38:34 +0000

    Forgot to add WxeResetInWaitingStrategy, also fixed usage in WaitFor.cs. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30086 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/Development.WebTesting/WaitingStrategies/WxeResetInWaitingStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Coypu;
+using JetBrains.Annotations;
+using Remotion.Utilities;
+
+namespace Remotion.Web.Development.WebTesting.WaitingStrategies
+{
+  /// <summary>
+  /// Todo RM-6297: Docs
+  /// </summary>
+  public class WxeResetInWaitingStrategy : WxeWaitingStrategyBase, IWaitingStrategy
+  {
+    private readonly ElementScope _scope;
+
+    public WxeResetInWaitingStrategy ([NotNull] ElementScope scope)
+    {
+      ArgumentUtility.CheckNotNull ("scope", scope);
+
+      _scope = scope;
+    }
+
+    public object OnBeforeActionPerformed (TestObjectContext context)
+    {
+      return null;
+    }
+
+    public void PerformWaitAfterActionPerformed (TestObjectContext context, object state)
+    {
+      const int expectedWxePostBackSequenceNumber = 2;
+      WaitForWxePostBackSequenceNumber (context, _scope, expectedWxePostBackSequenceNumber);
+    }
+  }
+}
