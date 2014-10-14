ef1bab74f0e95a1171b68551817f0ae671b2e4ae
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-10-14 15:34:34 +0000

    Derived LabelSelector from ControlSeletorBase. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30122 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Development.WebTesting/ControlObjects/Selectors/LabelSelector.cs
+++ Remotion/Web/Development.WebTesting/ControlObjects/Selectors/LabelSelector.cs
@@ -5,11 +5,7 @@ namespace Remotion.Web.Development.WebTesting.ControlObjects.Selectors
   /// <summary>
   /// Control object selector for <see cref="LabelControlObject"/>.
   /// </summary>
-  public class LabelSelector : RemotionControlSelectorBase<LabelControlObject>
+  public class LabelSelector : ControlSelectorBase<LabelControlObject>
   {
-    public LabelSelector ()
-        : base ("span", "<not possible>") // Todo RM-6297: do something about <not possible>...we need a second base class...
-    {
-    }
   }
 }
