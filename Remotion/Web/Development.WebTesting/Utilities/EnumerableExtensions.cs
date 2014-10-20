871d83d955dfba7c75b83033e7b1117b7f3ccfe2
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-10-20 12:50:35 +0000

    Added EnumerableExtensions.IndexOf() LINQ-style utility method. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30153 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/Development.WebTesting/Utilities/EnumerableExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Remotion.Utilities;
+
+namespace Remotion.Web.Development.WebTesting.Utilities
+{
+  /// <summary>
+  /// Various extension methods for <see cref="IEnumerable{T}"/>.
+  /// </summary>
+  public static class EnumerableExtensions
+  {
+    /// <summary>
+    /// Returns the index of the first item in <paramref name="enumerable"/> matching the given <paramref name="predicate"/>.
+    /// </summary>
+    /// <remarks>
+    /// This method enumerates the <paramref name="enumerable"/>.
+    /// </remarks>
+    /// <returns>The index of the matching item, or -1 if no item matches.</returns>
+    public static int IndexOf<T> ([NotNull] this IEnumerable<T> enumerable, [NotNull] Func<T, bool> predicate)
+    {
+      ArgumentUtility.CheckNotNull ("enumerable", enumerable);
+      ArgumentUtility.CheckNotNull ("predicate", predicate);
+
+      var index = 0;
+      foreach(var item in enumerable)
+      {
+        if (predicate (item))
+          return index;
+
+        index++;
+      }
+
+      return -1;
+    }
+  }
+}
