0485703c1e47f1993d12f4f51551709dc37532a0
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-10-21 15:11:01 +0000

    Added CoypuElementScopeSelectExtensions with various useful extension methods (required for following BocReferenceValue & already skeleton-implemented BocListControlObject view selection). Also added an ISelectableControlObject interface. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30180 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/Development.WebTesting/ControlObjects/ISelectableControlObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Remotion.Web.Development.WebTesting.ControlObjects
+{
+  /// <summary>
+  /// Interface for all <see cref="ControlObject"/> implementations representing something selectable.
+  /// </summary>
+  public interface ISelectableControlObject
+  {
+    /// <summary>
+    /// Selects an option of the control using the given <paramref name="itemID"/>.
+    /// </summary>
+    /// <param name="itemID">The item ID of the option to select.</param>
+    /// <param name="actionBehavior">Required <see cref="IActionBehavior"/>, implementation uses default behavior if <see langword="null" /> is passed.</param>
+    /// <returns>An unspecified page object, may be used in case a new page is expected after clicking the control object.</returns>
+    UnspecifiedPageObject SelectOption ([NotNull] string itemID, [CanBeNull] IActionBehavior actionBehavior = null);
+
+    /// <summary>
+    /// Selects an option of the control using the given <paramref name="index"/>.
+    /// </summary>
+    /// <param name="index">The index of the option to select.</param>
+    /// <param name="actionBehavior">Required <see cref="IActionBehavior"/>, implementation uses default behavior if <see langword="null" /> is passed.</param>
+    /// <returns>An unspecified page object, may be used in case a new page is expected after clicking the control object.</returns>
+    UnspecifiedPageObject SelectOption (int index, [CanBeNull] IActionBehavior actionBehavior = null);
+
+    /// <summary>
+    /// Selects an option of the control using the given <paramref name="text"/>.
+    /// </summary>
+    /// <param name="text">The text of the option to select.</param>
+    /// <param name="actionBehavior">Required <see cref="IActionBehavior"/>, implementation uses default behavior if <see langword="null" /> is passed.</param>
+    /// <returns>An unspecified page object, may be used in case a new page is expected after clicking the control object.</returns>
+    UnspecifiedPageObject SelectOptionByText ([NotNull] string text, [CanBeNull] IActionBehavior actionBehavior = null);
+  }
+}
---
+++ Remotion/Web/Development.WebTesting/CoypuElementScopeSelectExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Coypu;
+using JetBrains.Annotations;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using Remotion.Utilities;
+
+namespace Remotion.Web.Development.WebTesting
+{
+  /// <summary>
+  /// Extension methods for Coypu's <see cref="ElementScope"/> class regarding <see cref="ElementScope.SelectOption"/>.
+  /// </summary>
+  public static class CoypuElementScopeSelectExtensions
+  {
+    /// <summary>
+    /// Returns the text of the currently selected option. If more than one option is selected, this method returns the first selected item's text.
+    /// </summary>
+    /// <param name="scope">The <see cref="ElementScope"/> on which the action is performed.</param>
+    /// <returns>The text of the currently selected option.</returns>
+    public static string GetSelectedOptionText ([NotNull] this ElementScope scope)
+    {
+      ArgumentUtility.CheckNotNull ("scope", scope);
+
+      // Hack: Coypu does not yet support SelectElement, use Selenium directly.
+      var webElement = (IWebElement) scope.Native;
+
+      var select = new SelectElement (webElement);
+      return select.SelectedOption.Text;
+    }
+
+    /// <summary>
+    /// Selects an option of a &lt;select&gt; element by <paramref name="index"/>.
+    /// </summary>
+    /// <param name="scope">The <see cref="ElementScope"/> on which the action is performed.</param>
+    /// <param name="index">The index of the option to select.</param>
+    public static void SelectOptionByIndex ([NotNull] this ElementScope scope, int index)
+    {
+      ArgumentUtility.CheckNotNull ("scope", scope);
+
+      // Hack: Coypu does not yet support SelectElement, use Selenium directly.
+      var webElement = (IWebElement) scope.Native;
+
+      var select = new SelectElement (webElement);
+      select.SelectByIndex (index - 1);
+    }
+
+    /// <summary>
+    /// Selects an option of a &lt;select&gt; element by <paramref name="value"/>.
+    /// </summary>
+    /// <param name="scope">The <see cref="ElementScope"/> on which the action is performed.</param>
+    /// <param name="value">The value to select.</param>
+    public static void SelectOptionByValue ([NotNull] this ElementScope scope, [NotNull] string value)
+    {
+      ArgumentUtility.CheckNotNull ("scope", scope);
+      ArgumentUtility.CheckNotNull ("value", value);
+
+      // Hack: Coypu does not yet support SelectElement, use Selenium directly.
+      var webElement = (IWebElement) scope.Native;
+
+      var select = new SelectElement (webElement);
+      select.SelectByValue (value);
+    }
+  }
+}
