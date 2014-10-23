c5fed18aa1c49914650392e3091449ff085c58d5
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-10-23 13:50:52 +0000

    Fixed test name. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30216 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Development.WebTesting.IntegrationTests/InfrastructureTests.cs
+++ Remotion/Web/Development.WebTesting.IntegrationTests/InfrastructureTests.cs
@@ -13,7 +13,7 @@ namespace Remotion.Web.Development.WebTesting.IntegrationTests
     [TestCase ("! \" § $ % & / ( ) = ? ² ³ { [ ] } \\ + * ~ ' # @ < > | A Z a z 0 1 8 9! \" § $ % & / ( ) =  ? ² ³ { [ ] } \\ + * ~ ' # @ < > | A Z a z 0 1 8 9")]
     //[TestCase("°")] // Todo RM-6297: Does not work in Chrome with de_AT keyboard, see https://code.google.com/p/chromedriver/issues/detail?id=932
     //[TestCase("^")] // Todo RM-6297: Does not work in IE with de_AT keyboard, see http://stackoverflow.com/questions/26357191/
-    public void TestFillWithAndWait_FromCoypuWaitingElementScopeExtensions (string input)
+    public void TestCoypuElementScopeFillInWithAndSendKeysExtensions_FillWithAndWait (string input)
     {
       // Todo RM-6297: Fix problems on TeamCity with FillWithFixed.
       if (WebTestConfiguration.Current.BrowserIsInternetExplorer())
