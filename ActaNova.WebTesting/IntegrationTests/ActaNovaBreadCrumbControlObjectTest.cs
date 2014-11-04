a97fb458c37e0044dd01b92f09bb549ce2f6c440
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-11-04 15:56:54 +0000

    Moved tests for bread crumbs into own integration test class. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30356 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ ActaNova.WebTesting/IntegrationTests/ActaNovaBreadCrumbControlObjectTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ActaNova.WebTesting.SampleTests;
+using NUnit.Framework;
+
+namespace ActaNova.WebTesting.IntegrationTests
+{
+  [TestFixture]
+  public class ActaNovaBreadCrumbControlObjectTest : ActaNovaWebTestBase
+  {
+    [Test]
+    public void Test ()
+    {
+      var home = Start();
+
+      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (1));
+      Assert.That (home.Header.GetBreadCrumbs()[0].GetText(), Is.EqualTo ("Eigener AV"));
+
+      home.Tree.GetNode().WithText ("Stellvertretungs AV").Select();
+
+      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (1));
+      Assert.That (home.Header.GetBreadCrumbs()[0].GetText(), Is.EqualTo ("Stellvertretungs AV"));
+
+      home.MainMenu.Select ("Verfahrensbereich", "BA - Bürgeranliegen");
+      home.MainMenu.Select ("Extras", "Passwort ändern");
+
+      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (2));
+      Assert.That (home.Header.GetBreadCrumbs()[0].GetText(), Is.EqualTo ("Stellvertretungs AV"));
+      Assert.That (home.Header.GetBreadCrumbs()[1].GetText(), Is.EqualTo ("Passwort ändern"));
+
+      var searchPage = home.MainMenu.Select ("Suchen", "Geschäftsobjekt", "Akt").ExpectActaNova();
+
+      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (3));
+      Assert.That (home.Header.GetBreadCrumbs()[2].GetText(), Is.EqualTo ("Akt Suchen"));
+
+      searchPage.DetailsArea.Perform ("Search");
+
+      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (3));
+      Assert.That (home.Header.GetBreadCrumbs()[2].GetText(), Is.EqualTo ("Akt Suchen"));
+
+      searchPage.Header.GetBreadCrumbs()[0].Click();
+
+      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (1));
+      Assert.That (home.Header.GetBreadCrumbs()[0].GetText(), Is.EqualTo ("Stellvertretungs AV"));
+    }
+  }
+}
--- ActaNova.WebTesting/IntegrationTests/ActaNovaHeaderControlObjectTest.cs
+++ ActaNova.WebTesting/IntegrationTests/ActaNovaHeaderControlObjectTest.cs
@@ -79,32 +79,7 @@ namespace ActaNova.WebTesting.IntegrationTests
       Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (1));
       Assert.That (home.Header.GetBreadCrumbs()[0].GetText(), Is.EqualTo ("Eigener AV"));
 
-      home.Tree.GetNode().WithText ("Stellvertretungs AV").Select();
-
-      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (1));
-      Assert.That (home.Header.GetBreadCrumbs()[0].GetText(), Is.EqualTo ("Stellvertretungs AV"));
-
-      home.MainMenu.Select ("Verfahrensbereich", "BA - Bürgeranliegen");
-      home.MainMenu.Select ("Extras", "Passwort ändern");
-
-      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (2));
-      Assert.That (home.Header.GetBreadCrumbs()[0].GetText(), Is.EqualTo ("Stellvertretungs AV"));
-      Assert.That (home.Header.GetBreadCrumbs()[1].GetText(), Is.EqualTo ("Passwort ändern"));
-
-      var searchPage = home.MainMenu.Select ("Suchen", "Geschäftsobjekt", "Akt").ExpectActaNova();
-
-      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (3));
-      Assert.That (home.Header.GetBreadCrumbs()[2].GetText(), Is.EqualTo ("Akt Suchen"));
-
-      searchPage.DetailsArea.Perform ("Search");
-
-      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (3));
-      Assert.That (home.Header.GetBreadCrumbs()[2].GetText(), Is.EqualTo ("Akt Suchen"));
-
-      searchPage.Header.GetBreadCrumbs()[0].Click();
-
-      Assert.That (home.Header.GetBreadCrumbs().Count, Is.EqualTo (1));
-      Assert.That (home.Header.GetBreadCrumbs()[0].GetText(), Is.EqualTo ("Stellvertretungs AV"));
+      // Note: tests for ActaNovaBreadCrumbControlObject are found in ActaNovaBreadCrumbControlObjectTest, this test only tests the Header property.
     }
   }
 }
