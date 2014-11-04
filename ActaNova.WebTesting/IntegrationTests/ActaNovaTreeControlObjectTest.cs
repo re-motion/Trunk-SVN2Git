7eeed30d2c63504fd800e8d403f2183f13163aa2
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-11-04 16:02:16 +0000

    Moved tests for ActaNova tree into own integration test class. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30357 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ ActaNova.WebTesting/IntegrationTests/ActaNovaTreeControlObjectTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ActaNova.WebTesting.PageObjects;
+using ActaNova.WebTesting.SampleTests;
+using NUnit.Framework;
+
+namespace ActaNova.WebTesting.IntegrationTests
+{
+  [TestFixture]
+  public class ActaNovaTreeControlObjectTest : ActaNovaWebTestBase
+  {
+    [Test]
+    public void Test ()
+    {
+      var home = Start();
+
+      var eigenerAv = home.Tree.GetNode().WithIndex (1);
+      Assert.That (eigenerAv.GetText(), Is.EqualTo ("Eigener AV"));
+
+      home = home.Tree.GetNode().WithIndex (2).Expand().GetNode().WithText ("egora Gemeinde").Select().Expect<ActaNovaMainPageObject>();
+      Assert.That (home.DetailsArea.FormPageTitle, Is.EqualTo ("egora Gemeinde AV"));
+
+      var geschaeftsfall = home.Tree.GetNode().WithText ("Favoriten").Expand().Collapse().Expand().GetNode().WithIndex (2);
+      home = geschaeftsfall.Select().Expect<ActaNovaMainPageObject>();
+      Assert.That (home.DetailsArea.FormPageTitle, Is.EqualTo ("Geschäftsfall \"OE/1/BW-BV-BA-M/1\" bearbeiten"));
+
+      home =
+          geschaeftsfall.GetNode ("Files")
+              .Expand()
+              .GetNode().WithIndex (1)
+              .Expand()
+              .GetNode ("WrappedDocumentsHierarchy")
+              .Expand()
+              .Select()
+              .Expect<ActaNovaMainPageObject>();
+      Assert.That (home.DetailsArea.FormPageTitle, Is.EqualTo ("Akt \"OE/1\" bearbeiten"));
+    }
+  }
+}
--- ActaNova.WebTesting/SampleTests/SampleTest.cs
+++ ActaNova.WebTesting/SampleTests/SampleTest.cs
@@ -13,34 +13,6 @@ namespace ActaNova.WebTesting.SampleTests
   public class SampleTest : ActaNovaWebTestBase
   {
     [Test]
-    public void TestActaNovaTree ()
-    {
-      var home = Start();
-
-      var eigenerAv = home.Tree.GetNode().WithIndex (1);
-      Assert.That (eigenerAv.GetText(), Is.EqualTo ("Eigener AV"));
-
-      home = home.Tree.GetNode().WithIndex (2).Expand().GetNode().WithText ("egora Gemeinde").Select().Expect<ActaNovaMainPageObject>();
-      Assert.That (home.DetailsArea.FormPageTitle, Is.EqualTo ("egora Gemeinde AV"));
-
-      var geschaeftsfall = home.Tree.GetNode().WithText ("Favoriten").Expand().Collapse().Expand().GetNode().WithIndex (2);
-      home = geschaeftsfall.Select().Expect<ActaNovaMainPageObject>();
-      Assert.That (home.DetailsArea.FormPageTitle, Is.EqualTo ("Geschäftsfall \"OE/1/BW-BV-BA-M/1\" bearbeiten"));
-
-      home =
-          geschaeftsfall.GetNode ("Files")
-              .Expand()
-              .GetNode().WithIndex (1)
-              .Expand()
-              .GetNode ("WrappedDocumentsHierarchy")
-              .Expand()
-              .Select()
-              .Expect<ActaNovaMainPageObject>();
-      Assert.That (home.DetailsArea.FormPageTitle, Is.EqualTo ("Akt \"OE/1\" bearbeiten"));
-    }
-
-
-    [Test]
     public void TestRefresh ()
     {
       var home = Start();
