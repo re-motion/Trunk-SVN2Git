4fe5c5103581f60879c39675b55bc24e7821fc8a
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-11-10 18:14:32 +0000

    Added HtmlPageObject for simple HTML pages (like ActaNova help index). (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30442 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ Remotion/Web/Development.WebTesting/PageObjects/HtmlPageObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Remotion.Web.Development.WebTesting.PageObjects
+{
+  /// <summary>
+  /// Represents a simple HTML page.
+  /// </summary>
+  public class HtmlPageObject : PageObject
+  {
+    public HtmlPageObject ([NotNull] PageObjectContext context)
+        : base(context)
+    {
+    }
+
+    public void Close ()
+    {
+      Context.CloseWindow();
+    }
+  }
+}
