07cbd0634c890db827db8237fc6d9004d595fc95
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-11-11 11:06:45 +0000

    Added XMLDoc comments to HtmlPageObject. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30446 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Development.WebTesting/PageObjects/HtmlPageObject.cs
+++ Remotion/Web/Development.WebTesting/PageObjects/HtmlPageObject.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+
+using System;
 using JetBrains.Annotations;
 
 namespace Remotion.Web.Development.WebTesting.PageObjects
@@ -9,10 +26,13 @@ namespace Remotion.Web.Development.WebTesting.PageObjects
   public class HtmlPageObject : PageObject
   {
     public HtmlPageObject ([NotNull] PageObjectContext context)
-        : base(context)
+        : base (context)
     {
     }
 
+    /// <summary>
+    /// Closes the window.
+    /// </summary>
     public void Close ()
     {
       Context.CloseWindow();
