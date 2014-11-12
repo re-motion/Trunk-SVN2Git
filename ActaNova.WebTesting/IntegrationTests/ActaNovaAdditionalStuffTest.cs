8b54f2244285eb071cafc87f2748b10b9b31925d
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-11-12 17:59:41 +0000

    Added ActaNovaAdditionalStuffTest for control-object-unrelated integration tests - a collection of "special case" tests. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30473 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ ActaNova.WebTesting/IntegrationTests/ActaNovaAdditionalStuffTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ActaNova.WebTesting.ActaNovaExtensions;
+using NUnit.Framework;
+
+namespace ActaNova.WebTesting.IntegrationTests
+{
+  [TestFixture]
+  public class ActaNovaAdditionalStuffTest : ActaNovaWebTestBase
+  {
+    [Test]
+    public void TestMultipleGridColumns ()
+    {
+      var home = Start();
+
+      var editIncoming = home.WorkListPage.GetWorkList().GetCellWhere ("WorkItem", "04.04.2001/1").ExecuteCommand().ExpectMainPage();
+      editIncoming.FormPage.GetOnlyTabbedMultiView().SwitchTo ("EditOwnedObjectSpecificSecurityFormPage_view");
+
+      var typeEnum = editIncoming.FormPage.GetEnumValue ("VisibleObjectSpecificSecurityType");
+      Assert.That (typeEnum.GetSelectedOption(), Is.EqualTo ("Standard"));
+
+      var inheritBool = editIncoming.FormPage.GetBooleanValue ("InheritObjectSpecificSecurity");
+      inheritBool.SetTo (false);
+      typeEnum.SelectOption ("Extended");
+
+      var visibleObjectSpecificSecurtiyEntries = editIncoming.FormPage.GetList ("VisibleObjectSpecificSecurityEntries");
+      Assert.That (
+          visibleObjectSpecificSecurtiyEntries.GetTopBlock().Scope.Text,
+          Is.StringContaining ("Besitzer Muster Max, Ing. (EG/1) ist immer am Objekt berechtigt"));
+      Assert.That (visibleObjectSpecificSecurtiyEntries.GetRowCount(), Is.EqualTo (1));
+
+      editIncoming.FormPage.PerformAndConfirmDataLoss ("Cancel");
+    }
+  }
+}
