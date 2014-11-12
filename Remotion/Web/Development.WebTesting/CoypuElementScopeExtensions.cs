9c96ce86bfed946bca6955dbbf8f598236a772f0
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-11-12 17:38:25 +0000

    Improved XMLDoc comment for CoypuElementScopeExtensions.EnsureExistence(). (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30462 cb0deee2-328e-b949-94ea-fb3139fab790

--- Remotion/Web/Development.WebTesting/CoypuElementScopeExtensions.cs
+++ Remotion/Web/Development.WebTesting/CoypuElementScopeExtensions.cs
@@ -29,11 +29,12 @@ namespace Remotion.Web.Development.WebTesting
   public static class CoypuElementScopeExtensions
   {
     /// <summary>
-    /// This method reduces performance a little bit (<see cref="WebTestObjectContext"/>s which are never actually accessed by the web test are also
-    /// resolved). However, it ensures that any <see cref="MissingHtmlException"/> is thrown when the <see cref="WebTestObjectContext"/> is created,
-    /// which is always near the corresponding <c>parentScope.Find*()</c> method call. Otherwise, the <see cref="MissingHtmlException"/> would be
-    /// thrown when the context's <see cref="Scope"/> is actually used for the first time, which may be quite some time later and the exception would
-    /// provide a stack trace where the <c>parentScope.Find*()</c> call could not be found.
+    /// This method ensures that the given <paramref name="scope"/> is existent. Coypu would automatically check for existence whenever we access the
+    /// scope, however, we explicitly check the existence when creating new control objects. This ensures that any <see cref="MissingHtmlException"/>
+    /// or <see cref="AmbiguousException"/> are thrown when the control object's corresponding <see cref="WebTestObjectContext"/> is created, which
+    /// is always near the corresponding <c>parentScope.Find*()</c> method call. Otherwise, exceptions would be thrown when the context's
+    /// <see cref="Scope"/> is actually used for the first time, which may be quite some time later and the exception would provide a stack trace
+    /// where the <c>parentScope.Find*()</c> call could not be found.
     /// </summary>
     /// <param name="scope">The <see cref="ElementScope"/> which is asserted to exist.</param>
     public static void EnsureExistence ([NotNull] this ElementScope scope)
