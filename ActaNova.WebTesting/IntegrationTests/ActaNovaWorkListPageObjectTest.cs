4369225b32e005f9428013b7efde537bda5a7556
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-11-17 08:25:49 +0000

    Added integration test for ActaNovaWorkListPageObject. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30509 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ ActaNova.WebTesting/IntegrationTests/ActaNovaWorkListPageObjectTest.cs
@@ -0,0 +1,37 @@
+﻿using ActaNova.WebTesting.ActaNovaExtensions;
+using ActaNova.WebTesting.PageObjects;
+using NUnit.Framework;
+
+namespace ActaNova.WebTesting.IntegrationTests
+{
+  [TestFixture]
+  public class ActaNovaWorkListPageObjectTest : ActaNovaWebTestBase
+  {
+    [Test]
+    public void Test ()
+    {
+      var home = Start();
+
+      // Todo EVB-8268: Enable as soon as the RSS feed window does have a title.
+      //var rssFeedWindow = home.WorkListPage.OpenRssFeed();
+      //rssFeedWindow.Close();
+
+      // Todo EVB-8268: Enable as soon as the help window does have a title.
+      //var helpPage = home.WorkListPage.OpenHelp();
+      //helpPage.Close();
+
+      // Todo EVB-8268: Enable as soon as the bug report window does not show a yellow page anymore.
+      //var bugReportWindow = home.WorkListPage.CreateBugReport();
+      //bugReportWindow.Close();
+
+      var workList = home.WorkListPage.GetWorkList();
+      workList.GetRowWhere ("WorkItem", "04.06.2009/1").ClickSelectCheckbox();
+
+      var weiterleitenPopup = home.WorkListPage.GetWorkStepMenu()
+          .SelectItem()
+          .WithText ("Weiterleiten")
+          .ExpectNewPopupWindow<ActaNovaPopupWindowPageObject> ("Aktivität weiterleiten");
+      weiterleitenPopup.PerformAndCloseWindow ("Cancel");
+    }
+  }
+}
