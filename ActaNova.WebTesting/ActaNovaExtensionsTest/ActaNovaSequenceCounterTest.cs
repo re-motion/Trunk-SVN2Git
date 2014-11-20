bcafebf1c06e10d9d1e62c488b955e76f5ae1ee9
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-11-20 15:07:04 +0000

    Added ActaNovaConvinienceExtensions, capsuling special completion detections for various operations on ActaNova-related page- and control objects. Added integration test for them as well. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30581 cb0deee2-328e-b949-94ea-fb3139fab790

---
+++ ActaNova.WebTesting/ActaNovaExtensionsTest/ActaNovaSequenceCounterTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using ActaNova.WebTesting.ActaNovaExtensions;
+using NUnit.Framework;
+using Remotion.Web.Development.WebTesting;
+
+namespace ActaNova.WebTesting.ActaNovaExtensionsTest
+{
+  [TestFixture]
+  public class ActaNovaSequenceCounterTest : ActaNovaWebTestBase
+  {
+    [Test]
+    public void TestOuterFrameUsageAfterTabbedMultiViewTabSwitchAndAfterAdditionalCommandsMenuSelect ()
+    {
+      var home = Start();
+
+      var editCitizenConcern = home.WorkListPage.GetWorkList().GetCellWhere ("WorkItem", "04.06.2009/1").OpenWorkListItem();
+      Assert.That (home.Header.GetNumberOfBreadCrumbs(), Is.EqualTo (2));
+
+      editCitizenConcern.FormPage.SwitchTo ("IncomingEnclosuresFormPage_view");
+
+      editCitizenConcern.Tree.GetNode().WithDisplayText ("Gruppen AV").Expand();
+
+      var editPerson = editCitizenConcern.Tree.GetNode()
+          .WithDisplayText ("04.06.2009/1")
+          .GetNode ("ActualSubmitters")
+          .Expand();
+
+      editCitizenConcern.Tree.GetNode().WithDisplayText ("Gruppen AV").Collapse();
+
+      var createMail = editCitizenConcern.FormPage.GetAdditionalCommandsMenu()
+          .SelectItem()
+          .WithDisplayText ("Mail versenden", Continue.When (Wxe.PostBackCompletedInParent(editCitizenConcern)))
+          .ExpectMainPage();
+
+      Assert.That (home.Header.GetNumberOfBreadCrumbs(), Is.EqualTo (3));
+      
+      createMail.Header.GetBreadCrumb (3).Click();
+      createMail.Header.GetBreadCrumb (1).ClickAndPreventDataLoss();
+
+      Assert.That (home.Header.GetNumberOfBreadCrumbs(), Is.EqualTo (3));
+    }
+
+    [Test]
+    public void TestOuterFrameUsageAfterPerform ()
+    {
+      var home = Start();
+
+      var editCitizenConcern = home.WorkListPage.GetWorkList().GetCellWhere ("WorkItem", "04.06.2009/1").OpenWorkListItem();
+      home = editCitizenConcern.FormPage.Perform ("Cancel").ExpectMainPage();
+
+      Assert.That (home.Header.GetNumberOfBreadCrumbs(), Is.EqualTo (1));
+    }
+  }
+}
