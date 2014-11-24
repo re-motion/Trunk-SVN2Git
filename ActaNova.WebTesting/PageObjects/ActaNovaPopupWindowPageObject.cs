6f42ffbb8dbec36d2a2e2595d0f50ea36fd26598
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-11-24 12:55:37 +0000

    Code cleanup. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30661 cb0deee2-328e-b949-94ea-fb3139fab790

--- ActaNova.WebTesting/PageObjects/ActaNovaPopupWindowPageObject.cs
+++ ActaNova.WebTesting/PageObjects/ActaNovaPopupWindowPageObject.cs
@@ -7,7 +7,6 @@ namespace ActaNova.WebTesting.PageObjects
   /// <summary>
   /// Represents an ActaNova popup window.
   /// </summary>
-  // ReSharper disable once ClassNeverInstantiated.Global
   public class ActaNovaPopupWindowPageObject : ActaNovaPageObject
   {
     public ActaNovaPopupWindowPageObject ([NotNull] PageObjectContext context)
