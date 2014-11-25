8a79cd476a55b5d3296c5e3786c60a409d07599c
dominik.rauch <dominik.rauch@cb0deee2-328e-b949-94ea-fb3139fab790>
2014-11-25 16:16:41 +0000

    Moved GetFullPath() call to ActaNovaDownLevelDmsControlObject. (RM-6297)
    git-svn-id: https://svn.re-motion.org/svn/Remotion/trunk@30704 cb0deee2-328e-b949-94ea-fb3139fab790

--- ActaNova.WebTesting/ControlObjects/ActaNovaDownLevelDmsControlObject.cs
+++ ActaNova.WebTesting/ControlObjects/ActaNovaDownLevelDmsControlObject.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Coypu;
 using JetBrains.Annotations;
 using Remotion.Utilities;
@@ -27,11 +28,13 @@ namespace ActaNova.WebTesting.ControlObjects
     {
       ArgumentUtility.CheckNotNullOrEmpty ("filePath", filePath);
 
+      var fullFilePath = Path.GetFullPath (filePath);
+
       var scope = GetIframeScope();
 
       var fileScope = scope.FindCss ("input.HtmlUpload");
       // Note: do not use SendKeysFixed here, it does not work with file input tags in IE => we do not support "special characters" in file paths.
-      fileScope.SendKeys (filePath);
+      fileScope.SendKeys (fullFilePath);
 
       var uploadButton = scope.FindCss ("input.UploadButton");
       uploadButton.ClickAndWait (Context, GetActualCompletionDetector (completionDetection), modalDialogHandler);
