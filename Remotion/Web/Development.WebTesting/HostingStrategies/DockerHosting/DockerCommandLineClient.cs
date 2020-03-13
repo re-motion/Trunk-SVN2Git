070fff2fd901c1012e22d388db0aa94bdfce34da
Marcel Holle <marcel.holle@rubicon.eu>
2020-03-13 09:20:06 +0100

    Revert "Review: Use env special folder"
    This reverts commit e99384f3

--- Remotion/Web/Development.WebTesting/HostingStrategies/DockerHosting/DockerCommandLineClient.cs
+++ Remotion/Web/Development.WebTesting/HostingStrategies/DockerHosting/DockerCommandLineClient.cs
@@ -210,7 +210,9 @@ namespace Remotion.Web.Development.WebTesting.HostingStrategies.DockerHosting
 
     private static string GetDockerExeFullPath ()
     {
-      var programFiles = Environment.GetFolderPath (Environment.SpecialFolder.ProgramFiles);
+      // Note: We explicitly do not use Environment.ProgramFiles, as it chooses ProgramFiles (x86) when we run our Unit Tests in a 32 Bit Context
+      // As the Build Server is 64 bit, Docker cannot be found there under these circumstances.
+      const string programFiles = "C:\\Program Files";
 
       var listOfKnownDockerLocations = new List<string>
                                        {
