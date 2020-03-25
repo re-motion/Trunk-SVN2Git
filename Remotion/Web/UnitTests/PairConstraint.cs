4c6f5c7e33e0783ed68c35954dd4ef0bbcdcea72
Daniel Kudernatsch <daniel.kudernatsch@rubicon.eu>
2020-03-25 10:01:19 +0100

    pair constraint

--- Remotion/Web/UnitTests/PairConstraint.cs
+++ Remotion/Web/UnitTests/PairConstraint.cs
@@ -23,42 +23,30 @@ namespace Remotion.Web.UnitTests
   public class PairConstraint : EqualConstraint
   {
     private readonly Pair _expected;
-    private Pair _actual;
 
     public PairConstraint (Pair expected)
-        : base(expected)
+        : base (expected)
     {
       _expected = expected;
     }
 
-    public override bool Matches (object actual)
+    public override ConstraintResult ApplyTo<TActual> (TActual actual)
     {
-      _actual = actual as Pair;
+      var actualPair = actual as Pair;
+      var baseResult = base.ApplyTo (actual);
+      if (baseResult.IsSuccess)
+        return baseResult;
 
-      if (base.Matches (actual))
-        return true;
+      return new PairConstraintResult (this, actualPair, _expected, Matches (actualPair));
+    }
 
+    private bool Matches (Pair actual)
+    {
       if (actual == null)
         return _expected == null;
 
-      return object.Equals (_expected.First, ((Pair) actual).First) && object.Equals (_expected.Second, ((Pair) actual).Second);
-    }
-
-    public override void WriteMessageTo (MessageWriter writer)
-    {
-      if (_expected == null || _actual == null)
-      {
-        base.WriteMessageTo (writer);
-      }
-      else
-      {
-        writer.DisplayStringDifferences (
-            string.Format ("{{ {0} , {1} }}", _expected.First, _expected.Second),
-            string.Format ("{{ {0} , {1} }}", _actual.First, _actual.Second),
-            -1,
-            false,
-            false);
-      }
+      return object.Equals (_expected.First, actual.First)
+             && object.Equals (_expected.Second, actual.Second);
     }
   }
 }
